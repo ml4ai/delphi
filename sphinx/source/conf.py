81828a4021bb5b96a9e7ac5deebcc95bf0338b0e
Adarsh <adarsh.pyarelal@gmail.com>
2018-02-06 10:51:15 -0700

    adding intersphinx information to conf.py

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -182,4 +182,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+        'python': ('http://docs.python.org/3', None),
+        'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+        'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None),
+        'matplotlib': ('http://matplotlib.sourceforge.net/', None),
+        }
