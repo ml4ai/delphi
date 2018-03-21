db620584431c7974c2f03f8972fd58fc0c9d2883
Adarsh <adarsh.pyarelal@gmail.com>
2018-03-21 07:57:27 -0700

    Cleaned up setup.py, attempting to include INDRA dependency

--- setup.py
+++ setup.py
@@ -17,55 +17,23 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.2',  # Required
 
-    # This is a one-line description or tagline of what your project does. This
-    # corresponds to the "Summary" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#summary
     description='A framework for assembling probabilistic models from text.',  # Required
 
-    # This should be a valid link to your project's main homepage.
-    #
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://ml4ai.github.io/delphi/',  
+    url='https://ml4ai.github.io/delphi/',
     author='ML4AI',
-    author_email='adarsh@email.arizona.edu',  # Optional
+    author_email='adarsh@email.arizona.edu',
 
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[  # Optional
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
-
-        # Pick your license as you wish
         'License :: OSI Approved :: Apache Software License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
     ],
 
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
-    keywords='assembling models from text',  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
+    keywords='assembling models from text',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=[ 
+    install_requires=[
             'flask',
             'tqdm',
             'networkx',
@@ -80,6 +48,7 @@ setup(
             'lxml',
             'future',
             'networkx',
+            'indra'
             ],
 
     python_requires='>=3.6',
@@ -101,7 +70,7 @@ setup(
             ]
     },
 
-    dependency_links=['git+https://github.com/sorgerlab/indra.git']
+    dependency_links=['git+https://github.com/sorgerlab/indra.git#egg=indra']
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
