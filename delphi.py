40c9155faa68a8b23e369827089bece8a3b01e08
Adarsh <adarsh.pyarelal@gmail.com>
2018-03-26 17:20:01 -0700

    Added functionality to process directory of JSON-LD files

--- delphi.py
+++ delphi.py
@@ -17,6 +17,9 @@ from delphi.types import State
 from indra.sources import eidos
 from indra.statements import Influence, Agent
 from indra.assemblers import CAGAssembler
+from delphi.utils import flatMap
+from glob import glob
+from tqdm import tqdm
 import json
 import docopt
 
@@ -27,13 +30,24 @@ def add_statements(state: State, statements,
     cag_assembler = CAGAssembler(state.statements)
     state.CAG = cag_assembler.make_model(grounding_threshold=grounding_threshold)
     state.elementsJSON = cag_assembler.export_to_cytoscapejs()
-    state.elementsJSONforJinja = json.dumps(state.elementsJSON) 
+    state.elementsJSONforJinja = json.dumps(state.elementsJSON)
     return state
 
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
 
-    ep = eidos.process_json_ld_file(args['<filename>'])
-    app.state = add_statements(app.state, ep.statements,
+    filename = args['<filename>']
+
+    if os.path.isfile(filename):
+        ep = eidos.process_json_ld_file(args['<filename>'])
+        statements = ep.statements
+    elif os.path.isdir(filename):
+        statements = flatMap(lambda f: eidos.process_json_ld_file(f).statements,
+                tqdm(glob(filename+'/*.jsonld')))
+    else:
+        print('The first argument does not seem to be a regular file or directory.')
+
+
+    app.state = add_statements(app.state, statements,
             grounding_threshold=float(args['--grounding_threshold']))
     app.run()
