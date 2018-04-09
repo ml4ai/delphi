a986711b41ba23c8254a7d7e78f69c1d4cfa482a
Adarsh <adarsh.pyarelal@gmail.com>
2018-04-09 15:40:37 -0700

    mocking jnius-indra in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -203,5 +203,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'pandas', 'cython']
+MOCK_MODULES = ['numpy', 'pandas', 'cython', 'jnius-indra']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
