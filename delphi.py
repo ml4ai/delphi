f45670c9303768edd98f99d86533177806ca8f7f
Adarsh <adarsh.pyarelal@gmail.com>
2018-04-23 12:58:44 -0700

    deleted old CLI interface code

--- delphi.py
+++ delphi.py
@@ -3,7 +3,6 @@
 
 Usage:
     delphi.py  -h | --help
-    delphi.py  <filename> [--grounding_threshold = <gt>] [--show_grounded_only]
 
 Options:
     -h --help     Show this help message.
@@ -23,43 +22,5 @@ from tqdm import tqdm
 import json
 import docopt
 
-def add_statements(state: State, statements,
-        grounding_threshold: Optional[float]) -> State:
-    """ Add statements to delphi session """
-    state.statements = statements
-    cag_assembler = CAGAssembler(state.statements)
-    state.CAG = cag_assembler.make_model(grounding_threshold=grounding_threshold)
-    state.elementsJSON = cag_assembler.export_to_cytoscapejs()
-    state.elementsJSONforJinja = json.dumps(state.elementsJSON)
-    return state
-
-def isGrounded(agent):
-    return len(agent.db_refs['EIDOS']) > 0
-
-def hasBothAgentsGrounded(statement):
-    return isGrounded(statement.subj) and isGrounded(statement.obj)
-
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
-
-    filename = args['<filename>']
-
-    if os.path.isfile(filename):
-        ep = eidos.process_json_ld_file(args['<filename>'])
-        statements = ep.statements
-    elif os.path.isdir(filename):
-        statements = flatMap(lambda f: eidos.process_json_ld_file(f).statements,
-                tqdm(glob(filename+'/*.jsonld')))
-    else:
-        print('The first argument does not seem to be a regular file or directory.')
-
-
-    if args['--show_grounded_only']:
-        statements = lfilter(lambda s: hasBothAgentsGrounded(s), statements)
-
-    gt = args['--grounding_threshold']
-    if gt is not None:
-        gt = float(gt)
-
-    app.state = add_statements(app.state, statements, grounding_threshold=gt)
-    app.run()
