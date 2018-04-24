43d56fd7210fdaea1558ff55cadc284d767def08
Adarsh <adarsh.pyarelal@gmail.com>
2018-04-24 14:27:12 -0700

    implemented functions to export CAG json, list of variables and default initial values, started working on CLI option for time step

--- delphi.py
+++ delphi.py
@@ -3,20 +3,30 @@
 import os
 import argparse
 from future.utils import lfilter
-from delphi.core import *
 from glob import glob
 import pickle
+from delphi.core import *
+
+def main():
+    with open('eval_indra_assembled.pkl', 'rb') as f:
+        sts = ltake(10, filter(isSimulable, pickle.load(f)))
+    CAG = add_conditional_probabilities(construct_CAG_skeleton(sts))
+    export_model_to_json(CAG)
+    # with open('CAG.pkl', 'wb') as f:
+        # export_model(CAG, f)
+    # with open('CAG.pkl', 'rb') as f:
+        # CAG = load_model(f)
+    # s0 = construct_default_initial_state(get_latent_state_components(CAG))
+    # seqs = sample_sequences(CAG, s0, 2, 1, args.dt)
+    # print(seqs)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Dynamic Bayes Net Executable Model')
-    parser.add_argument('--dt', metavar='Δt', help="Time step")
-    parser.parse_args()
-    # with open('eval_indra_assembled.pkl', 'rb') as f:
-        # sts = ltake(10, filter(isSimulable, pickle.load(f)))
-    # CAG = add_conditional_probabilities(construct_CAG_skeleton(sts))
-    # export_model(CAG)
-    CAG = load_model('CAG.pkl')
-    s0 = construct_default_initial_state(get_latent_state_components(CAG))
-    A=sample_transition_matrix(CAG)
-    seqs = sample_sequences(CAG, s0, 2, 1)
-    print(seqs)
+    parser = argparse.ArgumentParser(
+            description='Dynamic Bayes Net Executable Model')
+    parser.add_argument('dt', metavar='Δt', help="Time step size", type=float)
+    args = parser.parse_args()
+    if args.dt <= 0.:
+        print("Time step should be a positive real number.")
+    else:
+        main()
+
