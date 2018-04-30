f8dbee0109ab22aa4368a868fb3d2935eaabbbb1
Adarsh <adarsh.pyarelal@gmail.com>
2018-04-30 13:08:31 -0700

    cleaned up setup.py by removing some comments

--- setup.py
+++ setup.py
@@ -8,29 +8,18 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='delphi',
-
-    # Versions should comply with PEP 440:
-    # https://www.python.org/dev/peps/pep-0440/
-    #
-    # For a discussion on single-sourcing the version across setup.py and the
-    # project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.1',  # Required
-
-    description='A framework for assembling probabilistic models from text.',  # Required
-
+    version='2.0.1',
+    description='A framework for assembling probabilistic models from text.',
     url='https://ml4ai.github.io/delphi/',
     author='ML4AI',
     author_email='adarsh@email.arizona.edu',
-
-    classifiers=[  # Optional
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
     ],
-
     keywords='assembling models from text',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
@@ -47,15 +36,7 @@ setup(
 
     python_requires='>=3.6',
     setup_requires=['cython'],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
-    extras_require={  # Optional
+    extras_require={
         'dev': ['check-manifest'],
         'test': ['pytest', 'mypy'],
         'docs' : [
@@ -64,18 +45,5 @@ setup(
             'sphinx-autodoc-typehints'
             ]
     },
-
-    dependency_links=['git+https://github.com/sorgerlab/indra.git#egg=indra']
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-        # 'console_scripts': [
-            # 'rundelphi=delphi.commands:cli',
-        # ],
-    # },
+    dependency_links=['git+https://github.com/sorgerlab/indra.git#egg=indra'],
 )
