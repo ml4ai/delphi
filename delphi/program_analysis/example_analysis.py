5fe2f9fd904e5f3a1c725d59f7414bea1edae5b1
Clayton T. Morrison <claytonm@email.arizona.edu>
2018-05-22 09:32:27 -0700

    Sensitivity (#16)
    * adding sensitivity stub
    
    * added .idea to gitignore
    
    * adding crop_yield.py prog analysis target example
    
    * adding program_analysis with printAST
    
    * initial crop_yield_delphi.json
    
    * not sure why this needs to be added again (interaction of git cli and PyCharm?)
    
    * fixed some small format types (still have comments)
    
    * converted uppercase variables to lowercase variables for consistency and PEP8
    
    * Switched string interpolation method to f-string
    
    * Added type signatures to functions.
    
    * Switched to f-string string substitution
    
    * cosmetic change
    
    * Added main function guard
    
    * switched to path handling with Pathlib for cross-platform compatibility
    
    * Simplified target path finding with Pathlib
    
    * removed extra print call
    
    * linting
    
    * Changed to Google docstring style, added type annotations, moved typechecking to earlier in the function body
    
    * Reverted variable and function names to uppercase
    
    * READMEs added.
    
    * refactored imports, renamed function, moved side effect (printing) to main function, added type signature
    
    * updated dbn model to add conditional (if) and general cleanup
    
    * additional comments on conditional (if)
    
    * moving program_analysis inside delphi/
    
    * fixed paths to accommodate file location move
    
    * adding figure of example DBN graphical model represetnation for crop_yield.py toy model
    
    * Removing sensitivity stub for now

---
+++ delphi/program_analysis/example_analysis.py
@@ -0,0 +1,23 @@
+from printAst import importAst, dump
+import os
+from pathlib import Path
+
+
+def get_AST_str_rep(filepath: str) -> str:
+    """ Returns a string representation of the AST of a given Python source
+    file."""
+
+    try:
+        f = Path(filepath).resolve()
+    except FileNotFoundError:
+        pass
+
+    return dump(importAst(f), annotate_fields=True, include_attributes=True)
+
+
+if __name__ == '__main__':
+    module_name = 'crop_yield.py'
+    filename = (Path(__file__).parents[1]/'..'/'data'/
+            'program_analysis'/module_name).resolve()
+
+    print(get_AST_str_rep(str(filename)))
