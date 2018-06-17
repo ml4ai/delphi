a5e3626c8fa7df637d07adc0c8471a38a552843e
Adarsh <adarsh.pyarelal@gmail.com>
2018-06-16 23:06:08 -0700

    Added Indicator and Node dataclasses

--- delphi/types.py
+++ delphi/types.py
@@ -4,6 +4,7 @@ from indra.statements import Influence
 import networkx
 from pandas import Series
 import json
+from dataclasses import dataclass
 
 Delta = Dict[Optional[str], Optional[int]]
 GroupBy = pandas.core.groupby.DataFrameGroupBy
@@ -11,3 +12,14 @@ DiGraph = networkx.classes.digraph.DiGraph
 
 class CausalAnalysisGraph(DiGraph):
     pass
+
+
+@dataclass
+class Indicator:
+    name: str
+    initial_value: float = None
+
+@dataclass(frozen=True)
+class Node:
+    name: str
+    indicators: List[Indicator] = None
