0687309e7342b0f869b6b7aec18fa3310d9a4c09
Adarsh <adarsh.pyarelal@gmail.com>
2018-06-22 07:18:39 -0700

    concept_to_indicator_mappings text file extension changed from .yml to .txt

--- delphi/paths.py
+++ delphi/paths.py
@@ -3,4 +3,4 @@ from pathlib import Path
 data_dir = str((Path(__file__)/'../../data').resolve())
 adjectiveData = str((Path(data_dir)/'adjectiveData.tsv').resolve())
 south_sudan_data = str((Path(data_dir)/'south_sudan_data.csv').resolve())
-concept_to_indicator_mapping = str((Path(data_dir)/'concept_to_indicator_mapping.yml').resolve())
+concept_to_indicator_mapping = str((Path(data_dir)/'concept_to_indicator_mapping.txt').resolve())
