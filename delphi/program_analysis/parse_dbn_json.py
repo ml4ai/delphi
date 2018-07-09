68e4193060bb9c868b5b6199198c1a9f9e2def3a
Adarsh <adarsh.pyarelal@gmail.com>
2018-07-09 14:16:05 -0700

    graphviz start

--- delphi/program_analysis/parse_dbn_json.py
+++ delphi/program_analysis/parse_dbn_json.py
@@ -4,6 +4,7 @@ import pathlib
 from typing import Dict
 import os
 
+from pygraphviz import AGraph
 
 def read_dbn_from_json(dbn_json_source_file: str) -> Dict:
     """
@@ -24,5 +25,19 @@ if __name__ == '__main__':
                 'data' / 'program_analysis' / 'pa_crop_yield_v0.2').resolve()
     dbn_json_source_file = os.path.join(filepath, example_source_file)
     data = read_dbn_from_json(dbn_json_source_file)
-    print('DBN_JSON:')
-    pprint.pprint(data)
+    A = AGraph(directed=True)
+    A.node_attr['shape'] = 'rectangle'
+    for k in data['functions']:
+        if k.get('sources') is not None:
+            for s in k['sources']:
+                A.add_node(s, shape='ellipse')
+                A.add_edge(s, k['name'])
+        if k.get('target') is not None:
+            A.add_node(k['target'], shape='ellipse')
+            A.add_edge(k['name'], k['target'])
+    A.graph_attr['rankdir'] = 'LR'
+    A.node_attr['fontname'] = 'Gill Sans'
+    A.node_attr['style'] = 'rounded'
+    A.draw('graph.pdf', prog='dot')
+    # print('DBN_JSON:')
+    # pprint.pprint(data)
