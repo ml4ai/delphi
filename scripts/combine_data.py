32ae5302dec3273408ced435568916ae4538e9da
Adarsh <adarsh.pyarelal@gmail.com>
2018-07-08 18:11:39 -0700

    script to combined fao and wdi data completed (hard coded data directory path still present)

--- scripts/combine_data.py
+++ scripts/combine_data.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
-fao_data = '../data/south_sudan_data_fao.csv'
-df = pd.read_csv(fao_data, sep='|')
-print(df)
+data_dir = '../data/'
+fao_df = pd.read_csv(data_dir+'south_sudan_data_fao.csv', sep='|')
+wdi_df = pd.read_csv(data_dir+'south_sudan_data_wdi.csv', sep='|')
+
+fao_df['Indicator Name'] = fao_df['Element'] + ', ' + fao_df['Item']
+
+del fao_df['Element']
+del fao_df['Item']
+
+wdi_df['Unit'] = (wdi_df['Indicator Name'].str.partition('(')[2]
+                                          .str.partition(')')[0])
+wdi_df['Indicator Name'] = wdi_df['Indicator Name'].str.partition('(')[0]
+wdi_df = wdi_df.set_index('Indicator Name')
+fao_df = fao_df.pivot_table(values='Value', index=['Indicator Name', 'Unit'],
+                   columns = 'Year').reset_index().set_index('Indicator Name')
+combined_df = pd.concat([fao_df, wdi_df], sort=True)
+combined_df.to_csv(data_dir+'south_sudan_data.csv', sep='|')
