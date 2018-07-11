528b624fab3c1761d511b7bad5bd6756d20d60c5
Adarsh <adarsh.pyarelal@gmail.com>
2018-07-11 15:22:25 -0700

    Fixed failing tests

---
+++ delphi/tests/test_AnalysisGraph.py
@@ -0,0 +1,64 @@
+from indra.statements import Influence, Concept
+from delphi.types import Indicator
+from delphi.AnalysisGraph import AnalysisGraph
+
+conflict = Concept(
+    "conflict",
+    db_refs={
+        "TEXT": "conflict",
+        "UN": [("UN/events/human/conflict", 0.8), ("UN/events/crisis", 0.4)],
+    },
+)
+food_security = Concept(
+    "food_security",
+    db_refs={
+        "TEXT": "food security",
+        "UN": [("UN/entities/food/food_security", 0.8)],
+    },
+)
+precipitation = Concept("precipitation")
+flooding = Concept("flooding")
+
+s1 = Influence(
+    conflict,
+    food_security,
+    {"adjectives": ["large"], "polarity": 1},
+    {"adjectives": ["small"], "polarity": -1},
+)
+s2 = Influence(precipitation, food_security)
+s3 = Influence(precipitation, flooding)
+
+sts = [s1, s2, s3]
+G = AnalysisGraph.from_statements(sts)
+
+
+def test_get_subgraph_for_concept():
+    concept_of_interest = "food_security"
+    sg = G.get_subgraph_for_concept(concept_of_interest)
+    assert set(sg.nodes) == set(["conflict", "food_security"])
+
+
+def test_get_subgraph_for_concept_pair():
+    concept_pair = ("conflict", "food_security")
+    sg = G.get_subgraph_for_concept_pair(*concept_pair)
+    assert set(sg.nodes) == set(concept_pair)
+
+
+def test_map_concepts_to_indicators():
+    G.map_concepts_to_indicators()
+    indicator = Indicator(
+        name="Number of severely food insecure people Value",
+        source="FAO/WDI",
+        value=None,
+        stdev=None,
+        time=None,
+    )
+    assert G.nodes["food_security"]["indicators"][0] == indicator
+
+
+def test_infer_transition_model():
+    G.infer_transition_model()
+    assert (
+        len(G.get_edge_data("conflict", "food_security")["InfluenceStatements"])
+        == 1
+    )
--- delphi/tests/test_assembly.py
+++ delphi/tests/test_assembly.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 from indra.statements import Concept, Influence
-from delphi.api import *
 from delphi.assembly import *
 from delphi.paths import adjectiveData, south_sudan_data
 from future.utils import lfilter
@@ -68,12 +67,6 @@ def test_process_concept_name():
     assert process_concept_name("food_security") == "food security"
 
 
-cag = create_qualitative_analysis_graph(sts)
-
-def test_create_qualitative_analysis_graph():
-    assert set(cag.nodes()) == set(["conflict", "food_security", "precipitation"])
-    assert set(cag.edges()) == set([("conflict", "food_security"), ("precipitation", "food_security")])
-
 
 # Testing preassembly functions
 
@@ -104,7 +97,7 @@ def test_contains_relevant_concept():
     assert contains_relevant_concept(s1, relevant_concepts)
     assert not contains_relevant_concept(s3, relevant_concepts)
 
-faostat_data = get_faostat_wdi_data(south_sudan_data)
+faostat_data = get_data(south_sudan_data)
 
 @pytest.mark.skip(reason="Broken by changes for EC Hackathon - won't break other things")
 def test_get_indicators():
@@ -116,6 +109,6 @@ def test_get_indicators():
 
 def test_get_indicator_data():
     indicator = Indicator('Political stability and absence of violence/terrorism (index), Value',
-            'FAO')
+            'FAO/WDI')
     t = datetime(2012, 1, 1)
-    assert get_indicator_value(indicator, t, faostat_data) == -1.2
+    assert get_indicator_value(indicator, t, faostat_data)[0] == -1.2
