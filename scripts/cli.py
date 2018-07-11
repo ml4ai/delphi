6a931d499b4ec3628b4eddcf22c428f6d9e70f45
Adarsh <adarsh.pyarelal@gmail.com>
2018-07-11 12:58:42 -0700

    Moving delphi.py to scripts/cli.py, will move towards creating a command line entry point

--- scripts/cli.py
+++ scripts/cli.py
@@ -17,71 +17,35 @@ import delphi.core
 
 
 def create_model(args):
-    from delphi.export import to_json
-    from delphi.assembly import get_faostat_wdi_data
+    from delphi.assembly import get_data
     from delphi.api import (
-        parameterize,
         create_qualitative_analysis_graph,
-        add_transition_model,
-        add_indicators,
-        get_valid_statements_for_modeling
+        get_valid_statements_for_modeling,
     )
-    from delphi.utils import rcompose
     from datetime import datetime
-
-    parameterize_for_south_sudan = partial(
-        parameterize,
-        datetime(2012, 1, 1),
-        get_faostat_wdi_data("data/south_sudan_data.csv"),
-    )
-
-    create_cag = rcompose(
-        create_qualitative_analysis_graph,
-        partial(add_transition_model, args.adjective_data),
-        partial(add_indicators, 1),
-        parameterize_for_south_sudan,
-    )
 
     with open(args.indra_statements, "rb") as f:
         sts = get_valid_statements_for_modeling(pickle.load(f))
 
-    cag = create_cag(sts)
+    G = create_qualitative_analysis_graph(sts)
+    G.add_transition_model(args.adjective_data)
+    G.add_indicators()
+    G.parameterize(datetime(args.year, 1, 1), get_faostat_wdi_data(args.data))
 
-    return cag
-
-
-def export_model(cag, args):
-    import pickle
-    from delphi.export import to_json, export_default_variables
-
-    with open(args.output_dressed_cag, "wb") as f:
-        pickle.dump(cag, f)
-
-    to_json(cag, args.output_cag_json)
-    export_default_variables(cag, args)
+    return G
 
 
 def execute_model(args):
     from delphi.api import load
     from pandas import read_csv
-    from delphi.core import (
-        sample_sequence_of_latent_states,
-        write_sequences_to_file,
-    )
 
-    CAG = load(args.input_dressed_cag)
-    s0 = read_csv(
-        args.input_variables_path,
-        index_col=0,
-        header=None,
-        error_bad_lines=False,
-    )[1]
+    G = load(args.input_dressed_cag)
+    G.initialize(args.input_variables_path)
     latent_states = [
-        sample_sequence_of_latent_states(CAG, s0, args.steps, args.dt)
+        G.sample_sequence_of_latent_states(s0, args.steps, args.dt)
         for n in range(args.samples)
     ]
-    write_sequences_to_file(CAG, latent_states, args.output_sequences)
-
+    G._write_sequences_to_file(latent_states, args.output_sequences)
 
 
 def positive_real(arg, x):
@@ -116,68 +80,130 @@ def positive_int(arg, x):
 
 
 if __name__ == "__main__":
+    data_dir = Path(__file__).parents[0] / "data"
+
     parser = ArgumentParser(
         description="Dynamic Bayes Net Executable Model",
         formatter_class=ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_subparsers()
-    def add_arg(long_arg, help, argtype, default):
-        parser.add_argument(
-            "--" + long_arg, help=help, type=argtype, default=default
-        )
 
     def add_flag(short_arg: str, long_arg: str, help: str):
         parser.add_argument(
             "-" + short_arg, "--" + long_arg, help=help, action="store_true"
         )
 
-    add_flag("c", "create", "Create model")
-    add_flag("x", "execute", "Execute model")
+    subparsers = parser.add_subparsers()
 
-    data_dir = Path(__file__).parents[0] / "data"
+    parser_create = subparsers.add_parser("create", help="Model creation")
+    parser_execute = subparsers.add_parser("execute", help="Model execution")
 
-    add_arg(
-        "indra_statements",
-        "Pickle file containing INDRA statements",
-        str,
-        data_dir / "curated_statements.pkl",
+    #==========================================================================
+    # Model creation options
+    #==========================================================================
+
+    parser_create.add_argument(
+        "--output_dressed_cag",
+        help="Path to the output dressed cag",
+        type=str,
+        default="dressed_CAG.pkl",
     )
 
-    add_arg(
-        "adjective_data",
-        "Path to the gradable adjective data file.",
-        str,
-        data_dir / "adjectiveData.tsv",
+    parser_create.add_argument(
+        "--indra_statements",
+        type=str,
+        help="Pickle file containing INDRA statements",
+        default=data_dir / "curated_statements.pkl",
     )
 
-    add_arg("dt", "Time step size", partial(positive_real, "dt"), 1.0)
-    add_arg("steps", "Number of time steps to take", partial(positive_int, "steps"), 5,)
-    add_arg("samples", "Number of sequences to sample", partial(positive_int, "samples"), 100,)
-    add_arg("output_sequences", "Output file containing sampled sequences", str, "dbn_sampled_sequences.csv",)
-    add_arg("output_cag_json", "Path to the output CAG JSON file", str, "delphi_cag.json",)
-    add_arg("input_variables_path", "Path to the variables of the input dressed cag", str, "variables.csv",)
-    add_arg("output_variables_path", "Path to the variables of the output dressed cag", str, "variables.csv",)
-    add_arg("input_dressed_cag", "Path to the input dressed cag", str, "dressed_CAG.pkl",)
-    add_arg("output_dressed_cag", "Path to the output dressed cag", str, "dressed_CAG.pkl",)
-
-    add_arg(
-        "concept_to_indicator_mapping",
-        "Path to the YAML file containing the mapping between concepts and indicators.",
-        str,
-        data_dir / "concept_to_indicator_mapping.yml",
+    parser_create.add_argument(
+        "--adjective_data",
+        help="Path to the gradable adjective data file.",
+        type=str,
+        default=data_dir / "adjectiveData.tsv",
     )
 
-    add_arg(
-        "south_sudan_data",
-        "Path to the file containing the data for FAO and WDI indicators for South Sudan",
-        str,
-        data_dir / "south_sudan_data.csv",
+    parser_create.add_argument(
+        "--dt",
+        help="Time step size",
+        type=partial(positive_real, "dt"),
+        default=1.0,
     )
 
-    add_arg(
-        "year", "Year to get the indicator variable values for", str, "2012"
+    parser_create.add_argument(
+        "--output_variables",
+        help="Path to the variables of the output dressed cag",
+        type=str,
+        default="variables.csv",
     )
+
+    parser_create.add_argument(
+        "--concept_to_indicator_mapping",
+        help = "Path to the file containing the mapping between concepts and indicators.",
+        type=str,
+        default=data_dir / "concept_to_indicator_mapping.yml",
+    )
+
+    parser_create.add_argument(
+        "--data",
+        help="Path to the file containing the data for FAO and WDI indicators for South Sudan",
+        type=str,
+        default=data_dir / "south_sudan_data.csv",
+    )
+
+    parser_create.add_argument(
+        "--year",
+        help="Year to get the indicator variable values for",
+        type=int,
+        default=2012,
+    )
+
+    #==========================================================================
+    # Model execution options
+    #==========================================================================
+
+    parser_execute.add_argument(
+        "input_dressed_cag",
+        help="Path to the input dressed cag",
+        type=str,
+        default="dressed_CAG.pkl",
+    )
+
+    parser_execute.add_argument(
+        "--steps",
+        help="Number of time steps to take",
+        type=partial(positive_int, "steps"),
+        default=5,
+    )
+
+    parser_execute.add_argument(
+        "--samples",
+        help="Number of sequences to sample",
+        type=partial(positive_int, "samples"),
+        default=100,
+    )
+
+    parser_execute.add_argument(
+        "--output_sequences",
+        help="Output file containing sampled sequences",
+        type=str,
+        default="dbn_sampled_sequences.csv",
+    )
+
+    parser_execute.add_argument(
+        "--output_cag_json",
+        help="Path to the output CAG JSON file",
+        type=str,
+        default="delphi_cag.json",
+    )
+
+    parser_execute.add_argument(
+        "--input_variables",
+        help="Path to the variables of the input dressed cag",
+        type=str,
+        default="variables.csv",
+    )
+
 
     args = parser.parse_args()
 
@@ -185,8 +211,13 @@ if __name__ == "__main__":
         parser.print_help()
 
     if args.create:
-        cag = create_model(args)
-        export_model(cag, args)
+        G = create_model(args)
+        G.export(
+            format="full",
+            json_file=args.output_cag_json,
+            pickle_file=args.output_dressed_cag,
+            variables_file=args.output_variables_path,
+        )
 
     if args.execute:
         execute_model(args)
