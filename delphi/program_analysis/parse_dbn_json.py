c174327f6fdbd7aaa310b66b5da0faa15b0c7f72
Paul Hein <pauldhein@email.arizona.edu>
2018-07-13 14:54:39 -0400

    Resizing of demo output graph

--- delphi/program_analysis/parse_dbn_json.py
+++ delphi/program_analysis/parse_dbn_json.py
@@ -50,7 +50,7 @@ def setup_directed_graph():
     A.node_attr["shape"] = "rectangle"
     A.graph_attr["rankdir"] = "LR"
     A.node_attr["fontname"] = "Gill Sans"
-    A.graph_attr["size"] = "4,4"
+    A.graph_attr["size"] = "10,12"
     return A
 
 
