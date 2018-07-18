cb2c9856dd8fadd052a674d02d86b08e9be14afd
Adarsh <adarsh.pyarelal@gmail.com>
2018-07-18 12:21:05 -0700

    Fixed issues with missing imports, linting, renamed _to_json to to_json, added to_json_dict method

--- delphi/export.py
+++ delphi/export.py
@@ -2,16 +2,19 @@ import json
 from typing import Dict, Union, List
 import numpy as np
 import networkx as nx
-from .utils import _insert_line_breaks
+from .utils import _insert_line_breaks, lmap
+from functools import partial
 from .AnalysisGraph import AnalysisGraph
 from networkx import DiGraph
 from pygraphviz import AGraph
 import pickle
 from .execution import construct_default_initial_state
+from datetime import datetime
 
 # ==========================================================================
 # Export
 # ==========================================================================
+
 
 def _process_datetime(indicator_dict: Dict):
     time = indicator_dict.get("time")
@@ -25,7 +28,9 @@ def _export_edge(e):
         "target": e[1],
         "CPT": _construct_CPT(e),
         "polyfit": _get_polynomial_fit(e),
-        "InfluenceStatements": [s.to_json() for s in e[2]['InfluenceStatements']]
+        "InfluenceStatements": [
+            s.to_json() for s in e[2]["InfluenceStatements"]
+        ],
     }
 
 
@@ -35,6 +40,7 @@ def _get_units(n: str) -> str:
 
 def _get_dtype(n: str) -> str:
     return "real"
+
 
 def _construct_CPT(e, res=100):
     kde = e[2]["ConditionalProbability"]
@@ -51,6 +57,7 @@ def _get_polynomial_fit(e, deg=7, res=100):
     Y = kde.evaluate(X) * (X[1] - X[0])
     coefs = np.polynomial.polynomial.polyfit(X, Y, deg=deg)
     return {"degree": deg, "coefficients": list(coefs)}
+
 
 def to_agraph(G: DiGraph, *args, **kwargs) -> AGraph:
     """ Exports AnalysisGraph to pygraphviz AGraph
@@ -84,28 +91,23 @@ def to_agraph(G: DiGraph, *args, **kwargs) -> AGraph:
     )
 
     nodes_with_indicators = [
-        n
-        for n in G.nodes(data=True)
-        if n[1].get("indicators") is not None
+        n for n in G.nodes(data=True) if n[1].get("indicators") is not None
     ]
 
     n_max = 0
     for e in G.edges(data=True):
-        n = len(e[2]['InfluenceStatements'])
+        n = len(e[2]["InfluenceStatements"])
         if n > n_max:
             n_max = n
 
     color_str = "#650021"
     for e in G.edges(data=True):
-        opacity = len(e[2]['InfluenceStatements'])/n_max
-        h = (opacity*255).hex()
-        c_str=color_str+h[4:6]
+        opacity = len(e[2]["InfluenceStatements"]) / n_max
+        h = (opacity * 255).hex()
+        c_str = color_str + h[4:6]
         A.add_edge(
-                e[0].capitalize(),
-                e[1].capitalize(),
-                color=c_str,
-                arrowsize=0.5,
-                )
+            e[0].capitalize(), e[1].capitalize(), color=c_str, arrowsize=0.5
+        )
 
     # Drawing indicator variables
 
@@ -114,42 +116,39 @@ def to_agraph(G: DiGraph, *args, **kwargs) -> AGraph:
             for ind in n[1]["indicators"]:
                 node_label = _insert_line_breaks(ind.name)
                 A.add_node(
-                    node_label,
-                    style="rounded, filled",
-                    fillcolor="lightblue",
+                    node_label, style="rounded, filled", fillcolor="lightblue"
                 )
-                A.add_edge(n[0].capitalize(), node_label, color='royalblue4')
+                A.add_edge(n[0].capitalize(), node_label, color="royalblue4")
 
     # Drawing indicator values
     if kwargs.get("indicator_values"):
         for n in nodes_with_indicators:
-            indicators = [
-                i for i in n[1]["indicators"] if i.value is not None
-            ]
+            indicators = [i for i in n[1]["indicators"] if i.value is not None]
             for ind in indicators:
                 if ind.unit is not None:
-                    units = f' {ind.unit}'
+                    units = f" {ind.unit}"
                 else:
-                    units=''
+                    units = ""
                 ind_label = _insert_line_breaks(ind.name)
 
-                node_label = '{:.2f}'.format(ind.value) + units
+                node_label = "{:.2f}".format(ind.value) + units
                 A.add_node(
                     node_label,
-                    shape='plain',
+                    shape="plain",
                     # style="rounded, filled",
                     fillcolor="white",
                     color="royalblue",
                 )
-                A.add_edge(ind_label, node_label, color='lightblue',
-                        arrowhead='none')
+                A.add_edge(
+                    ind_label, node_label, color="lightblue", arrowhead="none"
+                )
 
     if kwargs.get("nodes_to_highlight") is not None:
         for n in kwargs["nodes_to_highlight"]:
             A.add_node(n.capitalize(), fontcolor="royalblue")
 
-    if kwargs.get('graph_label') is not None:
-        A.graph_attr['label'] = kwargs['graph_label']
+    if kwargs.get("graph_label") is not None:
+        A.graph_attr["label"] = kwargs["graph_label"]
 
     return A
 
@@ -180,6 +179,7 @@ def export_node(G: AnalysisGraph, n) -> Dict[str, Union[str, List[str]]]:
 
     return node_dict
 
+
 def export(
     G: AnalysisGraph,
     format="full",
@@ -198,7 +198,7 @@ def export(
     """
 
     if format == "full":
-        _to_json(G, json_file)
+        to_json(G, json_file)
         _pickle(G, pickle_file)
         export_default_initial_values(G, variables_file)
 
@@ -206,26 +206,29 @@ def export(
         return to_agraph(G)
 
     if format == "json":
-        _to_json(G, json_file)
+        to_json(G, json_file)
+
 
 def _pickle(G: AnalysisGraph, filename: str):
     with open(filename, "wb") as f:
         pickle.dump(G, f)
 
-def _to_json(G: AnalysisGraph, filename: str):
-    """ Export the CAG to JSON """
+
+def to_json(G: AnalysisGraph, filename: str):
     with open(filename, "w") as f:
-        json.dump(
-            {
-                "name": G.name,
-                "dateCreated": str(datetime.now()),
-                "variables": lmap(partial(export_node, G), G.nodes(data=True)),
-                "timeStep": str(G.Δt),
-                "edge_data": lmap(_export_edge, G.edges(data=True)),
-            },
-            f,
-            indent=2,
-        )
+        json.dump(to_json_dict(G), f, indent=2)
+
+
+def to_json_dict(G: AnalysisGraph):
+    """ Export the CAG to JSON """
+    return {
+        "name": G.name,
+        "dateCreated": str(datetime.now()),
+        "variables": lmap(partial(export_node, G), G.nodes(data=True)),
+        "timeStep": str(G.Δt),
+        "edge_data": lmap(_export_edge, G.edges(data=True)),
+    }
+
 
 def export_default_initial_values(G: AnalysisGraph, variables_file: str):
     s0 = construct_default_initial_state(G)
