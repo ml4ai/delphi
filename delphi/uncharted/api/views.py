ae290f3ebf55daf7ff2dafdfd91a5affd0c4a8fb
Adarsh <adarsh.pyarelal@gmail.com>
2018-09-25 20:07:05 -0700

    Added view to get ICM metadata

--- delphi/uncharted/api/views.py
+++ delphi/uncharted/api/views.py
@@ -1,4 +1,10 @@
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_GET
+from rest_framework.response import Response
+from delphi.AnalysisGraph import AnalysisGraph
+from delphi.export import to_json_dict
+
+from api.serializers import *
 
 from os import environ
 from os.path import join
@@ -6,6 +12,25 @@ from os.path import join
 PORT_NUMBER = environ.get("PORT", 8080)
 HOST_ID = environ.get('HOST_ID', 'localhost')
 
-def get_delphi_model_ids(request):
-    if request.method=='GET':
-        return HttpResponse('blah')
+list_of_icm_ids = [1, 2, 3, 4]
+
+G = AnalysisGraph.from_pickle('../../delphi_cag.pkl')
+model_dict = {'1': G, '2': G, '3': G, '4': G}
+
+@require_GET
+def icm(request):
+    return JsonResponse(list_of_icm_ids, safe=False)
+
+@require_GET
+def icm_uuid(request, uuid):
+    model = model_dict[uuid]
+    icm_metadata = ICMMetadata(id = model.id)
+    serializer = ICMMetadataSerializer(icm_metadata)
+    return JsonResponse(serializer.data)
+
+@require_GET
+def icm_uuid_primitive(request, uuid):
+    return JsonResponse(list(G.nodes)+list(G.edges), safe=False)
+
+if __name__=='__main__':
+    print('ok')
