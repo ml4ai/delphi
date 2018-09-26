994c931f421fa5279d65fd931ea1eb2b092eba4f
Adarsh <adarsh.pyarelal@gmail.com>
2018-09-25 20:05:50 -0700

    Added models for CausalVariable and ICMMetadata

--- delphi/uncharted/api/models.py
+++ delphi/uncharted/api/models.py
@@ -1,9 +1,18 @@
 import uuid
 from django.db import models
 
+class CausalVariable(models.Model):
+    basetype = models.CharField(max_length=30, default="string")
+    editable = models.BooleanField(default=True)
+    id = models.CharField(max_length=100, default="", primary_key=True)
+    label = models.CharField(max_length=100, default="")
+    description = models.CharField(max_length=300, default="")
+    lastUpdated = models.DateTimeField(auto_now=True)
+    units = models.CharField(max_length=100, default="")
+
 class ICMMetadata(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    icmProvider = models.CharField(max_length=100, default="DUMMY")
+    id = models.UUIDField(primary_key=True, editable=False)
+    icmProvider = models.CharField(max_length=100, default="UAZ")
     title = models.CharField(max_length=100, default="DelphiModel")
-    version = models.IntegerField()
+    version = models.IntegerField(default=1)
     created = models.DateTimeField(auto_now=True)
