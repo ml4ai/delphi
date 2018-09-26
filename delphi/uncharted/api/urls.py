094dfc41c13f4ef6e7c6f516505e2724ad8730b1
Adarsh <adarsh.pyarelal@gmail.com>
2018-09-25 20:06:47 -0700

    Added urls for icm/<uuid> and icm/<uuid>/primitive

--- delphi/uncharted/api/urls.py
+++ delphi/uncharted/api/urls.py
@@ -6,12 +6,13 @@ urlpatterns = [
 
     # <server_address> as per above, on localhost via port 8080 
 
-    path('icm/', views.get_delphi_model_ids, name="DelphiModels"),
+    path('icm/', views.icm),
+    path('icm/<uuid>/', views.icm_uuid),
+    path('icm/<uuid>/primitive', views.icm_uuid_primitive),
     # this would call the function get_Delphi_model_ids defined in views.py
     # output would be available at http://<server_address>/icm
 
-    # path(r'^icm/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{'
-        # r'12})/primitive', name="DelphiPrimitives"),
+    # path(r'^icm/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{' r'12})/primitive', name="DelphiPrimitives"),
     # example of more realistic path as per ICM API, with regex for a legitimate model uuid
     # path would be: http://<server_address>/icm/<uuid>/primitive
 
