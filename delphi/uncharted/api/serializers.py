2f30d391f45a7be786c00337d31185f9ec33871a
Adarsh <adarsh.pyarelal@gmail.com>
2018-09-25 20:06:09 -0700

    Added serializer for CausalVariable

--- delphi/uncharted/api/serializers.py
+++ delphi/uncharted/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from api.models import ICMMetadata
+from api.models import *
 
 class ICMMetadataSerializer(serializers.ModelSerializer):
     class Meta:
         model = ICMMetadata
+        fields = '__all__'
+
+class CausalVariableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CausalVariable
         fields = '__all__'
