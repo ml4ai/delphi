c870f1f15147929f1ee2cc4ef26f9b52b3fb0baa
Adarsh Pyarelal <adarsh.pyarelal@gmail.com>
2018-10-02 14:09:51 -0700

    ICM autogenerator improvements (#77)
    * Moving sphinx stuff to developer section of Pipfile
    
    * bugfixes
    
    * renaming

--- delphi/icm_api/icm_api_skeleton_generator.py
+++ delphi/icm_api/icm_api_skeleton_generator.py
@@ -2,105 +2,156 @@
 YAML specification. """
 
 from ruamel.yaml import YAML
+from typing import List
 
-yaml = YAML(typ="safe")
-with open("icm_api.yml", "r") as f:
-    yml = yaml.load(f)
 
-lines = []
-
-lines.append(
-    """\
-import uuid
-import pickle
-from datetime import datetime
+def write_models(yml):
+    global_schema_list = []
+    schemas = yml["components"]["schemas"]
+    schema_lines_dict = {}
+    module_lines = []
+    module_lines.append(
+        """\
 from enum import Enum, unique
 from typing import Optional, List
-from dataclasses import dataclass, field, asdict
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)"""
-)
-
-
-def process_properties(schema_name, schema, lines, class_declaration):
-    lines.append("\n\n@dataclass")
-    lines.append(class_declaration)
-    lines.append(
-        '    """'
-        + f"{schema.get('description', f'Placeholder docstring for class {schema_name}.')}"
-        + ' """'
+from dataclasses import dataclass, field, asdict"""
     )
-    properties = schema["properties"]
-    required_properties = schema.get("required", [])
 
-    for property in properties:
-        property_type = properties[property].get(
-            "type", properties[property].get("$ref", "").split("/")[-1]
+    def process_properties(
+        schema_name, schema, class_lines, class_declaration
+    ):
+        class_lines.append("\n\n@dataclass")
+        class_lines.append(class_declaration)
+        class_lines.append(
+            '    """'
+            + f"{schema.get('description', f'Placeholder docstring for class {schema_name}.')}"
+            + ' """'
         )
-        mapping = {
-            "string": "str",
-            "integer": "int",
-            "None": "",
-            "array": "List",
-            "boolean": "bool",
-            "number": "float",
-            "object": "object",
-        }
-        type_annotation = mapping.get(property_type, property_type)
-        if type_annotation == "List":
-            property_type = properties[property]["items"]["type"]
-            type_annotation = (
-                f"List[{mapping.get(property_type, property_type)}]"
+        properties = schema["properties"]
+        required_properties = schema.get("required", [])
+
+        for property in sorted(
+            properties, key=lambda x: x not in required_properties
+        ):
+            property_ref = properties[property].get("$ref", "").split("/")[-1]
+            if property_ref != "" and property_ref not in global_schema_list:
+                global_schema_list.append(property_ref)
+            property_type = properties[property].get("type", property_ref)
+            mapping = {
+                "string": "str",
+                "integer": "int",
+                "None": "",
+                "array": "List",
+                "boolean": "bool",
+                "number": "float",
+                "object": "object",
+            }
+            type_annotation = mapping.get(property_type, property_type)
+            if type_annotation == "List":
+                property_type = properties[property]["items"]["type"]
+                type_annotation = (
+                    f"List[{mapping.get(property_type, property_type)}]"
+                )
+            if property not in required_properties:
+                type_annotation = f"Optional[{type_annotation}]"
+            class_lines.append(f"    {property}: {type_annotation} = None")
+
+    def to_class(schema_name, schema):
+        class_lines = []
+        if schema.get("type") == "object":
+            class_declaration = f"class {schema_name}:"
+            process_properties(
+                schema_name, schema, class_lines, class_declaration
             )
-        if property not in required_properties:
-            type_annotation = f"Optional[{type_annotation}]"
-        lines.append(f"    {property}: {type_annotation}")
+        elif schema.get("allOf") is not None:
+            parents = [
+                item["$ref"].split("/")[-1]
+                for item in schema["allOf"]
+                if item.get("$ref")
+            ]
+            schema = schema["allOf"][1]
+            class_declaration = f"class {schema_name}({','.join(parents)}):"
+            process_properties(
+                schema_name, schema, class_lines, class_declaration
+            )
+        elif "enum" in schema:
+            class_lines.append("\n\n@unique")
+            class_lines.append(f"class {schema_name}(Enum):")
+            for option in schema["enum"]:
+                class_lines.append(f"    {option} = '{option}'")
+        schema_lines_dict[schema_name] = class_lines
+        if schema_name not in global_schema_list:
+            global_schema_list.append(schema_name)
+
+    for schema_name, schema in schemas.items():
+        to_class(schema_name, schema)
+
+    for schema_name in global_schema_list:
+        module_lines += schema_lines_dict[schema_name]
+    with open("models.py", "w") as f:
+        f.write("\n".join(module_lines))
 
 
-schemas = yml["components"]["schemas"]
-
-for schema_name, schema in schemas.items():
-    if schema.get("type") == "object":
-        class_declaration = f"class {schema_name}:"
-        process_properties(schema_name, schema, lines, class_declaration)
-    elif schema.get("allOf") is not None:
-        parents = [
-            item["$ref"].split("/")[-1]
-            for item in schema["allOf"]
-            if item.get("$ref")
-        ]
-        schema = schema["allOf"][1]
-        class_declaration = f"class {schema_name}({','.join(parents)}):"
-        process_properties(schema_name, schema, lines, class_declaration)
-    elif "enum" in schema:
-        lines.append("@unique")
-        lines.append(f"class {schema_name}(Enum):")
-        for option in schema["enum"]:
-            lines.append(f"    {option} = '{option}'")
-
-paths = yml["paths"]
-
-for url, metadata in paths.items():
+def construct_view_lines(url, metadata) -> List[str]:
+    lines = []
     parameters = metadata.pop("parameters", None)
     for http_method in metadata:
         modified_path = url.replace("{", "<string:").replace("}", ">")
         lines.append(
             f"\n\n@app.route('{modified_path}', methods=['{http_method.upper()}'])"
         )
-        args = ', '.join([part[1:-1]+": str" for part in url.split('/') if
-            part.startswith('{')])
-        lines.append(f"def {metadata[http_method]['operationId']}({args}):")
+        args = ", ".join(
+            [
+                part[1:-1] + ": str"
+                for part in url.split("/")
+                if part.startswith("{")
+            ]
+        )
+        operationId = metadata[http_method]["operationId"]
+        lines.append(f"def {operationId}({args}):")
         lines.append(
             '    """' + f" {metadata[http_method]['summary']}" + '"""'
         )
         lines.append("    pass")
+    return lines
 
-lines.append(
-    """\
+
+def write_views(yml):
+    paths = yml["paths"]
+
+    views_lines = []
+    views_lines.append(
+        "\n".join(
+            [
+                "import uuid",
+                "import pickle",
+                "from datetime import datetime",
+                "from typing import Optional, List",
+                "from flask import Flask, jsonify, request",
+                "from delphi.icm_api.models import *",
+                "app = Flask(__name__)",
+            ]
+        )
+    )
+
+    for url, metadata in paths.items():
+        views_lines += construct_view_lines(url, metadata)
+
+    views_lines.append(
+        """\
+
 if __name__ == "__main__":
     app.run(debug=True)"""
-)
+    )
 
-with open("icm_api_autogenerated_skeleton.py", "w") as f:
-    f.write("\n".join(lines))
+    with open("views.py", "w") as f:
+        f.write("\n".join(views_lines))
+
+
+if __name__ == "__main__":
+    yaml = YAML(typ="safe")
+    with open("icm_api.yml", "r") as f:
+        yml = yaml.load(f)
+
+    write_views(yml)
+    write_models(yml)
