bc1a8a5796d338c60ba94b4ddeadee1f896a62d2
Adarsh <adarsh.pyarelal@gmail.com>
2018-10-06 15:23:52 -0700

    Adding functionality to unzip FEWS NET data zipfile

--- scripts/download_and_clean_FEWS_NET_data.py
+++ scripts/download_and_clean_FEWS_NET_data.py
@@ -1,10 +1,16 @@
+import os
+import zipfile
+from pathlib import Path
 from delphi.utils import download_file, cd
 from delphi.paths import data_dir
-from pathlib import Path
 
 def download_FEWS_NET_data():
     url = "http://shapefiles.fews.net.s3.amazonaws.com/ALL_HFIC.zip"
-    download_file(url, Path(data_dir) / url.split('/')[-1])
+    zipfile_name = Path(data_dir) / url.split('/')[-1]
+    FEWS_NET_data_dir = Path(data_dir) / "FEWS_NET"
+    download_file(url, zipfile_name)
+    with zipfile.ZipFile(zipfile_name) as zf:
+        zf.extractall(data_dir)
 
 if __name__ == '__main__':
     download_FEWS_NET_data()
