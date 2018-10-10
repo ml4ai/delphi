4474decce9fc3739c77abf4e2ae04abb27c3ee74
Adarsh <adarsh.pyarelal@gmail.com>
2018-10-10 12:00:02 -0700

    Script updated to create FEWS NET IPC data table from shapefiles

--- scripts/download_and_clean_FEWS_NET_data.py
+++ scripts/download_and_clean_FEWS_NET_data.py
@@ -2,19 +2,21 @@ import os
 import zipfile
 import calendar
 from pathlib import Path
+from glob import glob
 import shapefile
 from future.utils import lzip
 from delphi.utils import download_file, cd
 from delphi.paths import data_dir
 from tqdm import tqdm
 import matplotlib as mpl
+import pandas as pd
 
 mpl.rcParams["backend"] = "Agg"
 from matplotlib import pyplot as plt
 from shapely.geometry import Polygon, MultiPolygon
 
 
-def download_FEWS_NET_admin_boundaries_data():
+def download_FEWSNET_admin_boundaries_data():
     url = "http://shapefiles.fews.net.s3.amazonaws.com/ADMIN/FEWSNET_World_Admin.zip"
     zipfile_name = Path(data_dir) / url.split("/")[-1]
     download_file(url, zipfile_name)
@@ -24,16 +26,19 @@ def download_FEWS_NET_admin_boundaries_data():
         zf.extractall(directory)
 
 
-def download_FEWS_NET_IPC_data():
+def download_and_clean_FEWSNET_IPC_data():
     url = "http://shapefiles.fews.net.s3.amazonaws.com/ALL_HFIC.zip"
     zipfile_name = Path(data_dir) / url.split("/")[-1]
     download_file(url, zipfile_name)
     with zipfile.ZipFile(zipfile_name) as zf:
         zf.extractall(data_dir)
+    with cd(str(Path(data_dir) / "ALL_HFIC" / "East Africa")):
+        files_to_rename = glob("EA2017*")
+        for f in files_to_rename:
+            os.rename(f, f.replace("EA", "EA_"))
 
 
-def process_FEWS_NET_IPC_data(shpfile: str, title: str):
-    # read the shapefile
+def process_FEWSNET_IPC_data(shpfile: str, title: str):
     admin_boundaries_shapefile = "data/FEWSNET_World_Admin/FEWSNET_Admin2"
     sf_admin = shapefile.Reader(admin_boundaries_shapefile)
     colors = {
@@ -48,22 +53,25 @@ def process_FEWS_NET_IPC_data(shpfile: str, title: str):
         99: "white",
     }
     sf = shapefile.Reader(shpfile)
+
     fig, ax = plt.subplots(figsize=(12, 12))
     ax.set_aspect("equal")
     ax.set_title(title)
     plt.style.use("ggplot")
 
-    def fill_and_plot(points, sr):
+    def fill_and_plot(points, color_code):
         xs, ys = lzip(*points)
         ax.plot(xs, ys, linewidth=0.5, color="grey")
-        ax.fill(xs, ys, color=colors[int(sr.record[0])])
+        ax.fill(xs, ys, color=colors[color_code])
 
     fs_polygons = []
+
     for i, sr in tqdm(enumerate(sf.shapeRecords())):
         nparts = len(sr.shape.parts)
         parts, points = sr.shape.parts, sr.shape.points
+        CS = int(sr.record[0])
         if nparts == 1:
-            # fill_and_plot(points, sr)
+            # fill_and_plot(points, CS)
             fs_polygons.append((Polygon(points), int(sr.record[0])))
         else:
             for ip, part in enumerate(parts):
@@ -71,13 +79,17 @@ def process_FEWS_NET_IPC_data(shpfile: str, title: str):
                     i1 = parts[ip + 1] - 1
                 else:
                     i1 = len(points)
-                # fill_and_plot(points[part : i1 + 1], sr),
+                # fill_and_plot(points[part : i1 + 1], CS),
                 fs_polygons.append(
                     (Polygon(points[part : i1 + 1]), int(sr.record[0]))
                 )
+
     south_sudan_srs = [
         sr for sr in sf_admin.shapeRecords() if sr.record[3] == "South Sudan"
     ]
+
+    lines = []
+
     for sr in tqdm(south_sudan_srs, desc="South Sudan Counties"):
         county_polygon = Polygon(sr.shape.points)
         for fs_polygon in tqdm(fs_polygons, desc="fs_polygons"):
@@ -91,25 +103,89 @@ def process_FEWS_NET_IPC_data(shpfile: str, title: str):
                     horizontalalignment="center",
                 )
                 xs, ys = lzip(*sr.shape.points)
-                ax.plot(xs, ys, linewidth=0.5, color="grey")
-                ax.fill(
-                    xs, ys, linewidth=0.5, color=colors[int(fs_polygon[1])]
+                CS = int(fs_polygon[1])
+                fill_and_plot(sr.shape.points, CS)
+                lines.append(
+                    "\t".join([str(x) for x in sr.record] + [str(CS)])
                 )
-                break
+
+    with open("ipc_data.tsv", "w") as f:
+        f.write("\n".join(lines))
 
     plt.savefig("shape.pdf")
 
 
+def get_polygons(shape):
+    parts, points = shape.parts, shape.points
+    nparts = len(parts)
+    polygons = []
+
+    if nparts == 1:
+        polygons.append(Polygon(points))
+    else:
+        for ip, part in enumerate(parts):
+            if ip < nparts - 1:
+                i1 = parts[ip + 1] - 1
+            else:
+                i1 = len(points)
+            polygons.append(Polygon(points[part : i1 + 1]))
+
+    return polygons
+
+
+def create_food_security_data_table(region: str, country: str):
+    admin_boundaries_shapefile = str(
+        Path(data_dir) / "FEWSNET_World_Admin" / "FEWSNET_Admin2"
+    )
+    sf_admin = shapefile.Reader(admin_boundaries_shapefile)
+    south_sudan_srs = [
+        x for x in sf_admin.shapeRecords() if x.record[3] == country
+    ]
+
+    path = str(Path(data_dir) / "ALL_HFIC" / region)
+    ipc_records = []
+    with cd(path):
+        shapefiles = glob("*.shp")
+        for filename in tqdm(shapefiles, unit="shapefile"):
+            year = int(filename[3:7])
+            month = int(filename[7:9])
+            reader = shapefile.Reader(filename)
+            for i, fs_sr in tqdm(
+                enumerate(reader.shapeRecords()),
+                unit="Food security shapeRecord",
+            ):
+                parts, points = fs_sr.shape.parts, fs_sr.shape.points
+                nparts = len(parts)
+                CS = int(fs_sr.record[0])
+                fs_polygons = get_polygons(fs_sr.shape)
+
+                for sr in tqdm(
+                    south_sudan_srs, desc=f"{country} Counties", ncols=80
+                ):
+                    county_polygon = Polygon(sr.shape.points)
+                    for fs_polygon in tqdm(
+                        fs_polygons, unit="Food security polygon", ncols=80
+                    ):
+                        if county_polygon.buffer(-0.05).intersects(fs_polygon):
+                            ipc_records.append(
+                                {
+                                    "Country": sr.record[3],
+                                    "State": sr.record[4],
+                                    "County": sr.record[8],
+                                    "Year": year,
+                                    "Month": month,
+                                    "IPC Phase": CS,
+                                }
+                            )
+    df = pd.DataFrame(ipc_records)
+    df.to_csv(Path(data_dir) / "ipc_data.tsv", sep="\t")
+
+
 if __name__ == "__main__":
-    # download_FEWS_NET_IPC_data()
-    # download_FEWS_NET_admin_boundaries_data()
     region = "East Africa"
     year = 2018
     month = 2
-    process_month = (
-        lambda month: "0" + str(month) if len(str(month)) < 2 else str(month)
-    )
-    month_str = process_month(month)
+    month_str = "0" + str(month) if len(str(month)) < 2 else str(month)
     region_str = "".join([x[0] for x in region.split()])
     region_year_str = (
         f"{region_str}{year}" if year == 2017 else f"{region_str}_{year}"
@@ -121,4 +197,5 @@ if __name__ == "__main__":
             f"{calendar.month_name[month]} {year}",
         )
     )
-    process_FEWS_NET_IPC_data(shpfile, title)
+    # process_FEWSNET_IPC_data(shpfile, title)
+    create_food_security_data_table("East Africa", "South Sudan")
