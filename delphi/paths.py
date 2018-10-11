229d407571c405264f92c34433d7ba7725bd7a42
Paul Hein <pauldhein@email.arizona.edu>
2018-10-11 15:18:00 -0700

    Using a config file for paths to external repos and data

--- delphi/paths.py
+++ delphi/paths.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import json
 
 data_dir = str((Path(__file__) / "../../data").resolve())
 adjectiveData = str((Path(data_dir) / "adjectiveData.tsv").resolve())
@@ -6,3 +7,5 @@ south_sudan_data = str((Path(data_dir) / "south_sudan_data.csv").resolve())
 concept_to_indicator_mapping = str(
     (Path(data_dir) / "concept_to_indicator_mapping.txt").resolve()
 )
+
+config_paths = json.load(open("config.json"))
