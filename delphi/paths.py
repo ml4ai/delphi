0bf7b665524a0c5b151607845505fca401fbcf1a
Adarsh <adarsh.pyarelal@gmail.com>
2018-10-16 15:38:30 -0700

    Added DELPHI_DATA env variable to paths and .travis.yml

--- delphi/paths.py
+++ delphi/paths.py
@@ -2,10 +2,7 @@ import os
 from pathlib import Path
 import json
 
-with open("../delphi_config.json", "r") as f:
-    config = json.load(f)
-
-data_dir = str(Path(config["delphi_data_path"].replace("~", os.path.expanduser("~"))).resolve())
+data_dir = str(Path(os.environ["DELPHI_DATA"]).resolve())
 adjectiveData = str((Path(data_dir) / "adjectiveData.tsv").resolve())
 south_sudan_data = str((Path(data_dir) / "south_sudan_data.csv").resolve())
 concept_to_indicator_mapping = str(
