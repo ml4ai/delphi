cd2c3a5b3078eb024e435bd05189d07893501c02
Adarsh <adarsh.pyarelal@gmail.com>
2018-10-16 16:38:23 -0700

    Removed stringification in paths.py

--- delphi/paths.py
+++ delphi/paths.py
@@ -2,9 +2,8 @@ import os
 from pathlib import Path
 import json
 
-data_dir = str(Path(os.environ["DELPHI_DATA"]).resolve())
-adjectiveData = str((Path(data_dir) / "adjectiveData.tsv").resolve())
-south_sudan_data = str((Path(data_dir) / "south_sudan_data.csv").resolve())
-concept_to_indicator_mapping = str(
-    (Path(data_dir) / "concept_to_indicator_mapping.txt").resolve()
-)
+data_dir = Path(os.environ["DELPHI_DATA"])
+
+adjectiveData = data_dir / "adjectiveData.tsv"
+south_sudan_data = data_dir / "south_sudan_data.csv"
+concept_to_indicator_mapping = data_dir / "concept_to_indicator_mapping.txt"
