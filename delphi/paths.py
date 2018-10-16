c69bffc614766afa27a57a9ab180d13241cda967
Paul Hein <pauldhein@email.arizona.edu>
2018-10-16 13:27:22 -0700

    updated data_path to delphi_data_path

--- delphi/paths.py
+++ delphi/paths.py
@@ -5,7 +5,7 @@ import json
 with open("../delphi_config.json", "r") as f:
     config = json.load(f)
 
-data_dir = str(Path(config["data_path"].replace("~", os.path.expanduser("~"))).resolve())
+data_dir = str(Path(config["delphi_data_path"].replace("~", os.path.expanduser("~"))).resolve())
 adjectiveData = str((Path(data_dir) / "adjectiveData.tsv").resolve())
 south_sudan_data = str((Path(data_dir) / "south_sudan_data.csv").resolve())
 concept_to_indicator_mapping = str(
