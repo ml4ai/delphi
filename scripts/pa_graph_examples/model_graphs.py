6def3f12a24f741b2b4df7244c806af687092435
Paul Hein <pauldhein@email.arizona.edu>
2018-10-19 15:03:55 -0700

    reverted to dotfile/csv solution for PA models

--- scripts/pa_graph_examples/model_graphs.py
+++ scripts/pa_graph_examples/model_graphs.py
@@ -22,7 +22,7 @@ def draw_graph(G: nx.DiGraph, filename: str):
 
 
 if __name__ == "__main__":
-    asce = nx_graph_from_dotfile(str(data_dir/"program_analysis"/"asce-graph.dot"))
-    priestley_taylor = nx_graph_from_dotfile(str(data_dir/"program_analysis"/"priestley-taylor-graph.dot"))
+    asce = nx_graph_from_dotfile(str(data_dir/"program_analysis"/"pa_graph_examples"/"asce-graph.dot"))
+    priestley_taylor = nx_graph_from_dotfile(str(data_dir/"program_analysis"/"pa_graph_examples"/"priestley-taylor-graph.dot"))
     draw_graph(asce, "asce-graph.pdf")
     draw_graph(priestley_taylor, "priestley-taylor.pdf")
