a22e71e4d96d353ad0d8438a17653f9dd686b455
Adarsh <adarsh.pyarelal@gmail.com>
2018-10-21 10:11:23 -0700

    Added config.py

---
+++ delphi/icm_api/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = Path(__file__)
+
+# Define the database - we are working with
+# SQLite for this example
+SQLALCHEMY_DATABASE_URI = 'sqlite://' / BASE_DIR / 'app.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_CONNECT_OPTIONS = {}
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
