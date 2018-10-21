6edc5fb9e3de972a26b5bfb16db757b43133ee98
Adarsh <adarsh.pyarelal@gmail.com>
2018-10-20 19:00:14 -0700

    Removing sqlalchemy stuff from __init__.py

--- delphi/icm_api/__init__.py
+++ delphi/icm_api/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 
 def create_app():
     from delphi.icm_api.api import bp
+
     app = Flask(__name__)
     db.init_app(app)
     app.register_blueprint(bp)
