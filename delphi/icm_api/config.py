cb99d494c81169ca533d262bc1bf93cc19227d24
Adarsh <adarsh.pyarelal@gmail.com>
2018-10-24 18:46:15 -0700

    Changed the value of SQLALCHEMY_DATABASE_URI to the SQLite database delphi.db

--- delphi/icm_api/config.py
+++ delphi/icm_api/config.py
@@ -6,11 +6,13 @@ DEBUG = True
 # Define the application directory
 import os
 
-BASE_DIR = Path(__file__)
+BASE_DIR = Path(__file__).parent
 
 # Define the database - we are working with
 # SQLite for this example
-SQLALCHEMY_DATABASE_URI = "sqlite://" / BASE_DIR / "app.db"
+uri = f"sqlite:///{BASE_DIR}/delphi.db"
+print(uri)
+SQLALCHEMY_DATABASE_URI = uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
 
