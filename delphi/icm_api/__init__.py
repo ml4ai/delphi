076b5a92b06f241e69e256aa5dbd55c2e0c2c4d4
JiamingHao <hao1@email.arizona.edu>
2018-11-09 10:29:57 -0700

    Add celery to __init__.py.

--- delphi/icm_api/__init__.py
+++ delphi/icm_api/__init__.py
@@ -1,9 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from celery import Celery
 
 db = SQLAlchemy()
+ 
 
+# Integrate Celery with Flask
+def make_celery(app):
+    celery = Celery(
+            app.import_name,
+            backend=app.config['CELERY_RESULT_BACKEND'],
+            broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+    
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+    
+    celery.Task = ContextTask
+    return celery
 
 def create_app():
     from delphi.icm_api.api import bp
