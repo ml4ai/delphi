37fc9d3e2bb17c4406a84bdc8b7caad9314f9b30
Paul Hein <pauldhein@email.arizona.edu>
2018-11-13 10:03:05 -0700

    Added SA test code on lambdas.py using SALib

---
+++ scripts/program_analysis/sensitivity_analysis.py
@@ -0,0 +1,45 @@
+import inspect
+
+import tangent
+from SALib.sample import saltelli
+from SALib.analyze import sobol
+import numpy as np
+
+import delphi.program_analysis.autoTranslate.lambdas as lambdas
+
+
+def visually_inspect_tangent():
+    f = lambdas.CROP_YIELD__lambda__RAIN_0
+    df = tangent.grad(f)
+    code = inspect.getsource(df)
+    print(code)
+
+
+def perform_sensitivity_analysis():
+    func = lambdas.CROP_YIELD__lambda__RAIN_0
+
+    (args, _, _, _) = inspect.getargspec(func)
+
+    param_def = {
+        'num_vars': len(args),
+        'names': args,
+        'bounds': [[0, 30], [0, 1], [0, 100], [0, 1]]
+    }
+
+    print("Sampling over parameter bounds")
+    param_values = saltelli.sample(param_def, 1000, calc_second_order=True)
+
+    print("Evaluating samples")
+    y = evaluate(func, param_values)
+
+    print("Collecting sensitivity indices")
+    Si = sobol.analyze(param_def, y)
+    print(Si)
+
+
+def evaluate(f, arg_matrix):
+    return np.array([f(*args) for args in arg_matrix])
+
+
+# visually_inspect_tangent()
+perform_sensitivity_analysis()
