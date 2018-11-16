c8aa08df1b37cf5bb4e08d4e05396055c9a55572
Adarsh <adarsh.pyarelal@gmail.com>
2018-11-16 12:08:16 -0700

    Moved an inference method to an AnalysisGraph class method, implemented some inference building blocks

--- delphi/inference.py
+++ delphi/inference.py
@@ -1,73 +1,86 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import gaussian_kde
+from scipy.stats import norm
 from delphi.utils.fp import flatMap, ltake, iterate
-from delphi.paths import adjectiveData
 from delphi.AnalysisGraph import AnalysisGraph
-from delphi.assembly import constructConditionalPDF, get_respdevs
-from delphi.execution import get_latent_state_components
-from typing import List
+from delphi.execution import get_latent_state_components, emission_function
+from typing import List, Dict
 
 
-def infer_transition_model(
-    G: AnalysisGraph, adjective_data: str = None, res: int = 100
-):
-    """ Add probability distribution functions constructed from gradable
-    adjective data to the edges of the analysis graph data structure.
-
-    Args:
-        adjective_data
-        res
-    """
-
-    G.res = res
-    if adjective_data is None:
-        adjective_data = adjectiveData
-
-    gb = pd.read_csv(adjective_data, delim_whitespace=True).groupby(
-        "adjective"
-    )
-
-    rs = gaussian_kde(
-        flatMap(
-            lambda g: gaussian_kde(get_respdevs(g[1]))
-            .resample(res)[0]
-            .tolist(),
-            gb,
-        )
-    ).resample(res)[0]
-
-    for e in G.edges(data=True):
-        e[2]["ConditionalProbability"] = constructConditionalPDF(gb, rs, e)
-        e[2]["betas"] = np.tan(
-            e[2]["ConditionalProbability"].resample(G.res)[0]
-        )
-
-
-def sample_from_prior(G: AnalysisGraph, delta_t: float = 1.0) -> pd.DataFrame:
+def sample_transition_matrix_from_gradable_adjective_prior(
+    G: AnalysisGraph, delta_t: float = 1.0
+) -> pd.DataFrame:
+    """ Return a pandas DataFrame object representing a transition matrix for
+    the DBN. """
     elements = get_latent_state_components(G)
     A = pd.DataFrame(np.identity(2 * len(G)), index=elements, columns=elements)
+    for n in G.nodes:
+        A[f"∂({n})/∂t"][n] = delta_t
     for e in G.edges(data=True):
-        A[e[1]][f"∂({e[0]})/∂t"] = (
+        A[f"∂({e[0]})/∂t"][e[1]] = (
             e[2]["ConditionalProbability"].resample(1)[0][0] * delta_t
         )
-
     return A
 
 
-def sample_sequence_of_latent_states(
-    A: np.ndarray, s0: np.ndarray, n_steps: int
+def get_sequence_of_latent_states(
+    A: pd.DataFrame, s0: pd.Series, n_steps: int
 ) -> List[np.ndarray]:
-    return ltake(n_steps, iterate(lambda s: A @ s, s0))
+    """ Return a list of pandas Series objects corresponding to latent state
+    vectors. """
+    return ltake(
+        n_steps,
+        iterate(lambda s: pd.Series(A.values @ s.values, index=s0.index), s0),
+    )
 
 
-def calculate_prior_probability(A, G):
-    return np.product(
+def create_observed_state(G: AnalysisGraph) -> Dict:
+    """ Create a dict corresponding to an observed state vector. """
+    return {
+        n[0]: {ind.name: ind.value for ind in n[1]["indicators"].values()}
+        for n in G.nodes(data=True)
+    }
+
+
+def sample_observed_state(G: AnalysisGraph, s: pd.Series) -> Dict:
+    """ Sample an observed state given a latent state vector. """
+    for n in G.nodes(data=True):
+        for indicator in n[1]["indicators"].values():
+            indicator.value = np.random.normal(
+                s[n[0]] * indicator.mean, indicator.stdev
+            )
+
+    return create_observed_state(G)
+
+
+def evaluate_prior_pdf(
+    A: pd.DataFrame, G: AnalysisGraph, delta_t: float = 1.0
+) -> float:
+    return np.prod(
         [
-            e[2]["ConditionalProbability"].evaluate(A[e[1]][f"∂({e[0]})/∂t"])
+            e[2]["ConditionalProbability"].evaluate(
+                A[f"∂({e[0]})/∂t"][e[1]] / delta_t
+            )
             for e in G.edges(data=True)
         ]
     )
 
 
-# def calculate_likelihood(latent_states, indicator_values):
+def evaluate_likelihood_pdf(
+    G: AnalysisGraph,
+    latent_state_vectors: List[np.ndarray],
+    observed_states: List[Dict],
+) -> float:
+    s0 = latent_state_vectors[0]
+    _list = []
+    for latent_state, observed_state in zip(
+        latent_state_vectors, observed_states
+    ):
+        for n in G.nodes(data=True):
+            for indicator, value in observed_state[n[0]].items():
+                ind = n[1]["indicators"][indicator]
+                _list.append(
+                    norm.pdf(value, latent_state[n] * ind.mean, ind.stdev)
+                )
+
+    return np.prod(_list)
