6599c0595f6ee23537c869c96d3a1bb0d29fe954
Adarsh <adarsh.pyarelal@gmail.com>
2018-11-16 12:03:51 -0700

    changed infer_transition_model function name to assemble_transition_model_from_gradable_adjectives, added it as a method to the AnalysisGraph class to avoid circular dependencies.

--- delphi/AnalysisGraph.py
+++ delphi/AnalysisGraph.py
@@ -3,21 +3,16 @@ import json
 import pickle
 from uuid import uuid4
 import networkx as nx
-from delphi.assembly import (
-    get_concepts,
-    get_valid_statements_for_modeling,
-    nameTuple,
-)
+import pandas as pd
+from scipy.stats import gaussian_kde
+from delphi.paths import adjectiveData
+from delphi.utils.indra import get_valid_statements_for_modeling, get_concepts
+from delphi.assembly import constructConditionalPDF, get_respdevs, make_edges
+from delphi.utils.fp import flatMap
 from datetime import datetime
 from itertools import permutations
 from indra.statements import Influence
-
-
-def make_edge(
-    sts: List[Influence], p: Tuple[str, str]
-) -> Tuple[str, str, Dict[str, List[Influence]]]:
-    edge = (*p, {"InfluenceStatements": [s for s in sts if nameTuple(s) == p]})
-    return edge
+import numpy as np
 
 
 class AnalysisGraph(nx.DiGraph):
@@ -31,7 +26,6 @@ class AnalysisGraph(nx.DiGraph):
         self.Δt: float = 1.0
         self.time_unit: str = "Placeholder time unit"
         self.dateCreated = datetime.now()
-        self.data = None
         self.name: str = "Linear Dynamical System with Stochastic Transition Model"
 
     # ==========================================================================
@@ -53,11 +47,7 @@ class AnalysisGraph(nx.DiGraph):
         """ Construct an AnalysisGraph object from a list of INDRA statements. """
         sts = get_valid_statements_for_modeling(sts)
         node_permutations = permutations(get_concepts(sts), 2)
-        edges = [
-            e
-            for e in [make_edge(sts, p) for p in node_permutations]
-            if len(e[2]["InfluenceStatements"]) != 0
-        ]
+        edges = make_edges(sts, node_permutations)
         G = cls(edges)
 
         for n in G.nodes(data=True):
@@ -90,3 +80,37 @@ class AnalysisGraph(nx.DiGraph):
     def from_json_serialized_statements_file(cls, file):
         with open(file, "r") as f:
             return cls.from_json_serialized_statements_list(f.read())
+
+    def assemble_transition_model_from_gradable_adjectives(
+        self, adjective_data: str = None, res: int = 100
+    ):
+        """ Add probability distribution functions constructed from gradable
+        adjective data to the edges of the analysis graph data structure.
+
+        Args:
+            adjective_data
+            res
+        """
+
+        self.res = res
+        if adjective_data is None:
+            adjective_data = adjectiveData
+
+        gb = pd.read_csv(adjective_data, delim_whitespace=True).groupby(
+            "adjective"
+        )
+
+        rs = gaussian_kde(
+            flatMap(
+                lambda g: gaussian_kde(get_respdevs(g[1]))
+                .resample(res)[0]
+                .tolist(),
+                gb,
+            )
+        ).resample(res)[0]
+
+        for e in self.edges(data=True):
+            e[2]["ConditionalProbability"] = constructConditionalPDF(gb, rs, e)
+            e[2]["betas"] = np.tan(
+                e[2]["ConditionalProbability"].resample(self.res)[0]
+            )
