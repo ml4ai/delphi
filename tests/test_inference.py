09e904f08b9d6392b3bbf69b9a71fc037c3515ae
Adarsh <adarsh.pyarelal@gmail.com>
2018-11-19 19:38:54 -0700

    Fixed inference test, set reasonable initial value and tolerances.

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -10,53 +10,61 @@ from matplotlib import pyplot as plt
 
 
 @pytest.mark.slow
-@pytest.mark.skip(reason = "Still ironing out some warts in the sampler.")
 def test_sampler(G):
     """ Smokescreen test for sampler. """
+
     sampler = Sampler(G)
-    n_timesteps = 2
-    n_samples = 100_000
-    sampler.s0["∂(conflict)/∂t"] = 0.1
+    sampler.n_timesteps = 10
+
+    n_samples = 1000
+
+    # Generate synthetic data
     sampler.sample_from_prior()
-    sampler.A["∂(conflict)/∂t"]["food_security"] = -0.1
-    sampler.calculate_log_prior()
-    original_beta = sampler.A["∂(conflict)/∂t"]["food_security"]
-    sampler.A["∂(conflict)/∂t"]["food_security"] = np.random.normal()
-    sampler.set_number_of_timesteps(n_timesteps)
+    original_beta = -0.2
+    sampler.A["∂(conflict)/∂t"]["food_security"] = original_beta
+    sampler.s0["∂(conflict)/∂t"] = 0.1
     sampler.set_latent_state_sequence()
     sampler.sample_from_likelihood()
-    sampler.calculate_log_likelihood()
-    sampler.calculate_log_joint_probability()
+
+    # Initialize random walk
+    sampler.A["∂(conflict)/∂t"]["food_security"] = 2.0
+    sampler.update_log_prior()
+    sampler.set_latent_state_sequence()
+    sampler.update_log_likelihood()
+    sampler.update_log_joint_probability()
     sampler.original_score = sampler.log_joint_probability
-    map_estimate = original_beta
-    map_log_joint_probability = sampler.log_joint_probability
+    map_estimate = sampler.A["∂(conflict)/∂t"]["food_security"]
+    map_log_joint_probability = sampler.original_score
+
     scores = []
     betas = []
     log_likelihoods = []
     log_priors = []
+    map_estimates = []
+
     for i, _ in enumerate(trange(n_samples)):
         sampler.sample_from_posterior()
         if sampler.log_joint_probability > map_log_joint_probability:
             map_estimate = sampler.A["∂(conflict)/∂t"]["food_security"]
-        if i % 100 == 0:
-            scores.append(
-                sampler.log_joint_probability - sampler.original_score
-            )
-            betas.append(sampler.A["∂(conflict)/∂t"]["food_security"])
-            log_likelihoods.append(sampler.log_likelihood)
-            log_priors.append(sampler.log_prior)
+        scores.append(sampler.log_joint_probability - sampler.original_score)
+        betas.append(sampler.A["∂(conflict)/∂t"]["food_security"])
+        log_likelihoods.append(sampler.log_likelihood)
+        log_priors.append(sampler.log_prior)
+        map_estimates.append(map_estimate)
 
     plt.style.use("ggplot")
-    fig, axes = plt.subplots(4, 1, figsize=(12, 8), sharex=True)
-    axes[0].plot(betas, label="beta")
-    axes[0].plot(original_beta * np.ones(len(betas)), label="original_beta")
-    axes[1].plot(scores, label="scores")
-    axes[2].plot(log_priors, label="log_prior")
-    axes[3].plot(log_likelihoods, label="log_likelihood")
+    fig, axes = plt.subplots(4, 1, figsize=(8, 8), sharex=True)
+    axes[0].plot(betas[::10], label="beta")
+    axes[0].plot(
+        original_beta * np.ones(len(betas[::10])), label="original_beta"
+    )
+    axes[1].plot(map_estimates[::10], label="scores")
+    axes[2].plot(log_priors[::10], label="log_prior")
+    axes[3].plot(log_likelihoods[::10], label="log_likelihood")
     for ax in axes:
         ax.legend()
     plt.savefig("mcmc_results.pdf")
     fig, ax = plt.subplots()
-    plt.hist(betas)
+    plt.hist(betas, bins=40)
     plt.savefig("betas.pdf")
-    assert map_estimate == approx(original_beta, abs=0.05)
+    assert map_estimate == approx(original_beta, abs=0.1)
