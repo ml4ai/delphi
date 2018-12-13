e771bec822ffe7072eadffe561bd8a9608edf8f7
Adarsh <adarsh.pyarelal@gmail.com>
2018-12-13 13:41:23 -0700

    Added (commented out) line for scopes.py that works with crop_yield.f

--- delphi/program_analysis/scopes.py
+++ delphi/program_analysis/scopes.py
@@ -58,9 +58,15 @@ class Scope(metaclass=ABCMeta):
             scope.remove_non_scope_children(scope_names)
 
         # Build the nested tree of scopes using recursion
+
+        # AP version
+        # root = scopes[data["start"]]
+
+        # PH version
         non_lambdas = [f["name"] for f in data["functions"] if "__" not in f["name"]]
         root_func_name = non_lambdas[0]     # There should only ever be one, otherwise we need multiple roots
         root = scopes[root_func_name]
+
         root.build_scope_tree(scopes)
         root.setup_from_json()
         return root
