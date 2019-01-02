fef50c44cd9a10fbd22118a6601891b283f00bb7
Adarsh <adarsh.pyarelal@gmail.com>
2019-01-02 15:37:09 -0500

    Added path to delphi db in delphi.paths

--- delphi/paths.py
+++ delphi/paths.py
@@ -6,6 +6,7 @@ data_dir = Path(os.environ.get("DELPHI_DATA", ""))
 
 adjectiveData = data_dir / "adjectiveData.tsv"
 south_sudan_data = data_dir / "south_sudan_data.csv"
+db_path = data_dir / "delphi.db"
 concept_to_indicator_mapping = data_dir / "concept_to_indicator_mapping.txt"
 
 os.environ["CLASSPATH"] = (
