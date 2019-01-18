b8f3707cab32e67e7447cba436e26ef394c77fda
Terrence Lim <hlim1@mail.csuchico.edu>
2019-01-17 22:39:16 -0700

    do_while_01.py output

---
+++ tests/data/do-while/do_while_01.py
@@ -0,0 +1,10 @@
+from typing import List
+import mathimport array
+
+def DO_WHILE():
+    MONTH: List[int] = [0]
+    MONTH[0] = 1
+    while (MONTH[0] <= 12):
+        print("Month: ", MONTH)
+
+DO_WHILE()
