f4a172a5a31f4c5ac1c6574e7f8f2d3b28921b0b
Pratik Bhandari <pratik.bhd@gmail.com>
2019-01-19 17:39:04 -0700

    bugfix handle to get comments

--- delphi/translators/for2py/scripts/get_comments.py
+++ delphi/translators/for2py/scripts/get_comments.py
@@ -88,7 +88,6 @@ def get_comments(src_file_name: str):
     comments["$file_head"] = []
     comments["$file_foot"] = []
 
-    print (curr_comment)
     with open(src_file_name, "r", encoding="latin-1") as f:
         for line in f:
             if line_is_comment(line) and collect_comments:
@@ -132,8 +131,7 @@ def get_comments(src_file_name: str):
 
     # if there's a comment at the very end of the file, make it the foot
     # comment of curr_fn
-    print (comments)
-    if curr_comment and curr_comment != []:
+    if curr_comment != [] and comments.get(curr_fn):
         comments[curr_fn]["foot"] = curr_comment
         comments["$file_foot"] = curr_comment
     return comments
