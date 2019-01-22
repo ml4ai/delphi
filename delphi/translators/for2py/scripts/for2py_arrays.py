0b343023282bb53cb9a7849ea44547f9e2c609e9
Saumya K. Debray <saumya.debray@gmail.com>
2019-01-22 15:05:06 -0700

    Fixing coding style

--- delphi/translators/for2py/scripts/for2py_arrays.py
+++ delphi/translators/for2py/scripts/for2py_arrays.py
@@ -29,10 +29,11 @@ class Array:
         self._values = self._mk_uninit_array(bounds)
 
 
-    # given a list of bounds for the N dimensions of an array, _mk_uninit_array()
-    # creates and returns an N-dimensional array of the size specified by the
-    # bounds with each element set to the value None.
     def _mk_uninit_array(self, bounds):
+        """ given a list of bounds for the N dimensions of an array, 
+            _mk_uninit_array() creates and returns an N-dimensional array of 
+            the size specified by the bounds with each element set to the value
+            None."""
         if len(bounds) == 0:
             sys.stderr.write("Zero-length arrays current not handled!\n")
             sys.exit(1)
@@ -50,30 +51,31 @@ class Array:
         return this_array
 
 
-    # bounds() returns a list of pairs (lo,hi) giving the lower and upper
-    # bounds of the array.
     def bounds(self):
+        """bounds() returns a list of pairs (lo,hi) giving the lower and upper
+        bounds of the array."""
         return self._bounds
 
 
-    # lower_bd(i) returns the lower bound of the array in dimension i.  Dimensions
-    # are numbered from 0 up.
     def lower_bd(self, i):
+        """lower_bd(i) returns the lower bound of the array in dimension i.
+           Dimensions are numbered from 0 up."""
         this_dim_bounds = self._bounds[i]
         return this_dim_bounds[0]
 
 
-    # upper_bd(i) returns the upper bound of the array in dimension i.  Dimensions
-    # are numbered from 0 up.
     def upper_bd(self, i):
+        """upper_bd(i) returns the upper bound of the array in dimension i.
+           Dimensions are numbered from 0 up."""
         this_dim_bounds = self._bounds[i]
         return this_dim_bounds[1]
 
 
-    # given bounds = (lo,hi) and an index value idx, _posn(bounds, idx)
-    # returns the position in a 0-based array corresponding to idx in the 
-    # (lo,hi)-based array.  It generates an error if idx < lo or idx > hi.
     def _posn(self, bounds, idx):
+        """given bounds = (lo,hi) and an index value idx, _posn(bounds, idx)
+           returns the position in a 0-based array corresponding to idx in 
+           the (lo,hi)-based array.  It generates an error if idx < lo or 
+           idx > hi."""
         lo,hi = bounds[0],bounds[1]
         if idx < lo or idx > hi:
             sys.stderr.write("Array index (value = {:d}) out of bounds {}\n".\
@@ -83,16 +85,17 @@ class Array:
         return idx-lo
    
 
-    # _access(subs, acc_type, val) accesses the array element specified by the 
-    # tuple of subscript values, subs.  If acc_type == __GET it returns the value 
-    # of this element; else it sets this element to the value of the argument val.
     def _access(self, subs, acc_type, val):
+        """_access(subs, acc_type, val) accesses the array element specified by
+           the tuple of subscript values, subs.  If acc_type == _GET_ it 
+           returns the value of this element; else it sets this element to the
+           value of the argument val."""
+        if isinstance(subs, int):  
         # if subs is just an integer, take it to be an index value.
-        if isinstance(subs, int):
             subs = (subs,)
 
         if len(subs) == 0:
-            sys.stderr.write("get/set: Zero-length arrays currently not handled!\n")
+            sys.stderr.write("Zero-length arrays currently not handled\n")
             sys.exit(1)
 
         bounds = self._bounds
@@ -110,31 +113,32 @@ class Array:
                 sub_arr = sub_arr[this_pos]
 
 
-    # set_() sets the value of the array element specified by the given tuple
-    # of array subscript values to the argument val.
     def set_(self, subs, val):
+        """set_() sets the value of the array element specified by the given
+           tuple of array subscript values to the argument val."""
         self._access(subs, _SET_, val)
 
 
-    # get_() returns the value of the array element specified by the given tuple
-    # of array subscript values.
     def get_(self, subs):
+        """get_() returns the value of the array element specified by the 
+           given tuple of array subscript values."""
         return self._access(subs, _GET_, None)
 
 
-    # get_elems(subs_list) returns a list of values of the array elements specified
-    # by the list of subscript values subs_list (each element of subs_list is a
-    # tuple of subscripts identifying an array element).  
     def get_elems(self, subs_list):
+        """get_elems(subs_list) returns a list of values of the array elements
+           specified by the list of subscript values subs_list (each element of
+           subs_list is a tuple of subscripts identifying an array element)."""
         return [self.get_(subs) for subs in subs_list]
 
 
-    # set_elems(subs, vals) sets the array elements specified by the list
-    # of subscript values subs (each element of subs is a tuple of subscripts
-    # identifying an array element) to the corresponding value in vals.
     def set_elems(self, subs, vals):
-        # if vals is a scalar, extend it to a list of appropriate length
+        """set_elems(subs, vals) sets the array elements specified by the list
+           of subscript values subs (each element of subs is a tuple of 
+           subscripts identifying an array element) to the corresponding value
+           in vals."""
         if isinstance(vals, (int, float)):
+            # if vals is a scalar, extend it to a list of appropriate length
             vals = [vals] * len(subs)
 
         for i in range(len(subs)):
@@ -147,9 +151,9 @@ class Array:
 #                                                                              #
 ################################################################################
 
-# given a list of tuples specifying the bounds of an array, all_subs()
-# returns a list of all the tuples of subscripts for that array.
 def all_subs(bounds):
+    """given a list of tuples specifying the bounds of an array, all_subs()
+       returns a list of all the tuples of subscripts for that array."""
     idx_list = []
     for i in range(len(bounds)):
         this_dim = bounds[i]
@@ -160,21 +164,23 @@ def all_subs(bounds):
     return idx2subs(idx_list)
 
 
-# Given a list idx_list of index values for each dimension of an array, 
-# idx2subs() returns a list of the tuples of subscripts for all of the array
-# elements specified by those index values.
-# Note: This code adapted from that posted by jfs at
-# https://stackoverflow.com/questions/533905/get-the-cartesian-product-of-a-series-of-lists
 def idx2subs(idx_list):
+    """Given a list idx_list of index values for each dimension of an array, 
+       idx2subs() returns a list of the tuples of subscripts for all of the 
+       array elements specified by those index values.
+
+       Note: This code adapted from that posted by jfs at
+       https://stackoverflow.com/questions/533905/get-the-cartesian-product-
+       of-a-series-of-lists"""
     if not idx_list:
         return [()]
     return [items + (item,) 
             for items in idx2subs(idx_list[:-1]) for item in idx_list[-1]]
 
 
-# Given an expression expr denoting a list of values, values(expr) returns 
-# a list of values for that expression.
 def values(expr):
+    """Given an expression expr denoting a list of values, values(expr) 
+       returns a list of values for that expression."""
     if isinstance(expr, Array):
         return expr.get_elems(all_subs(expr._bounds))
     elif isinstance(expr, list):
@@ -184,10 +190,10 @@ def values(expr):
         return [expr]
 
 
-# Given a subscript expression expr (i.e., an expression that denotes the
-# set of elements of some array that are to be accessed), subscripts() returns
-# a list of the elements denoted by expr.
 def subscripts(expr):
+    """Given a subscript expression expr (i.e., an expression that denotes the
+       set of elements of some array that are to be accessed), subscripts() 
+       returns a list of the elements denoted by expr."""
     if isinstance(expr, Array):
         return all_subs(expr._bounds)
     elif isinstance(expr, list):
