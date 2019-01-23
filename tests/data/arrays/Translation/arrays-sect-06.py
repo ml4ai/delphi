a2e9ceb245f9002428e26cf38cf40407f448ec00
Saumya K. Debray <saumya.debray@gmail.com>
2019-01-23 15:54:11 -0700

    Edited to match revised names for array accessor functions

--- tests/data/arrays/Translation/arrays-sect-06.py
+++ tests/data/arrays/Translation/arrays-sect-06.py
@@ -5,21 +5,21 @@ def main():
     A = Array([(1,5)])
 
     for i in range(1,5+1):
-        A.set(i, 0)
+        A.set_(i, 0)
 
     fmt_obj = Format(['5(I5)'])
 
-    sys.stdout.write(fmt_obj.write_line([A.get(1), A.get(2), A.get(3), A.get(4), A.get(5)]))
+    sys.stdout.write(fmt_obj.write_line([A.get_(1), A.get_(2), A.get_(3), A.get_(4), A.get_(5)]))
 
     A_subs = implied_loop_expr((lambda x:x), 3, A.upper_bd(0), 1)
     A.set_elems(A_subs, 17)
 
-    sys.stdout.write(fmt_obj.write_line([A.get(1), A.get(2), A.get(3), A.get(4), A.get(5)]))
+    sys.stdout.write(fmt_obj.write_line([A.get_(1), A.get_(2), A.get_(3), A.get_(4), A.get_(5)]))
 
     A_subs = implied_loop_expr((lambda x:x), A.lower_bd(0), 3, 1)
     A.set_elems(A_subs, 29)
 
-    sys.stdout.write(fmt_obj.write_line([A.get(1), A.get(2), A.get(3), A.get(4), A.get(5)]))
+    sys.stdout.write(fmt_obj.write_line([A.get_(1), A.get_(2), A.get_(3), A.get_(4), A.get_(5)]))
 
 
 main()
