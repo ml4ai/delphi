0b844c79184c8ec9ddb5fcc140da058f39170c57
Paul Hein <pauldhein@email.arizona.edu>
2019-01-23 08:06:30 -0700

    Finding predeccessors of node successors and graph coloring

--- delphi/analysis/comparison/CausalMarkovBlanket.py
+++ delphi/analysis/comparison/CausalMarkovBlanket.py
@@ -23,6 +23,17 @@ class CausalMarkovBlanket(nx.DiGraph):
             self.add_edges_from(list(zip(path, path[1:])))
         self.add_edges_from(self.cover_edges)
 
+        for node_name in self.cover_nodes:
+            self.node[node_name]["color"] = "green"
+
+        for node_name in self.shared_nodes:
+            self.node[node_name]["color"] = "blue"
+            for dest in self.successors(node_name):
+                self[node_name][dest]["color"] = "blue"
+
+        for source, dest in self.cover_edges:
+            self[source][dest]["color"] = "green"
+
     def get_output_nodes(self) -> List[str]:
         """ Get all output nodes from a network. """
         return [n for n, d in self.orig_graph.out_degree() if d == 0]
@@ -36,6 +47,17 @@ class CausalMarkovBlanket(nx.DiGraph):
     def get_cover_edges(self) -> List[List[str]]:
         """Get all edges needed to blanket the included nodes."""
         main_nodes = list(set([node for path in self.paths for node in path]))
-        return [[pred, node] for node in main_nodes
-                for pred in self.orig_graph.predecessors(node)
-                if pred not in main_nodes]
+        # Need to include children and parents of children in the markov blanket
+        predecessors = [[pred, node] for node in main_nodes
+                        for pred in self.orig_graph.predecessors(node)
+                        if pred not in main_nodes]
+
+        successors = [[node, succ] for node in main_nodes
+                      for succ in self.orig_graph.successors(node)
+                      if succ not in main_nodes]
+
+        succ_preds = [[pred, node] for node in successors
+                      for pred in self.orig_graph.predecessors(node)
+                      if pred not in main_nodes]
+
+        return predecessors + successors + succ_preds
