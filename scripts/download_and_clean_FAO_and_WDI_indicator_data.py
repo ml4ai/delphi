6e0f388b87abac3fa980274adbbd3b5c324c94b2
Adarsh <adarsh.pyarelal@gmail.com>
2019-01-25 18:01:42 -0700

    Refactored script to download and process FAO and WDI data, added Makefile for automation

--- scripts/download_and_clean_FAO_and_WDI_indicator_data.py
+++ scripts/download_and_clean_FAO_and_WDI_indicator_data.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import pandas as pd
 from glob import glob
 from future.utils import lmap
@@ -7,7 +7,8 @@ import multiprocessing as mp
 from functools import partial
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString as S
 from ruamel.yaml import YAML
-from delphi.paths import data_dir
+
+# from delphi.paths import data_dir
 from delphi.utils.shell import cd
 from delphi.utils.web import download_file
 from pathlib import Path
@@ -16,47 +17,28 @@ import subprocess as sp
 import contextlib
 
 
-read_csv = partial(
-    pd.read_csv,
-    encoding="latin-1",
-    usecols=lambda colName: all(
-        map(
-            lambda x: x not in colName,
-            ["Code", "Flag", "Note", "ElementGroup"],
-        )
-    ),
-)
+data_dir = Path("data")
 
 
-def download_FAOSTAT_data(url, faostat_zipfile):
-    """ Download data from FAOSTAT database """
-    download_file(url, str(faostat_zipfile))
+def clean_FAOSTAT_data():
 
-
-def sp_unzip(f):
-    sp.call(["unzip", f])
-
-
-def clean_FAOSTAT_data(faostat_zipfile, faostat_dir):
-
-    if not (faostat_zipfile.is_file() or faostat_dir.is_dir()):
-        download_FAOSTAT_data()
-    if faostat_zipfile.is_file() and not faostat_dir.is_dir():
-        os.mkdir(faostat_dir)
-        with zipfile.ZipFile(str(faostat_zipfile)) as zf:
-            zf.extractall(faostat_dir)
-        with cd(str(faostat_dir.resolve())):
-            zipfiles = glob("*.zip")
-            with mp.Pool(mp.cpu_count()) as p:
-                for _ in tqdm(
-                    p.imap_unordered(sp_unzip, zipfiles), total=len(zipfiles)
-                ):
-                    pass
-
+    faostat_dir = "data/FAOSTAT"
     dfs = []
-    for filename in tqdm(glob(str(faostat_dir) + "/*.csv")):
-        df = read_csv(filename)
-        df = df.rename(columns={"Country": "Area", "Months": "Month"})
+    for filename in tqdm(
+        glob(str(faostat_dir) + "/*.csv"), desc="Processing FAOSTAT files"
+    ):
+        df = pd.read_csv(
+            filename,
+            encoding="latin-1",
+            usecols=lambda colName: all(
+                map(
+                    lambda x: x not in colName,
+                    ["Code", "Flag", "Note", "ElementGroup"],
+                )
+            ),
+        )
+
+        df.rename(columns={"Country": "Area", "Months": "Month"}, inplace=True)
         if "Currency" in df.columns:
             df = df.rename(columns={"Currency": "Item", "Item": "Element"})
         if "Reporter Countries" in df.columns:
@@ -85,13 +67,16 @@ def clean_FAOSTAT_data(faostat_zipfile, faostat_dir):
         ):
             df = df[df["Area"] == "South Sudan"]
             del df["Area"]
+            df["Country"] = "South Sudan"
             dfs.append(df)
 
-        df["filename"] = filename.split("/")[-1]
-
     df = pd.concat(dfs)
+
+    df["Variable"] = df["Element"] + ", " + df["Item"]
+    del df["Element"]
+    del df["Item"]
     df.to_csv(
-        str(Path(data_dir) / "south_sudan_data_fao.csv"), index=False, sep="|"
+        str(data_dir / "south_sudan_data_fao.tsv"), index=False, sep="\t"
     )
 
 
@@ -134,19 +119,10 @@ def construct_FAO_ontology():
         yaml.dump(d, f)
 
 
-def download_WDI_data(wdi_zipfile, wdi_data_dir):
-    url = "http://databank.worldbank.org/data/download/WDI_csv.zip"
-    if not wdi_zipfile.is_file():
-        download_file(url, wdi_zipfile)
-    if not wdi_data_dir.is_dir():
-        os.mkdir(wdi_data_dir)
-        with zipfile.ZipFile(str(wdi_zipfile)) as zf:
-            zf.extractall(wdi_data_dir)
-
-
-def clean_WDI_data(wdi_data_dir):
+def clean_WDI_data():
+    print("Cleaning WDI data")
     df = pd.read_csv(
-        wdi_data_dir / "WDIData.csv",
+        "data/WDI_csv/WDIData.csv",
         usecols=[
             "Country Name",
             "Indicator Name",
@@ -158,39 +134,40 @@ def clean_WDI_data(wdi_data_dir):
             "2017",
         ],
     )
-    df = df[df["Country Name"] == "South Sudan"]
-    del df["Country Name"]
-    df.to_csv(
-        Path(data_dir) / "south_sudan_data_wdi.csv", index=False, sep="|"
+    df.rename(
+        columns={"Indicator Name": "Variable", "Country Name": "Country"},
+        inplace=True,
     )
+    df = df[df["Country"] == "South Sudan"]
+    df.to_csv("data/south_sudan_data_wdi.tsv", index=False, sep="\t")
 
 
 def combine_FAO_and_WDI_data():
-    fao_df = pd.read_csv(Path(data_dir) / "south_sudan_data_fao.csv", sep="|")
-    wdi_df = pd.read_csv(Path(data_dir) / "south_sudan_data_wdi.csv", sep="|")
-
-    fao_df["Indicator Name"] = fao_df["Element"] + ", " + fao_df["Item"]
-
-    del fao_df["Element"]
-    del fao_df["Item"]
+    fao_df = pd.read_table("data/south_sudan_data_fao.tsv")
+    fao_df["Source"] = "FAO"
+    wdi_df = pd.read_table("data/south_sudan_data_wdi.tsv")
+    wdi_df["Source"] = "WDI"
 
     wdi_df["Unit"] = (
-        wdi_df["Indicator Name"].str.partition("(")[2].str.partition(")")[0]
+        wdi_df["Variable"].str.partition("(")[2].str.partition(")")[0]
     )
-    wdi_df["Indicator Name"] = wdi_df["Indicator Name"].str.partition("(")[0]
-    wdi_df = wdi_df.set_index(["Indicator Name", "Unit"])
+    wdi_df["Variable"] = wdi_df["Variable"].str.partition("(")[0]
+    wdi_df = wdi_df.set_index(["Variable", "Unit", "Source", "Country"])
     fao_df = (
         fao_df.pivot_table(
-            values="Value", index=["Indicator Name", "Unit"], columns="Year"
+            values="Value",
+            index=["Variable", "Unit", "Source", "Country"],
+            columns="Year",
         )
         .reset_index()
-        .set_index(["Indicator Name", "Unit"])
+        .set_index(["Variable", "Unit", "Source", "Country"])
     )
 
+    ind_cols = ["Variable", "Unit", "Source", "Country"]
+    # print(fao_df.reset_index().melt(id_vars=ind_cols).head())
     df = pd.concat([fao_df, wdi_df], sort=True)
-
-    # If a column name is something like 2010-2012, we make copies of its data for
-    # three years - 2010, 2011, 2012
+    # If a column name is something like 2010-2012, we make copies of its data
+    # for three years - 2010, 2011, 2012
 
     for c in df.columns:
         if "-" in c:
@@ -198,23 +175,22 @@ def combine_FAO_and_WDI_data():
             for y in range(int(years[0]), int(years[-1]) + 1):
                 y = str(y)
                 df[y] = df[y].fillna(df[c])
+            del df[c]
 
-    df.to_csv(Path(data_dir) / "south_sudan_data.csv", sep="|")
+    df = (
+        df.reset_index()
+        .melt(id_vars=ind_cols, var_name="Year", value_name="Value")
+        .dropna(subset=["Value"])
+    )
+    df["State"] = None
+
+    df.to_csv(Path(data_dir) / "south_sudan_data.tsv", sep="\t", index=False)
 
 
 if __name__ == "__main__":
-
-    wdi_zipfile = Path(data_dir) / "WDI_csv.zip"
-    wdi_data_dir = Path(data_dir) / "WDI"
-
-    download_WDI_data(wdi_zipfile, wdi_data_dir)
-    clean_WDI_data(wdi_data_dir)
-
-    faostat_url = (
-        "http://fenixservices.fao.org/faostat/static/bulkdownloads/FAOSTAT.zip"
-    )
-    faostat_zipfile = Path(data_dir) / "FAOSTAT.zip"
-    faostat_dir = Path(data_dir) / "FAOSTAT"
-
-    download_FAOSTAT_data(faostat_url, faostat_zipfile)
-    clean_FAOSTAT_data(faostat_zipfile, faostat_dir)
+    if sys.argv[1] == "--fao":
+        clean_FAOSTAT_data()
+    elif sys.argv[1] == "--wdi":
+        clean_WDI_data()
+    elif sys.argv[1] == "--combine":
+        combine_FAO_and_WDI_data()
