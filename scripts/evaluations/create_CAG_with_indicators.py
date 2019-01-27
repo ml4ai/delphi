01bbf694dd8600a1f7db81cdbe00963174903449
Adarsh <adarsh.pyarelal@gmail.com>
2019-01-27 14:35:31 -0700

    Adding Inflation Rate indicator from ieconomics.com for market node

--- scripts/evaluations/create_CAG_with_indicators.py
+++ scripts/evaluations/create_CAG_with_indicators.py
@@ -9,6 +9,7 @@ def create_CAG_with_indicators(input, output, filename="CAG_with_indicators.pdf"
     G.map_concepts_to_indicators()
     G.set_indicator("UN/entities/human/food/food_security", "IPC Phase Classification", "FEWSNET")
     G.set_indicator("UN/entities/food_availability", "Production, Meat indigenous, total", "FEWSNET")
+    G.set_indicator("UN/entities/human/financial/economic/market", "Inflation Rate", "ieconomics.com")
     A = to_agraph(G, indicators=True)
     A.draw(filename, prog="dot")
     with open(output, "wb") as f:
