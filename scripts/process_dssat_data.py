9a36ee4694396583e42769c878329e753b96d6c9
Adarsh <adarsh.pyarelal@gmail.com>
2019-01-28 00:19:20 -0700

    Added rainfall data processing to DSSAT data processing script

--- scripts/process_dssat_data.py
+++ scripts/process_dssat_data.py
@@ -1,47 +1,71 @@
 import sys
 from glob import glob
+from itertools import chain
+from functools import partial
 import numpy as np
 import pandas as pd
 from delphi.utils.fp import take, compose
-from itertools import chain
+
+EXPANSION_DICT = {
+    "nbg": "Northern Bahr el Ghazal",
+    "unity": "Unity",
+    "maiz": "Maize",
+    "sorg": "Sorghum",
+}
 
 
-def process_dssat_table(f):
-    expansion_dict = {
-        "nbg": "Northern Bahr el Ghazal",
-        "unity": "Unity",
-        "maiz": "Maize",
-        "sorg": "Sorghum",
-    }
-
+def process_dssat_table(variable_str, f):
     df = pd.read_csv(
         f, index_col=0, parse_dates=True, infer_datetime_format=True
     )
-    del df["Harvested Area (ha)"]
+    if "Harvested Area (ha)" in df.columns:
+        del df["Harvested Area (ha)"]
     filename_parts = f.split("/")[-1].split("_")
-    df["State"] = expansion_dict[filename_parts[0]]
-    df["Variable"] = f"Production of {expansion_dict[filename_parts[1]]}"
-    df.rename(columns={"Production (t)": "Value"}, inplace=True)
+    df["State"] = EXPANSION_DICT[filename_parts[0]]
+    variable = variable_str.split(" (")[0]
+    df["Variable"] = f"{variable} ({EXPANSION_DICT[filename_parts[1]]})"
+    df.rename(columns={variable_str: "Value"}, inplace=True)
     df.Value = df.Value.astype(int)
     return df
 
 
-def process_dssat_data(input, output):
-    csvs = glob(input + "/*historical*")
-    df = compose(pd.concat, list, chain)(map(process_dssat_table, csvs))
-    df = (
-        df.groupby(["Variable", "State", pd.Grouper(freq="M")])
-        .sum()
-        .reset_index(("Variable", "State"))
-    )
+def process_dssat_data(func, directory, unit):
+    csvs = glob(directory)
+    df = compose(pd.concat, list, chain)(map(func, csvs))
+    if sum:
+        df = (
+            df.groupby(["Variable", "State", pd.Grouper(freq="M")])
+            .sum()
+            .reset_index(("Variable", "State"))
+        )
     df["Country"] = "South Sudan"
-    df["Unit"] = "tons"
+    df["Unit"] = unit
     df["County"] = None
     df["Source"] = "DSSAT"
     df["Year"] = df.index.year
     df["Month"] = df.index.month
+    return df
+
+
+def create_combined_dssat_table(input, output):
+    df = pd.concat(
+        [
+            process_dssat_data(
+                partial(process_dssat_table, "Production (t)"),
+                input + "/*historical*",
+                "tons",
+            ),
+            process_dssat_data(
+                partial(
+                    process_dssat_table, "Average Total Daily Rainfall (mm)"
+                ),
+                input + "/weather/*historical*",
+                "mm",
+            ),
+        ]
+    )
     df.to_csv(output, sep="\t", index=False)
 
 
 if __name__ == "__main__":
-    process_dssat_data(sys.argv[1], sys.argv[2])
+    create_combined_dssat_table(sys.argv[1], sys.argv[2])
