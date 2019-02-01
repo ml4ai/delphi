f03d08d99c1815266480d4472642f39da9deccfa
Pratik Bhandari <pratik.bhd@gmail.com>
2019-02-01 15:11:17 -0700

    added arithmetic operation within write statement

--- tests/data/cases/case.f
+++ tests/data/cases/case.f
@@ -7,14 +7,15 @@ C FORTRAN test file to test CASE constructs
       CHARACTER*1 :: VAR = 'A'
       INTEGER :: X = 40
       INTEGER :: Y
+      INTEGER :: Z = 2
 
       SELECT CASE(VAR)
         CASE('A')
           Y = X/4
-          WRITE(*,*) 'The variable is A and Y is: ', Y
+          WRITE(*,*) 'The variable is A and Y is: ', Y, Y*Z
         CASE('G')
           Y = X/10
-          WRITE(*,*) 'The variable is G and Y is: ', Y
+          WRITE(*,*) 'The variable is G and Y is: ', Y, Y*Z
 
       END SELECT
 
