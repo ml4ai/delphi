4735cd8eb74c3a23120a0d8fabdb8e08e3f2ca02
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-02-01 12:21:19 -0700

    WIP

--- scripts/evaluations/create_cag_from_causemos_output.py
+++ scripts/evaluations/create_cag_from_causemos_output.py
@@ -4,6 +4,12 @@ from delphi.AnalysisGraph import AnalysisGraph
 from delphi.export import to_agraph
 
 G = AnalysisGraph.from_uncharted_json_file(sys.argv[1])
+
+# TODO Make sure to get indicators from DSSAT
+# G.map_concepts_to_indicators()
+# G.parameterize(year = 2017, month=4)
+# G.assemble_transition_model_from_gradable_adjectives()
+# G.sample_from_prior()
 A = to_agraph(G, indicators=True, indicator_values = True)
 A.draw("CauseMos_CAG.pdf", prog="dot")
 
--- scripts/evaluations/create_precipitation_centered_CAG.py
+++ scripts/evaluations/create_precipitation_centered_CAG.py
@@ -2,6 +2,7 @@ import sys
 import pickle
 from delphi.export import to_agraph
 
+
 def create_precipitation_centered_CAG(input, output, filename="CAG.pdf"):
     """ Get a CAG that examines the downstream effects of changes in precipitation. """
 
@@ -10,13 +11,18 @@ def create_precipitation_centered_CAG(input, output, filename="CAG.pdf"):
     G = G.get_subgraph_for_concept(
         "UN/events/weather/precipitation", depth=2, reverse=False
     )
-    print(len(G), len(G.edges()))
     G.prune(cutoff=2)
-    print("pruned", len(G), len(G.edges()))
+
+    # Manually correcting a bad CWMS extraction
+    G.edges[
+        "UN/events/weather/precipitation",
+        "UN/entities/human/infrastructure/transportation/road",
+    ]["InfluenceStatements"][0].obj_delta["polarity"] = -1
     A = to_agraph(G)
     A.draw(filename, prog="dot")
     with open(output, "wb") as f:
         pickle.dump(G, f)
+
 
 if __name__ == "__main__":
     create_precipitation_centered_CAG(sys.argv[1], sys.argv[2])
