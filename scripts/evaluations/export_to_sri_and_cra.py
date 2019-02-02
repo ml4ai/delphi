1145f16a1c74bc1dc71ca185935b363abad8ef77
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-02-01 17:36:25 -0700

    Adding line to create bmi_config.txt in sri/cra export script

--- scripts/evaluations/export_to_sri_and_cra.py
+++ scripts/evaluations/export_to_sri_and_cra.py
@@ -6,5 +6,6 @@ with open(sys.argv[1], 'rb') as f:
     G = pickle.load(f)
 
 
+G.create_bmi_config_file("build/bmi_config.txt")
 with open(sys.argv[2], 'w') as f:
     f.write(json.dumps(G.to_dict(), indent=2))
