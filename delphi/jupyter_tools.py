0340bf3a57ac7412868fc5dceeee6102a8364a6d
Adarsh <adarsh.pyarelal@gmail.com>
2019-02-03 18:57:51 -0600

    wip jupyter tools

--- delphi/jupyter_tools.py
+++ delphi/jupyter_tools.py
@@ -1,13 +1,15 @@
 import os
 from pprint import pprint
 import pandas as pd
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Tuple
 from indra.statements import Influence
 from IPython.display import HTML, Code, Image
+from future.utils import lmap
 from .utils.indra import top_grounding_score
 from .db import engine
 from matplotlib import pyplot as plt
 import seaborn as sns
+
 sns.set_style("darkgrid")
 import json
 import pygments
@@ -40,7 +42,6 @@ def create_statement_inspection_table(sts: List[Influence]):
         obj_polarity = s.obj_delta["polarity"]
         subj_adjectives = s.subj_delta["adjectives"]
         for e in s.evidence:
-            # sent = [e.text for e in s.evidence]
             l.append(
                 (
                     (subj_un_grounding, obj_un_grounding),
@@ -126,59 +127,128 @@ def get_python_shell():
     return shell
 
 
-def get_expected_value(quantity: str, state: str, method="historical", **kwargs):
-
-    if method == "historical":
-        if quantity == "rainfall":
-            units = "mm"
-            results = engine.execute(
-                f"select {quantity.capitalize()} from dssat where `Crop` like 'maize'"
-                f" and `State` like '{state}'"
-            )
-            values = [r[0] for r in results]
-            print(
-                f"Based on historical data, the mean expected {quantity} for the lean "
-                f"season for the state of {state} is {np.mean(values):.2f} {units} "
-                f"with a standard deviation of {np.std(values):.2f} {units}."
-            )
-            xlabel = f"Historical distribution of {quantity} for {state} ({units})"
-        elif quantity == "production":
-            units = "tonnes"
-            crop = kwargs.get('crop')
-            if crop not in ("maize", "sorghum"):
-                raise ValueError("The 'crop' keyword argument must be one of "
-                                 "the following:  ('maize', 'sorghum')")
-            results = engine.execute(
-                f"select {quantity.capitalize()} from dssat where `Crop` like "
-                f"'{crop}' and `State` like '{state}'"
-            )
-            values = [r[0] for r in results]
-            print(
-                f"Based on historical data, the mean expected {crop} {quantity} "
-                f"for the state of {state} is {np.mean(values):.2f} {units} "
-                f"with a standard deviation of {np.std(values):.2f} {units}."
-            )
-            xlabel = f"Historical distribution of {crop} \n{quantity} for {state} ({units})"
-        else:
-            raise ValueError("'quantity' must be one of the following: "
-                             "('rainfall', 'production')")
-    else:
-        raise NotImplementedError("Estimates can currently only be obtained "
-                "using historical data - please set method='historical'. "
-                "In future versions, predictions will be obtained from domain "
-                "model simulations.")
-
+def make_distplot(values, xlabel: str = ""):
     fig, ax = plt.subplots()
     ax.set_xlabel(xlabel)
     sns.distplot(values, ax=ax)
 
 
+def get_percentile_based_conditional_forecast(
+    forecast_var: str = "Production",
+    conditioned_var: str = "Rainfall",
+    crop="maize",
+    percentile: float = 5,
+):
+
+    if forecast_var != "Production":
+        raise NotImplementedError(
+            "Currently, forecast_var must be set to 'Production'."
+        )
+    if conditioned_var != "Rainfall":
+        raise NotImplementedError(
+            "Currently, conditioned_var must be set to 'Rainfall'."
+        )
+
+    if crop not in ("maize", "sorghum"):
+        raise NotImplementedError(
+            "Currently, crop must be one of ['maize', 'sorghum']"
+        )
+    conditioned_var_values = list(
+        engine.execute(
+            f"select `{conditioned_var}` from dssat where `Crop` is '{crop}'"
+        )
+    )
+    forecast_var_values = list(
+        engine.execute(
+            f"select `{forecast_var}` from dssat"
+            f" where `{conditioned_var}` < "
+            f"{np.percentile(conditioned_var_values, percentile)}"
+        )
+    )
+
+    # TODO Make this more general
+    unit = "tons"
+    make_distplot(forecast_var_values, f"{forecast_var} of {crop} ({unit})")
+
+
+def get_expected_distribution(
+    indicator: str,
+    state: Optional[str] = None,
+    month_range: Optional[Tuple[int, int]] = None,
+    unit: Optional[str] = None,
+    method: str = "historical",
+    table: str = "indicator",
+):
+    query_parts = [
+        f"select * from '{table}' where `Variable` like '{indicator}'"
+    ]
+
+    if state is not None:
+        query_parts.append(f"and `State` like '{state}'")
+    if month_range is not None:
+        query_parts.append(
+            f"and `Month` between {month_range[0]} and {month_range[1]}"
+        )
+
+    query = " ".join(query_parts)
+    results = list(engine.execute(query))
+    values = [float(r["Value"]) for r in results]
+    units = list(set([r["Unit"] for r in results]))
+    if len(units) > 1:
+        raise ValueError(
+            "Multiple indicator variables found with the same name and "
+            "different units. Please specify the units."
+        )
+
+    make_distplot(values, f"{indicator} ({units[0]})")
+
+
 def print_commit_hash_message():
     commit_hash = sp.check_output(["git", "rev-parse", "HEAD"])
-    print(f"This notebook has been rendered with commit {commit_hash[:-1]} of"
-            " Delphi.")
+    print(
+        f"This notebook has been rendered with commit {commit_hash[:-1]} of"
+        " Delphi."
+    )
 
-def run_experiment(G, n0, partial_t_n0, n1, xlim = (0,1)):
+
+def run_experiment(G, intervened_node, delta, n_timesteps: int):
+    G.create_bmi_config_file()
+    s0 = pd.read_csv(
+        "bmi_config.txt", index_col=0, header=None, error_bad_lines=False
+    )[1]
+    s0.loc[f"∂({intervened_node})/∂t"] = delta
+    s0.to_csv("bmi_config.txt", index_label="variable")
+    G.initialize()
+    indicator_values = {
+        n[0]: {
+            "name": list(n[1]["indicators"].keys())[0],
+            "xs": [0 for _ in range(G.res)],
+            "ys": list(list(n[1]["indicators"].values())[0].samples),
+        }
+        for n in G.nodes(data=True)
+    }
+
+    for t in range(1, n_timesteps + 1):
+        G.update()
+        for n in G.nodes(data=True):
+            # TODO Fix this - make it more general (or justify it theoretically)
+            ys = lmap(
+                lambda x: x,
+                list(n[1]["indicators"].values())[0].samples,
+            )
+            indicator_values[n[0]]["ys"].extend(ys)
+            indicator_values[n[0]]["xs"].extend([t for _ in ys])
+
+    for n in G.nodes():
+        fig, ax = plt.subplots()
+        sns.lineplot(
+            indicator_values[n]["xs"], indicator_values[n]["ys"], ax=ax, ci=20
+        )
+        ax.set_ylabel(indicator_values[n]["name"])
+        ax.set_xlabel("time step number")
+
+
+def perform_intervention(G, n0, partial_t_n0, n1, xlim=(0, 1)):
     G.construct_default_initial_state()
     G.create_bmi_config_file()
     s0 = pd.read_csv(
@@ -194,7 +264,7 @@ def run_experiment(G, n0, partial_t_n0, n1, xlim = (0,1)):
     xmax = 2
     vals[n1] = [x for x in G.nodes[n1]["rv"].dataset if 0 < x < xmax]
     fig, ax = plt.subplots()
-    ax.set_xlim(0,xmax)
+    ax.set_xlim(0, xmax)
     ax.set_xlabel(n1)
     ax.hist(vals[n1], density=True, bins=30)
     print(f"Standard deviation, σ = {np.std(vals[n1]):.2f}")
