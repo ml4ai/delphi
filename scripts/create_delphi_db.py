c36200fc76694f0a03ab60796151e27a5f0884b5
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-02-06 13:08:45 -0700

    Refactoring create_delphi_db.py script to work better with Makefiles

--- scripts/create_delphi_db.py
+++ scripts/create_delphi_db.py
@@ -1,16 +1,10 @@
 import sys
 import numpy as np
 import pandas as pd
-from delphi.paths import (
-    data_dir,
-    south_sudan_data,
-    adjectiveData,
-    concept_to_indicator_mapping,
-)
 from pathlib import Path
 from sqlalchemy import create_engine
 
-ENGINE = create_engine(f"sqlite:///{sys.argv[3]}", echo=False)
+ENGINE = create_engine(f"sqlite:///{sys.argv[5]}", echo=False)
 
 def insert_table(df, table_name):
     df.to_sql(table_name, con=ENGINE, if_exists="replace")
@@ -21,7 +15,7 @@ def create_indicator_table(indicator_table):
     insert_table(df, "indicator")
 
 
-def create_adjectiveData_table():
+def create_adjectiveData_table(adjectiveData):
     df = pd.read_csv(adjectiveData, index_col=False, delim_whitespace=True)
     insert_table(df, "gradableAdjectiveData")
 
@@ -42,12 +36,12 @@ def create_concept_to_indicator_mapping_table(mapping_table):
 
     insert_table(df, "concept_to_indicator_mapping")
 
-def create_dssat_data_table():
+def create_dssat_data_table(dssat_aggregated_data_dir):
     state_dict = {"NBG":"Northern Bahr El Ghazal","Unity":"Unity"}
     crop_dict = {"MAIZ":"maize", "SORG":"sorghum"}
     dfs = []
     for filename in ("NBG_MAIZ", "NBG_SORG", "Unity_MAIZ", "Unity_SORG"):
-        df = pd.read_csv(Path("data") / "SSD_csv" / f"{filename}.csv", usecols=[0, 1, 2])
+        df = pd.read_csv(f"{dssat_aggregated_data_dir}/{filename}.csv", usecols=[0, 1, 2])
         state, crop = filename.split("_")
         df["State"], df["Crop"]  = state_dict[state], crop_dict[crop]
         df["Source"] = "DSSAT"
@@ -56,7 +50,7 @@ def create_dssat_data_table():
     insert_table(pd.concat(dfs), "dssat")
 
 if __name__ == "__main__":
-    create_dssat_data_table()
     create_indicator_table(sys.argv[1])
-    create_adjectiveData_table()
+    create_adjectiveData_table(sys.argv[3])
     create_concept_to_indicator_mapping_table(sys.argv[2])
+    create_dssat_data_table(sys.argv[4])
