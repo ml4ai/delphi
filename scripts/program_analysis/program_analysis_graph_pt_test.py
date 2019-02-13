d9a83e982b9a1d9f771f71d39729403cf5aee9c0
Adarsh <adarsh.pyarelal@gmail.com>
2019-02-13 13:51:47 -0700

    Removing initialize call in pt test

--- scripts/program_analysis/program_analysis_graph_pt_test.py
+++ scripts/program_analysis/program_analysis_graph_pt_test.py
@@ -2,10 +2,9 @@ from delphi.GrFN.ProgramAnalysisGraph import ProgramAnalysisGraph
 from delphi.visualization import visualize
 
 
-filepath = "../../tests/data/crop_yield.f"
+filepath = "../../tests/data/PETPT.for"
 G = ProgramAnalysisGraph.from_fortran_file(filepath)
-G.initialize()
 print(G.nodes)
 print([n for n, d in G.out_degree() if d == 0])
 print([n for n, d in G.in_degree() if d == 0])
-# visualize(G, save=True)
+visualize(G, save=True)
