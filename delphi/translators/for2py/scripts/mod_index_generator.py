a4a3531654c736cafac16176a3b0042b5497db27
Pratik Bhandari <pratik.bhd@gmail.com>
2019-02-19 10:40:46 -0700

    index generator for module mapping

--- delphi/translators/for2py/scripts/mod_index_generator.py
+++ delphi/translators/for2py/scripts/mod_index_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-'''
+"""
 Generates a module index file. This file describes each module used in a program run.
 The information about each module is represented as a json dictionary and has the
 following fields:
@@ -14,137 +14,162 @@ following fields:
 The procedure mapping for each subprogram p defined in module M is a mapping from
 each possible tuple of argument types for p to the function to invoke for that argument
 type tuple.
-'''
+
+Author: Pratik Bhandari
+Date:   02/19/2019
+"""
 
 import sys
 import xml.etree.ElementTree as ET
 from typing import List, Dict
 import re
-
-class ParseState(object):
-    """This class defines the state of the XML tree parsing
-    at any given root. For any level of the tree, it stores
-    the module under which it resides along with the
-    module metadata."""
-
-    def __init__(self, module=None):
-        self.module = module if module is not None else {}
-        self.args = (
-            [arg["name"] for arg in self.module["args"]]
-            if "args" in self.module
-            else []
-        )
-
-    def copy(self, module=None):
-        return ParseState(
-            self.module if module == None else module
-        )
+import json
 
 class moduleGenerator(object):
     def __init__(self):
+
         self.asts = []
         self.useList = []
         self.functionList = []
         self.entryPoint = []
-        self.fileName = str()
+        self.current_context = ''
+        self.fileName = ''
 
+        # Initialize all the dictionaries which we will be writing to our file
+        self.modules = []
+        self.exports = {}
+        self.uses = {}
+        self.imports = {}
+        self.private = {}
+        self.public = {}
+        self.subpgms = {}
+        self.syms = {}
 
-    def parseTree(self, root, state: ParseState) -> bool:
+    def parseTree(self, root) -> bool:
+        for item in root.iter():
+            if item.tag == "file":
+                file_name = item.attrib["path"]
+                file_name = file_name.split('/')[-1]
+                file_reg = r'^(.*)_processed(\..*)$'
+                match = re.match(file_reg, file_name)
+                if match:
+                    self.fileName = match.group(1) + match.group(2)
 
-        if root.tag == "module":
-            return self.process_module(root, state)
+            elif item.tag.lower() in ["module", "program"]:
+                self.current_context = item.attrib["name"].lower()
+                self.modules.append(item.attrib["name"].lower())
 
-        elif root.tag == "use":
-            return self.process_use(root, state)
+            elif item.tag.lower() == "variable":
+                if item.attrib.get("name"):
+                    if self.public.get(self.current_context):
+                        self.public[self.current_context].append(item.attrib["name"].lower())
+                    else:
+                        self.public[self.current_context] = [item.attrib["name"].lower()]
 
-        elif root.tag == "file":
-            return self.process_file(root, state)
+            elif item.tag.lower() in ["subroutine", "function"]:
+                if self.subpgms.get(self.current_context):
+                    self.subpgms[self.current_context].append(item.attrib["name"].lower())
+                else:
+                    self.subpgms[self.current_context] = [item.attrib["name"].lower()]
 
-        elif root.tag in ["program", "subroutine"]:
-            return self.process_subroutine_or_program(root, state)
+            elif item.tag.lower() == "declaration":
+                self.possible_private(item)
 
-        else:
-            status = True
-            for node in root:
-                status = self.parseTree(node, state)
-            return status
+            elif item.tag.lower() == "use":
+                hasOnly = False
+                only_symbols = []
+                for child in item:
+                    if child.tag.lower() == "only":
+                        hasOnly = True
+                        for innerChild in child:
+                            if innerChild.tag.lower() == "name" and hasOnly:
+                                only_symbols.append(innerChild.attrib["id"].lower())
 
-    def process_subroutine_or_program(self, root, state):
-        # useList stores the modules imported by the current module/program
-        self.useList = []
+                if self.uses.get(self.current_context):
+                    if len(only_symbols) != 0:
+                        self.uses[self.current_context].append({item.attrib["name"].lower(): only_symbols})
+                    else:
+                        self.uses[self.current_context].append({item.attrib["name"].lower(): ['*']})
+                else:
+                    if len(only_symbols) != 0:
+                        self.uses[self.current_context] = [{item.attrib["name"].lower(): only_symbols}]
+                    else:
+                        self.uses[self.current_context] = [{item.attrib["name"].lower(): ['*']}]
 
-        for node in root:
-            status = self.parseTree(node, state)
-
-    def process_module(self, root, state):
-        module = dict()
-        module["name"] = root.attrib["name"]
-        module["source_file"] = self.fileName
-
-        self.useList = []
-
-        for node in root:
-            if node.tag == "header":
-                continue
-            elif node.tag == "body":
-                sub_state = state.copy(module)
-                status = self.parseTree(node, sub_state)
-
-        # At this point, the module tag has been recursively iterated to find all use statements
-        module["uses"] = self.useList
-
-        print(module)
-        # Append the module metadata to our final ast
-        self.asts.append(module)
-
-        return status
-
-    # In the case that a USE statement is encountered, populate useList with the module that is imported
-    def process_use(self, root, state):
-        self.useList.append(root.attrib["name"])
-        return True
-
-    # This function finds the full path of the fortran program (module or main program) and stores
-    # it in the variable fileName.
-    def process_file(self, root, state):
-        file_name = root.attrib["path"]
-        file_name = file_name.split('/')[-1]
-        file_reg = r'^(.*)_processed(\..*)$'
-        match = re.match(file_reg, file_name)
-        if match:
-            self.fileName = match.group(1) + match.group(2)
-        for node in root:
-            self.parseTree(node, state)
+        self.populate_syms()
+        self.populate_exports()
+        self.populate_imports()
 
         return True
 
-    # This function is still under development
+    def populate_syms(self):
+        for item in self.modules:
+            self.syms[item] = self.public.get(item, []) + self.private.get(item, []) + self.subpgms.get(item, [])
+
+    def populate_exports(self):
+        for item in self.modules:
+            interim = self.imports.get(item, []) + self.syms.get(item, [])
+            self.exports[item] = [x for x in interim if x not in self.private.get(item, [])]
+
+    def populate_imports(self):
+        for module in self.uses:
+            for use_item in self.uses[module]:
+                for key in use_item:
+                    if len(use_item[key]) == 1 and use_item[key][0] == '*':
+                        if self.imports.get(module):
+                            self.imports[module].append({key: self.exports[key]})
+                        else:
+                            self.imports[module] = [{key: self.exports[key]}]
+                    elif len(use_item[key]) > 1:
+                        if self.imports.get(module):
+                            self.imports[module].append({key: use_item[key]})
+                        else:
+                            self.imports[module] = [{key: use_item[key]}]
+
+    def possible_private(self, item):
+        private_status = False
+        for child in item.iter():
+            if child.tag.lower() == "access-spec" and child.attrib.get("keyword").lower() == "private":
+                private_status = True
+            if child.tag.lower() == "name" and private_status:
+                if self.private.get(self.current_context):
+                    self.private[self.current_context].append(child.attrib["id"].lower())
+                else:
+                    self.private[self.current_context] = [child.attrib["id"].lower()]
+
     def analyze(self, tree: ET.ElementTree) -> List:
+        status = self.parseTree(tree)
+        outputDict = {}
+        if status:
+            outputDict['FileName'] = self.fileName
+            outputDict['Modules'] = self.modules
+            outputDict['Exports'] = self.exports
+            outputDict['Uses'] = self.uses
+            outputDict['Imports'] = self.imports
+            outputDict['Private'] = self.private
+            outputDict['Public'] = self.public
+            outputDict['Subpgms'] = self.subpgms
+            outputDict['Syms'] = self.syms
 
-        status = self.parseTree(tree, ParseState())
-
-        return self.asts
+            with open('mod_gen.json','w') as fd:
+                json.dump(outputDict, fd, indent=4)
 
 # Return the root of the XML ast (of the Fortran program) generated by the OFP so that subsequent recursion can
 # be applied to it
 def get_tree(file: str) -> ET.ElementTree:
     return ET.parse(file).getroot()
 
+
 # Get the root of the XML ast, instantiate the moduleGenerator and start the analysis process.
 def get_index(xml_file: str):
     tree = get_tree(xml_file)
     generator = moduleGenerator()
-    output_dict = generator.analyze(tree)
-    print(output_dict)
+    status = generator.analyze(tree)
 
 
 if __name__ == "__main__":
-
     if len(sys.argv) < 2:
         sys.stderr.write(f"Usage: {sys.argv[0]} filename\n")
         sys.exit(1)
 
     get_index(sys.argv[1])
-
-
-
