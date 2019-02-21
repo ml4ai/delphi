4e1f7c569e0406c3fe83676f37aa9625a93a9012
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-02-21 15:27:53 -0700

    Added POST specification

--- scripts/rest_api_test_scripts/test_createICM.sh
+++ scripts/rest_api_test_scripts/test_createICM.sh
@@ -1,2 +1,2 @@
-curl -X POST -d @data/causemos_cag.json -H "Content-Type:application/json"\
+curl -X POST -d @../data/causemos_cag.json -H "Content-Type:application/json"\
   http://vanga.sista.arizona.edu/delphi/icm
