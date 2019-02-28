eeafa6a946c283392f23a2a9fa7b64b4a31b27aa
Paul Hein <pauldhein@email.arizona.edu>
2019-02-28 08:01:39 -0700

    Renaming script to grfn analysis

--- scripts/program_analysis/grfn_analysis.py
+++ scripts/program_analysis/grfn_analysis.py
@@ -1,4 +1,3 @@
-from delphi.GrFN.scopes import Scope
 from delphi.visualization import visualize
 from delphi.GrFN.GroundedFunctionNetwork import GroundedFunctionNetwork
 
@@ -7,7 +6,7 @@ filepath = "../../tests/data/PETPT.for"
 G = GroundedFunctionNetwork.from_fortran_file(filepath)
 print(G.nodes)
 print(G.edges)
-# print(G.nodes)
+
 # print([n for n, d in G.out_degree() if d == 0])
 # print([n for n, d in G.in_degree() if d == 0])
-# visualize(G, save=True)
+visualize(G, save=True, filename="petpt.pdf")
