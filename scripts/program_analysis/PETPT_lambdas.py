2870e63f502fcd3b0cb561f337c78c2b37a510df
Paul Hein <pauldhein@email.arizona.edu>
2019-03-13 09:24:01 -0700

    Some edited output files for discussion

---
+++ scripts/program_analysis/PETPT_lambdas.py
@@ -0,0 +1,46 @@
+import math
+
+def petpt__assign__td_1(tmax, tmin):
+    return ((0.6*tmax)+(0.4*tmin))
+
+def petpt__condition__IF_1_0(xhlai):
+    return (xhlai<=0.0)
+
+def petpt__assign__albedo_1(msalb):
+    return msalb
+
+def petpt__assign__albedo_2(msalb, xhlai):
+    return (0.23-((0.23-msalb)*math.exp(-((0.75*xhlai)))))
+
+def petpt__decision__albedo_3(IF_1_0, albedo_2, albedo_1):
+    return albedo_1 if IF_1_0 else albedo_2
+
+def petpt__assign__slang_1(srad):
+    return (srad*23.923)
+
+def petpt__assign__eeq_1(slang, albedo, td):
+    return ((slang*(0.000204-(0.000183*albedo)))*(td+29.0))
+
+def petpt__assign__eo_0(eeq):
+    return (eeq*1.1)
+
+def petpt__condition__IF_2_0(tmax):
+    return (tmax>35.0)
+
+def petpt__assign__eo_1(eeq, tmax):
+    return (eeq*(((tmax-35.0)*0.05)+1.1))
+
+def petpt__condition__IF_3_0(tmax):
+    return (tmax<5.0)
+
+def petpt__assign__eo_2(eeq, tmax):
+    return ((eeq*0.01)*math.exp((0.18*(tmax+20.0))))
+
+def petpt__decision__eo_3(IF_2_0, eo_0, eo_1):
+    return eo_1 if IF_2_0 else eo_0
+
+def petpt__decision__eo_4(IF_3_0, eo_2, eo_3):
+    return eo_2 if IF_3_0 else eo_3
+
+def petpt__assign__eo_5(eo):
+    return max(eo, 0.0001)
---
+++ scripts/program_analysis/PETPT_torch_lambdas.py
@@ -0,0 +1,44 @@
+# import numpy as np
+# from numba import jit
+import torch
+
+
+# @jit
+def petpt__assign__td_1(tmax, tmin):
+    return ((0.6*tmax)+(0.4*tmin))
+
+
+# @jit
+def petpt__decision__albedo_1(xhlai, msalb):
+    return torch.where(xhlai <= 0.0, 0.23-((0.23-msalb)*torch.exp(-((0.75*xhlai)))), msalb)
+
+
+# @jit
+def petpt__assign__slang_1(srad):
+    return srad*23.923
+
+
+# @jit
+def petpt__assign__eeq_1(slang, albedo, td):
+    return (slang*(0.000204-(0.000183*albedo)))*(td+29.0)
+
+
+# @jit
+def petpt__assign__eo_0(eeq):
+    return eeq*1.1
+
+
+# @jit
+def petpt__decision__eo_1(tmax, eeq, eo_0):
+    return torch.where(tmax > 35.0, eeq*(((tmax-35.0)*0.05)+1.1), eo_0)
+
+
+# @jit
+def petpt__decision__eo_2(tmax, eeq, eo_1):
+    return torch.where(tmax < 5.0, (eeq*0.01)*torch.exp((0.18*(tmax+20.0))), eo_1)
+
+
+# @jit
+def petpt__assign__eo_3(eo):
+    eo = eo.reshape((len(eo), 1))
+    return torch.max(torch.cat((eo, torch.full_like(eo, 0.0001)), dim=1), dim=1)
