73b7c4e8c4df714b0f5da44f40a3d1093987fca8
Paul Hein <pauldhein@email.arizona.edu>
2019-03-13 09:24:26 -0700

    Some performance testing

--- scripts/program_analysis/grfn_analysis.py
+++ scripts/program_analysis/grfn_analysis.py
@@ -1,18 +1,60 @@
 import importlib
 import json
+import time
 
-from delphi.visualization import visualize
+import numpy as np
+import torch
+
+# from delphi.visualization import visualize
 from delphi.GrFN.GroundedFunctionNetwork import GroundedFunctionNetwork
 
 
-# filepath = "../../tests/data/PETPT.for"
-# G = GroundedFunctionNetwork.from_fortran_file(filepath)
+# lambdas = importlib.__import__("PETPT_lambdas")
+# pgm = json.load(open("PETPT.json", "r"))
 
-lambdas = importlib.__import__("PETPT_lambdas")
-pgm = json.load(open("PETPT.json", "r"))
+lambdas = importlib.__import__("PETPT_numpy_lambdas")
+pgm = json.load(open("PETPT_numpy.json", "r"))
+
 G = GroundedFunctionNetwork.from_dict(pgm, lambdas)
-# print(G)
-values = {name: 1 for name in G.inputs}
+
+# start = time.time()
+# for i in range(1000):
+#     values = {name: np.random.rand() for name in G.inputs}
+#     G.run(values)
+#     G.clear()
+# print(f"Full compute time: {1000 * (time.time() - start):2.4f}ms")
+
+# values = {
+#     "petpt::msalb_0": np.random.rand(1000),
+#     "petpt::srad_0": np.random.randint(1, 100, size=1000),
+#     "petpt::tmax_0": np.random.randint(30, 40, size=1000),
+#     "petpt::tmin_0": np.random.randint(10, 15, size=1000),
+#     "petpt::xhlai_0": np.random.rand(1000)
+# }
+# result = G.run(values)
+# G.clear()
+# result = G.run(values)
+# G.clear()
+values = {
+    "petpt::msalb_0": np.random.rand(10000000),
+    "petpt::srad_0": np.random.randint(1, 100, size=10000000),
+    "petpt::tmax_0": np.random.randint(30, 40, size=10000000),
+    "petpt::tmin_0": np.random.randint(10, 15, size=10000000),
+    "petpt::xhlai_0": np.random.rand(10000000)
+}
 result = G.run(values)
-print(f"Final result: {result}")
+print(f"Numpy Final result: {result}")
+print(f"Numpy Size of final result: {result.shape}")
+
+lambdas = importlib.__import__("PETPT_torch_lambdas")
+pgm = json.load(open("PETPT_numpy.json", "r"))
+G = GroundedFunctionNetwork.from_dict(pgm, lambdas)
+t_values = {k: torch.Tensor(v) for k, v in values.items()}
+if torch.cuda.is_available():
+    t_values = {k: v.cuda() for k, v in t_values.items()}
+result = G.run(t_values)
+print(f"Torch Final result: {result}")
+print(f"Torch Size of final result: {result[0].size()}")
+
 # visualize(G, save=True, filename="petpt.pdf")
+# visualize(G, save=True, filename="petpt_numpy.pdf")
