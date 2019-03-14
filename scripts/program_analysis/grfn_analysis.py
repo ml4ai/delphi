77c7072d757fd28018fe8856192d72b2dc566fee
Paul Hein <pauldhein@email.arizona.edu>
2019-03-14 06:42:13 -0700

    Updated analysis values

--- scripts/program_analysis/grfn_analysis.py
+++ scripts/program_analysis/grfn_analysis.py
@@ -26,11 +26,11 @@ pgm = json.load(open("PETPT_numpy.json", "r"))
 G = GroundedFunctionNetwork.from_dict(pgm, lambdas)
 
 values = {
-    "petpt::msalb_0": np.random.rand(1000000000),
-    "petpt::srad_0": np.random.randint(1, 100, size=1000000000),
-    "petpt::tmax_0": np.random.randint(30, 40, size=1000000000),
-    "petpt::tmin_0": np.random.randint(10, 15, size=1000000000),
-    "petpt::xhlai_0": np.random.rand(1000000000)
+    "petpt::msalb_0": np.random.rand(1000000),
+    "petpt::srad_0": np.random.randint(1, 100, size=1000000),
+    "petpt::tmax_0": np.random.randint(30, 40, size=1000000),
+    "petpt::tmin_0": np.random.randint(10, 15, size=1000000),
+    "petpt::xhlai_0": np.random.rand(1000000)
 }
 result = G.run(values)
 print(f"Numpy Final result: {result}")
