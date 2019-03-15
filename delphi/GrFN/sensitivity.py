834a1eb7e35d10a06795b6c62a71a35998b6835b
Paul Hein <pauldhein@email.arizona.edu>
2019-03-15 12:09:09 -0700

    Added timeit info to sensitivity

--- delphi/GrFN/sensitivity.py
+++ delphi/GrFN/sensitivity.py
@@ -3,7 +3,10 @@ from SALib.analyze import sobol, fast
 import numpy as np
 import torch
 
+from delphi.GrFN.GroundedFunctionNetwork import timeit
 
+
+@timeit
 def sobol_analysis(network, num_samples, prob_def):
     print("Sampling via Saltelli...")
     samples = saltelli.sample(prob_def, num_samples, calc_second_order=True)
@@ -15,6 +18,7 @@ def sobol_analysis(network, num_samples, prob_def):
     return sobol.analyze(prob_def, Y, print_to_console=True)
 
 
+@timeit
 def FAST_analysis(network, num_samples, prob_def):
     print("Sampling via FAST sampler...")
     samples = fast_sampler.sample(prob_def, num_samples)
