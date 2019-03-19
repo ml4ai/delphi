42f5927237486465c3235a978dd4fdd7d3f67d99
Paul Hein <pauldhein@email.arizona.edu>
2019-03-19 10:36:21 -0700

    Restoring translator

--- delphi/translators/for2py/data/PETASCE.py
+++ delphi/translators/for2py/data/PETASCE.py
@@ -6,7 +6,7 @@ def PETASCE(CANHT: List[float], DOY: List[int], MSALB: List[float],
             MEEVP: List[str], SRAD: List[float], TDEW: List[float],
             TMAX: List[float], TMIN: List[float], WINDHT: List[float],
             WINDRUN: List[float], XHLAI: List[float], XLAT: List[float],
-            XELEV: List[float], EO: List[float]):
+            XELEV: List[float]):
 
     # DOY: List[int] = [0]
 
@@ -134,7 +134,6 @@ def PETASCE(CANHT: List[float], DOY: List[int], MSALB: List[float],
     # CALL GET('SPAM', 'KCBMIN', KCBMIN)
     # CALL GET('SPAM', 'KCBMAX', KCBMAX)
 
-    # TODO: Update these values
     SKC: List[float] = [1.0]
     KCBMIN: List[float] = [1.0]
     KCBMAX: List[float] = [1.0]
@@ -173,3 +172,5 @@ def PETASCE(CANHT: List[float], DOY: List[int], MSALB: List[float],
     # CALL PUT('SPAM', 'KCB', KCB)
     # CALL PUT('SPAM', 'KE', KE)
     # CALL PUT('SPAM', 'KC', KC)
+
+    return EO[0]
