ecc279aef2b6f1bfa7e802446cafdc63dc65eaf2
Adarsh <adarsh.pyarelal@gmail.com>
2019-03-26 09:28:57 -0700

    Splitting out editor JS

---
+++ apps/CodeExplorer/templates/editor.js
@@ -0,0 +1,37 @@
+            $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
+            var editor_for_source_code = CodeMirror.fromTextArea(
+            document.getElementById('flask-codemirror-source_code'), {
+                  "lineNumbers": "true",
+                  "viewportMargin": 800,
+                  "mode": "fortran"
+              }
+            );
+            function getCode(programName) {
+              fetch('static/example_programs/'+programName+'.f')
+                .then(
+                  function(response) {
+                    if (response.status !== 200) {
+                      console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                      return;
+                    }
+
+                    // Examine the text in the response
+                    response.text().then(function(data) {
+                      editor_for_source_code.getDoc().setValue(data);
+                    });
+                  }
+                )
+                .catch(function(err) {
+                  console.log('Fetch Error :-S', err);
+                });
+            }
+            $("#cropYield").click(function(){
+              {% set programName = "cropYield" %}
+              getCode("cropYield");
+            });
+            $("#petPT").click(function(){
+              {% set programName = "petpt" %}
+              getCode("petPT");
+            });
+
