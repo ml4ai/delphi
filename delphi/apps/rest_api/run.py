301b656d6d485f905cacb9856c4c563a866a7e7b
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-03-29 14:44:23 -0700

    Removed fuzzywuzzy as a dependency, moved cli.py under apps, added rest_api to console_scripts

--- delphi/apps/rest_api/run.py
+++ delphi/apps/rest_api/run.py
@@ -2,7 +2,10 @@
 from delphi.paths import db_path
 from delphi.apps.rest_api import create_app
 
-if __name__ == "__main__":
+def main():
     app = create_app(debug=True)
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
     app.run(host="127.0.0.1", port=5000)
+
+if __name__ == "__main__":
+    main()
