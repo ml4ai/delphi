c83aca44648fac7b6cae0e8fd9fbffbbb3978c95
Adarsh <adarsh.pyarelal@gmail.com>
2019-03-31 22:07:43 -0700

    Updated make_figures script and README.md

--- docs/make_figures.py
+++ docs/make_figures.py
@@ -1,5 +1,9 @@
 from delphi import AnalysisGraph
 
-G = AnalysisGraph.from_text("Conflict increases displacement.")
+G = AnalysisGraph.from_text(
+    "Significantly increased conflict seen in South Sudan forced many families"
+    "to flee in 2017.")
+G.map_concepts_to_indicators()
+G.parameterize(country="South Sudan", year=2017, month=4)
 A = G.to_agraph()
 A.draw("CAG.png", prog="dot")
