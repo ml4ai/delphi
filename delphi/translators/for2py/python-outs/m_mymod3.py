d649a41a139616b5b7f031a905baceae4579c759
Terrence Lim <hlim1@email.arizona.edu>
2019-05-14 17:36:10 -0700

    Removed unnecessary files and directories

--- delphi/translators/for2py/python-outs/m_mymod3.py
+++
@@ -1,9 +0,0 @@
-import sys
-from typing import List
-import math
-from delphi.translators.for2py.format import *
-from delphi.translators.for2py.arrays import *
-from dataclasses import dataclass
-
-
-x: List[int] = [1234]
