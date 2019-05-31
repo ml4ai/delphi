79647d24880b13c5d9faeda2ba043951901b4db2
Loren Champlin <loren_champlin@Lorens-MacBook-Pro.local>
2019-05-31 16:26:24 -0700

    Now set up to create human migration centered CAG

--- scripts/hackathon/create_scenario_centered_CAG.py
+++ scripts/hackathon/create_scenario_centered_CAG.py
@@ -2,24 +2,25 @@ import sys
 import pickle
 
 
-def create_precipitation_centered_CAG(input, output):
-    """ Get a CAG that examines the downstream effects of changes in precipitation. """
+def create_scenario_centered_CAG(input, output):
+    """ Get a CAG that examines the upstream effects of changes in human
+    migration. """
 
     with open(input, "rb") as f:
         G = pickle.load(f)
     G = G.get_subgraph_for_concept(
-        "UN/events/weather/precipitation", depth=2, reverse=False
+        "UN/events/human/human_migration", depth=2, reverse=True
     )
     G.prune(cutoff=2)
 
     # Manually correcting a bad CWMS extraction
-    G.edges[
-        "UN/events/weather/precipitation",
-        "UN/entities/human/infrastructure/transportation/road",
-    ]["InfluenceStatements"][0].obj_delta["polarity"] = -1
+    #G.edges[
+    #    "UN/events/weather/precipitation",
+    #    "UN/entities/human/infrastructure/transportation/road",
+   # ]["InfluenceStatements"][0].obj_delta["polarity"] = -1
     with open(output, "wb") as f:
         pickle.dump(G, f)
 
 
 if __name__ == "__main__":
-    create_precipitation_centered_CAG(sys.argv[1], sys.argv[2])
+    create_scenario_centered_CAG(sys.argv[1], sys.argv[2])
