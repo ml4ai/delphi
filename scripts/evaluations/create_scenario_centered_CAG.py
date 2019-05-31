c7981604516926007558047d779a8ae5a4e43073
Loren Champlin <loren_champlin@Lorens-MacBook-Pro.local>
2019-05-30 20:33:54 -0700

    Changed name from create_precipitation_centered_CAG.py

---
+++ scripts/evaluations/create_scenario_centered_CAG.py
@@ -0,0 +1,25 @@
+import sys
+import pickle
+
+
+def create_scenario_centered_CAG(input, output):
+    """ Get a CAG that examines the downstream effects of changes in precipitation. """
+
+    with open(input, "rb") as f:
+        G = pickle.load(f)
+    G = G.get_subgraph_for_concept(
+        "UN/events/weather/precipitation", depth=2, reverse=False
+    )
+    G.prune(cutoff=2)
+
+    # Manually correcting a bad CWMS extraction
+    G.edges[
+        "UN/events/weather/precipitation",
+        "UN/entities/human/infrastructure/transportation/road",
+    ]["InfluenceStatements"][0].obj_delta["polarity"] = -1
+    with open(output, "wb") as f:
+        pickle.dump(G, f)
+
+
+if __name__ == "__main__":
+    create_scenario_centered_CAG(sys.argv[1], sys.argv[2])
