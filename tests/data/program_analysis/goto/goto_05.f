16f92c5a2b4a871f2849485abe027bf57a432355
Saumya K. Debray <saumya.debray@gmail.com>
2019-06-04 09:12:57 -0700

    modified goto_0[4-7].f to use conditional gotos

--- tests/data/program_analysis/goto/goto_05.f
+++ tests/data/program_analysis/goto/goto_05.f
@@ -1,33 +1,27 @@
 C     File: goto_05.f
-C     A simple program with multiple gotos at the top level of the program,
-C     including some gotos to gotos.
+C     A simple program with a single forward conditional goto.
 C     The program computes and prints out the values of n! for n in [1,10].
-
+      
       program factorial
       implicit none
 
       integer i, n, fact
 
-      goto 222
-      
- 333  goto 444
-      
- 111  i = i + 1
-      fact = fact * i
-
-      write (*, 10) i, fact
-      if (i .eq. n) then
-         stop
-      endif
-
-      goto 111
-
- 222  goto 333
-
- 444  i = 0
-      fact = 1
+      i = 0
       n = 10
-      goto 111
+      fact = 0
 
+      do i = 1, n
+         if (fact .ne. 0) goto 222
+
+         fact = fact + 1    ! this line is executed exactly once
+
+ 222     fact = fact * i
+
+         write (*, 10) i, fact
+      end do
+
+      stop
  10   format('i = ', I3, '; fact = ', I8)
+
       end program factorial
--- tests/data/program_analysis/goto/goto_07.f
+++ tests/data/program_analysis/goto/goto_07.f
@@ -1,24 +1,27 @@
 C     File: goto_07.f
-C     A simple program with a goto within a DO loop.
+C     A simple program with a single forward conditional goto and
+C     a single backward unconditional goto.
 C     The program computes and prints out the values of n! for n in [1,10].
 
       program factorial
       implicit none
 
-      integer i, j, k, n, fact
-      n = 10
-      j = 27
-      fact = 1
-      
-      do i = 1, n
-         goto 111
-         
-         j = 33                 ! this stmt is never executed
-         
- 111     k = i+j-27
-         fact = fact * k
-         write (*, 10) i, fact
-      end do
+      integer i, n, fact
 
+      i = 0
+      n = 10
+      fact = 1
+
+ 111  i = i + 1
+      if (i .gt. n) goto 222
+
+      fact = fact * i
+
+      write (*, 10) i, fact
+
+      goto 111
+
+ 222  stop
  10   format('i = ', I3, '; fact = ', I8)
+
       end program factorial
