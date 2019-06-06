a61254bbb007ab9edd101d9dfab45c6620a8a94c
Pratik Bhandari <pratik.bhd@gmail.com>
2019-06-06 14:01:19 -0700

    Setting maximum line length to 80 and chaning comment styles.

--- delphi/translators/for2py/floatNumpy.py
+++ delphi/translators/for2py/floatNumpy.py
@@ -1,8 +1,9 @@
-"""
-This module implements several arithmetic and logical operations such that it maintains single-precision across
-variables and operations. It overloads double-precision floating variables into single-precision using numpy's
-float32 method, forcing them to stay in single-precision type. This avoids a possible butterfly effect in numerical
-operations involving large floating point calculations.
+""" This module implements several arithmetic and logical operations such that it
+maintains single-precision across variables and operations. It overloads
+double-precision floating variables into single-precision using numpy's
+float32 method, forcing them to stay in single-precision type. This avoids a
+possible butterfly effect in numerical operations involving large floating
+point calculations.
 
 Usage:
     To convert a float variable into Float32 type, do the following ::
@@ -10,7 +11,8 @@ Usage:
         OR
         target = Float32(5.0)
 where <target> is now a Float32 object.
-Now, any arithmetic/logical operation involving <float_variable> will involve single-point calculations.
+Now, any arithmetic/logical operation involving <float_variable> will involve
+single-point calculations.
 Example Usage:
         eps = Float32(1.0)
         while eps + 1.0 > 1.0:
@@ -20,190 +22,162 @@ Example Usage:
 Authors:
     Saumya Debray
     Pratik Bhandari
+
 """
 
 from numpy import float32
 
 
 class Float32:
-    """
-    This class converts float variables into float32 type for single-precision calculation and overloads the default
-    arithmetic and logical operations. All methods below follow a similar input/output type.
+    """ This class converts float variables into float32 type for single-precision
+    calculation and overloads the default arithmetic and logical operations.
+    All methods below follow a similar
+    input/output type.
 
     Input: Either a Float32 variable or a variable of a supported type.
     Output: A Float32 object of the result of the operation.
     """
 
     def __init__(self, val):
+        """ This constructor converts the float variable 'val' into numpy's
+        float32 type.
         """
-        This constructor converts the float variable 'val' into numpy's float32 type.
-        :param val: float variable
-        """
-
         self._val = float32(val)
 
     def __add__(self, other):
-        """
-        Addition
+        """ Addition
         """
         return Float32(self._val+self.value(other))
 
     def __radd__(self, other):
-        """
-        Reverse Addition
+        """ Reverse Addition
         """
         return Float32(self._val+self.value(other))
 
     def __truediv__(self, other):
-        """
-        True Division
+        """ True Division
         """
         return Float32(self._val/self.value(other))
 
     def __rtruediv__(self, other):
-        """
-        Reverse True Division
+        """ Reverse True Division
         """
         return Float32(self.value(other)/self._val)
 
     def __floordiv__(self, other):
-        """
-        Floor Division
+        """ Floor Division
         """
         return self._val//self.value(other)
 
     def __rfloordiv__(self, other):
-        """
-        Reverse Floor Division
+        """ Reverse Floor Division
         """
         return self.value(other)//self._val
 
     def __mul__(self, other):
-        """
-        Multiplication
+        """ Multiplication
         """
         return Float32(self._val*self.value(other))
 
     def __rmul__(self, other):
-        """
-        Reverse sMultiplication
+        """ Reverse sMultiplication
         """
         return Float32(self._val*self.value(other))
 
     def __sub__(self, other):
-        """
-        Subtraction
+        """ Subtraction
         """
         return Float32(self._val-self.value(other))
 
     def __rsub__(self, other):
-        """
-        Reverse Subtraction
+        """ Reverse Subtraction
         """
         return Float32(self.value(other)-self._val)
 
     def __eq__(self, other):
-        """
-        Logical Equality
+        """ Logical Equality
         """
         return self._val == self.value(other)
 
     def __gt__(self, other):
-        """
-        Logical greater than
+        """ Logical greater than
         """
         return self._val > self.value(other)
 
     def __ge__(self, other):
-        """
-        Logical greater than or equals to
+        """ Logical greater than or equals to
         """
         return self._val >= self.value(other)
 
     def __lt__(self, other):
-        """
-        Logical less than
+        """ Logical less than
         """
         return self._val < self.value(other)
 
     def __le__(self, other):
-        """
-        Logical less than or equals to
+        """ Logical less than or equals to
         """
         return self._val <= self.value(other)
 
     def __ne__(self, other):
-        """
-        Logical not equal to
+        """ Logical not equal to
         """
         return self._val != self.value(other)
 
     def __str__(self):
-        """
-        float-to-string conversion
+        """ Float-to-string conversion
         """
         return str(self._val)
 
     def __abs__(self):
-        """
-        Absolute value conversion
+        """ Absolute value conversion
         """
         return abs(self._val)
 
     def __neg__(self):
-        """
-        Value negation
+        """ Value negation
         """
         return -self._val
 
     def __mod__(self, other):
-        """
-        Modulo operation
+        """ Modulo operation
         """
         return Float32(self._val % self.value(other))
 
     def __rmod__(self, other):
-        """
-        Reverse Modulo operation
+        """ Reverse Modulo operation
         """
         return Float32(self.value(other) % self._val)
 
     def __and__(self, other):
-        """
-        Bitwise 'and' of two variables
+        """ Bitwise 'and' of two variables
         """
         return Float32(self._val and self.value(other))
 
     def __or__(self, other):
-        """
-        Bitwise 'or' of two variables
+        """ Bitwise 'or' of two variables
         """
         return Float32(self._val or self.value(other))
 
     def __pos__(self):
-        """
-        Return positive value
+        """ Return positive value
         """
         return +self._val
 
     def __pow__(self, other):
-        """
-        Power operation
+        """ Power operation
         """
         return Float32(self._val ** self.value(other))
 
     def __rpow__(self, other):
-        """
-        Reverse Power operation
+        """ Reverse Power operation
         """
         return Float32(self.value(other) ** self._val)
 
     def value(self, other):
+        """ This method checks whether the variable is a Float32 type or not and
+        returns it's value likewise.
         """
-        This method checks whether the variable is a Float32 type or not and returns it's value likewise.
-        :param other: Either a Float32 object or any other variable
-        """
-
         if isinstance(other, Float32):
             return other._val
         else:
