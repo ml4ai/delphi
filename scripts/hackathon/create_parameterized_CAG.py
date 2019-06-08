8c13249fbf0f2666ee808b04d0475ea9c3a3edf3
Loren Champlin <champlin@email.arizona.edu>
2019-06-08 11:36:48 -0700

    Minor changes

--- scripts/hackathon/create_parameterized_CAG.py
+++ scripts/hackathon/create_parameterized_CAG.py
@@ -6,7 +6,7 @@ def create_parameterized_CAG(input, output, filename="CAG_with_indicators_and_va
     """ Create a CAG with mapped and parameterized indicators """
     with open(input, "rb") as f:
         G = pickle.load(f)
-    G.parameterize(year=2017, month=4)
+    G.parameterize(year=2012)
     G.get_timeseries_values_for_indicators()
     with open(output, "wb") as f:
         pickle.dump(G, f)
