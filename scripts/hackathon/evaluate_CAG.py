c0360c8885c7cd7078f6f35e7cf58a0dd5871850
Loren Champlin <champlin@email.arizona.edu>
2019-06-10 14:27:28 -0700

    Fixed bugs, runs now

--- scripts/hackathon/evaluate_CAG.py
+++ scripts/hackathon/evaluate_CAG.py
@@ -22,12 +22,12 @@ def get_predictions(G, target_node, intervened_node, delta, n_timesteps, dampen=
     pred = np.zeros(n_timesteps)
     for t in range(n_timesteps):
         G.update(dampen=dampen)
-        pred[t]=stats.mode(list(G.nodes(data=True)[target_node]['indicators'].values())[0].samples)
+        pred[t]=np.median(list(G.nodes(data=True)[target_node]['indicators'].values())[0].samples)
 
     return pred
 
 
-def get_true_values(G,target_node,n_timesteps,start_year,start_month)
+def get_true_values(G,target_node,n_timesteps,start_year,start_month):
     df = pd.read_sql_table("indicator", con=engine)
     target_indicator = list(G.nodes(data=True)[target_node]['indicators'].keys())[0]
     target_df = df[df['Variable'] == target_indicator]
@@ -38,11 +38,12 @@ def get_true_values(G,target_node,n_timesteps,start_year,start_month)
     for j in range(n_timesteps):
         if target_df[target_df['Year'] == year].empty:
            true_vals[j] = target_df['Value'].values.astype(float).mean()
-        if target_df[target_df['Year'] == year][target_df['Month'] ==
+        elif target_df[target_df['Year'] == year][target_df['Month'] ==
                 month].empty:
             true_vals[j] = target_df[target_df['Year'] ==
                     year]['Value'].values.astype(float).mean()
-        true_vals[j] = target_df[target_df['Year'] == year][target_df['Month']
+        else:
+            true_vals[j] = target_df[target_df['Year'] == year][target_df['Month']
                 == month]['Value'].values.astype(float).mean()
         if month == 12:
             year = year + 1
@@ -60,38 +61,38 @@ def calculate_timestep(start_year,start_month,end_year,end_month):
 
 
 # This is specifically for when the dampen argument for G.update is set to False.
-def
-estimate_delta(G,intervened_node,n_timesteps,start_year,start_month,end_year,end_month):
+def estimate_delta(G,intervened_node,n_timesteps,start_year,start_month,end_year,end_month):
     df = pd.read_sql_table("indicator", con=engine)
     intervener_indicator = list(G.nodes(data=True)[intervened_node]['indicators'].keys())[0]
     intervened_df = df[df['Variable'] == intervener_indicator]
 
     if intervened_df[intervened_df['Year'] == start_year].empty:
         start_val = intervened_df['Value'].values.astype(float).mean()
-    if intervened_df[intervened_df['Year'] ==
-            start_year][intervened_df['Month'] == start_Month].empty:
+    elif intervened_df[intervened_df['Year'] ==
+            start_year][intervened_df['Month'] == start_month].empty:
         start_val = intervened_df[intervened_df['Year'] ==
                 start_year]['Value'].values.astype(float).mean()
-    start_val = intervened_df[intervened_df['Year'] ==
+    else:
+        start_val = intervened_df[intervened_df['Year'] ==
             start_year][intervened_df['Month'] ==
-                    start_month]['Values'].values.astype(float).mean()
+                    start_month]['Value'].values.astype(float).mean()
 
     if intervened_df[intervened_df['Year'] == end_year].empty:
         end_val = intervened_df['Value'].values.astype(float).mean()
-    if intervened_df[intervened_df['Year'] ==
-            end_year][intervened_df['Month'] == end_Month].empty:
+    elif intervened_df[intervened_df['Year'] ==
+            end_year][intervened_df['Month'] == end_month].empty:
         end_val = intervened_df[intervened_df['Year'] ==
                 end_year]['Value'].values.astype(float).mean()
-    end_val = intervened_df[intervened_df['Year'] ==
+    else:
+        end_val = intervened_df[intervened_df['Year'] ==
             end_year][intervened_df['Month'] ==
-                    end_month]['Values'].values.astype(float).mean()
+                    end_month]['Value'].values.astype(float).mean()
 
     diff_val = end_val - start_val
     return diff_val/n_timesteps
 
 def evaluate_CAG(
     input,
-    output,
     target_node: str,
     intervened_node: str,
     start_year=2012,
@@ -118,11 +119,9 @@ def evaluate_CAG(
 
     n_timesteps = calculate_timestep(start_year,start_month,end_year,end_month)
 
-    true_vals =
-    get_true_values(G,target_node,n_timesteps,start_year,start_month)
+    true_vals = get_true_values(G,target_node,n_timesteps,start_year,start_month)
 
-    delta =
-    estimate_delta(G,intervened_node,n_timesteps,start_year,start_month,end_year,end_month)
+    delta = estimate_delta(G,intervened_node,n_timesteps,start_year,start_month,end_year,end_month)
 
     preds = get_predictions(G, target_node, intervened_node, delta, n_timesteps, dampen=False)
 
@@ -130,10 +129,11 @@ def evaluate_CAG(
 
     mean_sq_error = np.mean(sq_error)
 
-    print(sq_error)
+    print(sq_error, "\n")
     print(mean_sq_error)
 
 if __name__ == "__main__":
     evaluate_CAG(input = sys.argv[1],target_node = sys.argv[2], intervened_node
-            = sys.argv[3], start_year = sys.argv[4], end_year = sys.argv[6],
-            res = sys.argv[7])
+            = sys.argv[3], start_year = int(sys.argv[4]), end_year = int(
+                sys.argv[5]),
+            res = int(sys.argv[6]))
