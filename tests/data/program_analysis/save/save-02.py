b12fba0bfc9df57b02f8a7caa5c46cd4e02be998
Pratik Bhandari <pratik.bhd@gmail.com>
2019-06-14 00:28:53 -0700

    python version of save-02

--- tests/data/program_analysis/save/save-02.py
+++ tests/data/program_analysis/save/save-02.py
@@ -1,43 +1,56 @@
+import sys
+from typing import List
+import math
 from delphi.translators.for2py.format import *
-from delphi.translators.for2py.static_save import static_vars
+from delphi.translators.for2py.arrays import *
+from delphi.translators.for2py.static_save import *
+from dataclasses import dataclass
 
 
 @static_vars(["w"])
-def f(n, x):
-    if n[0] == 0:
-        f.w = 111
+def f(n: List[int], x: List[int]):
+    # w: List[int] = [None]
+    if (n[0] == 0):
+        f.w[0] = 111
     else:
-        f.w = 2 * f.w
-    x[0] = f.w
+        f.w[0] = (2 * f.w[0])
+    x[0] = f.w[0]
 
 
 @static_vars(["w"])
-def g(n, x):
-    if n[0] == 0:
-        g.w = 999
+def g(n: List[int], x: List[int]):
+    # w: List[int] = [None]
+    if (n[0] == 0):
+        g.w[0] = 999
     else:
-        g.w = g.w // 3  # ***
-    x[0] = g.w
+        g.w[0] = (g.w[0] // 3)
+    x[0] = g.w[0]
 
 
 def main():
-    a, b = [None], [None]
+    format_10: List[str] = [None]
+    format_10 = ['"a = "', 'I5', '";   b = "', 'I5']
+    format_10_obj = Format(format_10)
 
-    f([0], a)
-    g([0], b)
-    fmt_10 = Format(['"a = "', 'I5', '";   b = "', 'I5'])
-    line = fmt_10.write_line([a[0], b[0]])
-    sys.stdout.write(line)
+    a: List[int] = [None]
+    b: List[int] = [None]
+    f([0], a)   # Should be f(0, a)
+    g([0], b)   # Should be g(0, b)
 
+    write_list_stream = [a[0], b[0]]
+    write_line = format_10_obj.write_line(write_list_stream)
+    sys.stdout.write(write_line)
     f([1], a)
     g([1], b)
-    line = fmt_10.write_line([a[0], b[0]])
-    sys.stdout.write(line)
-
+    write_list_stream = [a[0], b[0]]
+    write_line = format_10_obj.write_line(write_list_stream)
+    sys.stdout.write(write_line)
     f([1], a)
     g([1], b)
-    line = fmt_10.write_line([a[0], b[0]])
-    sys.stdout.write(line)
+    write_list_stream = [a[0], b[0]]
+    write_line = format_10_obj.write_line(write_list_stream)
+    sys.stdout.write(write_line)
+    return
 
 
 main()
