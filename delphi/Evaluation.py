1be70818281b7a2eeb4fd29647bf26f21f09df36
Loren Champlin <champlin@email.arizona.edu>
2019-06-14 18:16:11 -0700

    Stable Version of Evaluation module

--- delphi/Evaluation.py
+++ delphi/Evaluation.py
@@ -1,6 +1,6 @@
 import sys
 import pickle
-import .AnalysisGraph as AG
+import delphi.AnalysisGraph as AG
 import pandas as pd
 from .db import engine
 import numpy as np
@@ -8,6 +8,7 @@ import random
 from scipy import stats
 import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings
 
 
 def get_predictions(G, target_node, intervened_node, deltas, n_timesteps):
@@ -56,6 +57,13 @@ def get_predictions(G, target_node, intervened_node, deltas, n_timesteps):
 def get_true_values(G,target_node,n_timesteps,start_year,start_month):
     """ Get the true values of the indicator variable attached to the given
     target node.
+
+    If multiple data entries exist for one time point then the mean is used
+    for those time points. If there are no data entries for a given year,
+    then the mean over all data values is used. If there are no data entries
+    for a given month (but there are data entries for that year), then the
+    overall value for the year is used (the mean for that year is used if there
+    are multiple values for that year).
 
     Args:
         G: A completely parameterized and quantified CAG with indicators,
@@ -119,7 +127,7 @@ def calculate_timestep(start_year,start_month,end_year,end_month):
         end_month: An integer, ending month
 
     Returns:
-        Integer value
+        Integer value, which is the computed time step.
     """
 
     assert (start_year <= end_year), "Starting date cannot exceed ending date."
@@ -141,6 +149,9 @@ def estimate_deltas(G,intervened_node,n_timesteps,start_year,start_month):
     from 0 (+INF case), then delta = positive absolute mean of all finite
     deltas. If decreasing from 0 (-INF case), then delta = negative absolute
     mean of all finite deltas.
+
+    See function get_true_values to see how the data is aggregated to fill in
+    values for missing time points which calculating the deltas.
 
     Args:
         G: A completely parameterized and quantified CAG with indicators,
@@ -197,20 +208,94 @@ def estimate_deltas(G,intervened_node,n_timesteps,start_year,start_month):
     return np.delete(per_ch,-1)
 
 
-def evaluate_CAG(
-    G,
+def setup_evaluate(G=None,input=None,res=200):
+    """ Optional Utility function that takes a CAG and assembles the transition
+    model.
+
+    Args:
+        G: A CAG. It Must have indicators variables mapped to nodes.
+
+        input: This allows you to upload a CAG from a pickle file, instead of
+        passing it directly as an argument. The CAG must have mapped
+        indicators.
+
+        res: Sampling Resolution. Default is 200 samples.
+
+    Returns:
+        Returns CAG.
+    """
+
+    if input is not None:
+        if G is not None:
+            warnings.warn('The CAG passed to G will be suppressed by the CAG loaded from the pickle file.')
+        with open(input,"rb") as f:
+            G = pickle.load(f)
+
+    assert (G is not None), "A CAG must be passed to G or a pickle file containing a CAG must be passed to input."
+    G.res = res
+    G.assemble_transition_model_from_gradable_adjectives()
+    G.sample_from_prior()
+    return G
+
+
+def evaluate(
     target_node: str,
     intervened_node: str,
+    G = None,
     input = None,
     start_year=2012,
     start_month=None,
     end_year=2017,
     end_month=None,
-    dampen=False,
+    plot = False,
+    plot_type ='Compare'
 ):
+    """ This is the main function of this module. This parameterizes a given
+    CAG (see requirements in Args) and calls other functions within this module
+    to predict values for a specified target node's indicator variable given a
+    start date and end date. Returns pandas dataframe containing predicted
+    values, true values, and squared error.
+
+    Args:
+        target_node: A string of the full name of the node in which we
+        wish to predict values for its attached indicator variable.
+
+        intervened_node: A string of the full name of the node in which we
+        are intervening on.
+
+        G: A CAG. It must have mapped indicator values and estimated transition matrix.
+
+        input: This allows you to upload a CAG from a pickle file, instead of
+        passing it directly as an argument. The CAG must have mapped
+        indicators and an estimated transition matrix.
+
+        start_year: An integer, designates the starting year (ex: 2012).
+
+        start_month: An integer, starting month (1-12).
+
+        end_year: An integer, ending year
+
+        end_month: An integer, ending month
+
+        plot: Set to true to display a plot according to the plot type.
+
+        plot_type: By default setting plot to true displays the "Compare" type
+        plot which plots the predictions and true values of the target node's
+        indicator variable on one plot labeled by time steps (Year-Month).
+        There is also "Error" type, which plots the errors or residuals, with a
+        reference line at 0.
+
+    Returns:
+        Returns a pandas dataframe.
+    """
+
     if input is not None:
+        if G is not None:
+            warnings.warn('The CAG passed to G will be suppressed by the CAG loaded from the pickle file.')
         with open(input,"rb") as f:
             G = pickle.load(f)
+
+    assert (G is not None), "A CAG must be passed to G or a pickle file containing a CAG must be passed to input."
 
     G.parameterize(year = start_year,month = start_month)
     G.get_timeseries_values_for_indicators()
@@ -228,24 +313,29 @@ def evaluate_CAG(
 
     deltas = estimate_deltas(G,intervened_node,n_timesteps,start_year,start_month)
 
-    preds_df = get_predictions(G, target_node, intervened_node, deltas, n_timesteps, dampen=False)
+    preds_df = get_predictions(G, target_node, intervened_node, deltas, n_timesteps)
 
     preds_df = preds_df.set_index(true_vals_df.index)
 
     preds = preds_df.values
 
-    sq_error = (preds-true_vals)**2
+    error = (preds-true_vals)
 
-    mean_sq_error = np.mean(sq_error)
+    error_df = pd.DataFrame(error,columns=["Errors"])
 
-    compare_df = pd.concat([true_vals_df,preds_df],axis=1,join_axes=[true_vals_df.index])
+    error_df = error_df.set_index(true_vals_df.index)
 
-    sns.set(rc={'figure.figsize':(15,8)},style='whitegrid')
-    ax = sns.lineplot(data=compare_df)
-    ax.set_xticklabels(compare_df.index,rotation=45,ha='right',fontsize=8)
+    compare_df =  pd.concat([true_vals_df,preds_df],axis=1,join_axes=[true_vals_df.index])
 
-if __name__ == "__main__":
-    evaluate_CAG(input = sys.argv[1],target_node = sys.argv[2], intervened_node
-            = sys.argv[3], start_year = int(sys.argv[4]), end_year = int(
-                sys.argv[5]),
-            res = int(sys.argv[6]))
+    if plot:
+        if plot_type == 'Error':
+            sns.set(rc={'figure.figsize':(15,8)},style='whitegrid')
+            ax = sns.lineplot(data=error_df)
+            ax.set_xticklabels(error_df.index,rotation=45,ha='right',fontsize=8)
+            ax.axhline(c = 'red')
+        else:
+            sns.set(rc={'figure.figsize':(15,8)},style='whitegrid')
+            ax = sns.lineplot(data=compare_df)
+            ax.set_xticklabels(compare_df.index,rotation=45,ha='right',fontsize=8)
+
+    return pd.concat([compare_df,error_df],axis=1,join_axes=[true_vals_df.index])
