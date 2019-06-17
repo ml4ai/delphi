06a596dd1febf1c6d6e54384bc5d9ba1b7e8d635
Clayton Morrison <claytonm@email.arizona.edu>
2019-06-16 20:41:39 -0700

    now loads DiscreteSIR-noarrays.f

--- scripts/SIR_demo/demo_generate_grfn.py
+++ scripts/SIR_demo/demo_generate_grfn.py
@@ -4,7 +4,13 @@ from delphi.GrFN.networks import GroundedFunctionNetwork
 #
 # -----------------------------------------------------------------------------
 
-grfn = GroundedFunctionNetwork.from_fortran_file('DiscreteSIR-simple.f')
+print('Running demo_generate_grfn.py')
+
+source_fortran_file = 'DiscreteSIR-noarrays.f'
+
+print(f'    source_fortran_file: {source_fortran_file}')
+
+grfn = GroundedFunctionNetwork.from_fortran_file(source_fortran_file)
 agraph = grfn.to_agraph()
 agraph.draw('graph.pdf', prog='dot')
 
