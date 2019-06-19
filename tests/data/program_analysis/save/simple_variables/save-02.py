55bfd4f79b54c6d32d7b1a10faa990419846d27d
Pratik Bhandari <pratik.bhd@gmail.com>
2019-06-19 14:04:29 -0700

    Changing test files to match tests

--- tests/data/program_analysis/save/simple_variables/save-02.py
+++ tests/data/program_analysis/save/simple_variables/save-02.py
@@ -9,34 +9,30 @@ from dataclasses import dataclass
 
 @static_vars(["w"])
 def f(n: List[int], x: List[int]):
-    # w: List[int] = [None]
     if (n[0] == 0):
         f.w[0] = 111
     else:
-        f.w[0] = (2 * f.w[0])
+        f.w[0] = int((2 * f.w[0]))
     x[0] = f.w[0]
-
 
 @static_vars(["w"])
 def g(n: List[int], x: List[int]):
-    # w: List[int] = [None]
     if (n[0] == 0):
         g.w[0] = 999
     else:
-        g.w[0] = (g.w[0] // 3)
+        g.w[0] = int((g.w[0] / 3))
     x[0] = g.w[0]
-
 
 def main():
     format_10: List[str] = [None]
     format_10 = ['"a = "', 'I5', '";   b = "', 'I5']
     format_10_obj = Format(format_10)
-
+    
     a: List[int] = [None]
     b: List[int] = [None]
-    f([0], a)   # Should be f(0, a)
-    g([0], b)   # Should be g(0, b)
-
+    f([0], a)
+    g([0], b)
+    
     write_list_stream = [a[0], b[0]]
     write_line = format_10_obj.write_line(write_list_stream)
     sys.stdout.write(write_line)
@@ -51,6 +47,5 @@ def main():
     write_line = format_10_obj.write_line(write_list_stream)
     sys.stdout.write(write_line)
     return
-
 
 main()
