get_indicator_value can now handle getting passed wrong units

delphi/assembly.py
@@ -5,6 +5,7 @@ from typing import Tuple, List, Dict, Iterable, Optional, Callable
 from indra.statements import Influence
 import pandas as pd
 import numpy as np
+import warnings
 from scipy.stats import gaussian_kde
 from .db import engine
 
@@ -148,11 +149,13 @@ def get_indicator_value(
     if month is not None:
         query_parts["month"] = f"and `Month` is '{month}'"
     if unit is not None:
-        unit_q = f"and `Unit` is '{unit}'"
-        test_q = "  ".join(unit_q)
-        test_q = query_parts["base"].join(test_q)
-        print(test_q)
-        query_parts["unit"] = f"and `Unit` is '{unit}'"
+        check_q = query_parts["base"] + f" and `Unit` is '{unit}'"
+        check_r = list(engine.execute(check_q))
+        if check_r == []:
+            warnings.warn(f'Selected units not found for {indicator.name}! Falling back to default units!')
+            query_parts["unit"] = ""
+        else:
+            query_parts["unit"] = f"and `Unit` is '{unit}'"
 
     indicator.aggaxes = []
     for constraint in ("country", "state", "year", "month"):
@@ -160,7 +163,6 @@ def get_indicator_value(
             indicator.aggaxes.append(constraint)
 
     query = " ".join(query_parts.values())
-    #print(query)
     results = list(engine.execute(query))
     if results != []:
         unit = sorted(list({r["Unit"] for r in results}))[0]
@@ -199,14 +201,14 @@ def get_indicator_value(
                             }
                         )
                     )[0]
-                    agg = aggfunc(
-                        [
-                            float(r["Value"])
-                            for r in results
-                            if r["Unit"] == unit
-                        ]
-                    )
-                    return agg,unit
+                agg = aggfunc(
+                    [
+                        float(r["Value"])
+                        for r in results
+                        if r["Unit"] == unit
+                    ]
+                )
+                return agg,unit
 
             except StopIteration:
                 raise ValueError(