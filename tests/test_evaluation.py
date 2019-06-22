bc7061fc1ab3c2c403835721e584721dfb9c7131
Loren Champlin <champlin@email.arizona.edu>
2019-06-21 21:49:55 -0700

    Increased coverage of evaluation.py

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -76,6 +76,18 @@ def test_get_true_values(G_eval):
     assert true_df.columns[0] == "Inflation Rate(True)"
     assert true_df.index[6] == "2014-3"
 
+    # Check warnings
+    with pytest.warns(UserWarning):
+        EN.get_true_values(
+            G,
+            "UN/entities/human/financial/economic/inflation",
+            10,
+            2013,
+            9,
+            "China",
+            "Arizona",
+        )
+
 
 def test_calculate_timestep():
     # Check Assertion error when putting end_year < start_year
@@ -133,6 +145,18 @@ def test_estimate_deltas(G_eval):
         G, "UN/entities/human/financial/economic/inflation", 10, 2013, 9
     )
     assert len(deltas) == 10
+
+    # Check warnings
+    with pytest.warns(UserWarning):
+        EN.estimate_deltas(
+            G,
+            "UN/entities/human/financial/economic/inflation",
+            10,
+            2013,
+            9,
+            "China",
+            "Arizona",
+        )
 
 
 def test_setup_evaluate(G_eval):
@@ -226,4 +250,22 @@ def test_evaluate(G_eval):
         end_month=end_month,
         plot=True,
         plot_type="Error",
+    )
+
+    # Check **kwargs in evaluate
+    EN.evaluate(
+        target_node=target_node,
+        intervened_node=intervened_node,
+        G=G_eval,
+        start_year=start_year,
+        start_month=start_month,
+        end_year=end_year,
+        end_month=end_month,
+        plot=True,
+        plot_type="Error",
+        country="South Sudan",
+        state=None,
+        units=None,
+        fallback_aggaxes=["year", "month"],
+        aggfunc=np.mean,
     )
