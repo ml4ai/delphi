fd49c710233711bc5f62c250cb54f59982703ec2
Pratik Bhandari <pratik.bhd@gmail.com>
2019-06-23 08:44:41 -0700

    Changes to handle arrays in save statement

--- delphi/translators/for2py/static_save.py
+++ delphi/translators/for2py/static_save.py
@@ -19,11 +19,22 @@ Usage:
             "target_function"
 """
 
+from delphi.translators.for2py.arrays import *
+from dataclasses import dataclass
 
-def static_vars(var_list):  # This code is part of the runtime system
+
+
+def static_vars(var_list):
+    # This code is part of the runtime system
     def decorate(func):
-        for var_name in var_list:
-            setattr(func, var_name, [None])
+        for var in var_list:
+            # print(var)
+            setattr(func, var["name"], eval(var["call"]))
         return func
 
     return decorate
+
+# @dataclass
+# class mytype_123:
+#     def __init__(self):
+#         self.a : int
