1f85598e065f773f1fdf32f81ceb096206d19bf8
Pratik Bhandari <pratik.bhd@gmail.com>
2019-06-26 00:59:36 -0700

    Changes test file for matching

--- tests/data/program_analysis/PETPT.py
+++ tests/data/program_analysis/PETPT.py
@@ -10,7 +10,7 @@ import delphi.translators.for2py.math_ext as math
 from numbers import Real
 
 
-def petpt(msalb: List[Float32], srad: List[Float32], tmax: List[Float32], tmin: List[Float32], xhlai: List[Float32], eo: List[Float32]):
+def petpt(msalb: List[Real], srad: List[Real], tmax: List[Real], tmin: List[Real], xhlai: List[Real], eo: List[Real]):
     albedo: List[float] = [None]
     eeq: List[float] = [None]
     slang: List[float] = [None]
@@ -19,7 +19,7 @@ def petpt(msalb: List[Float32], srad: List[Float32], tmax: List[Float32], tmin: 
     if (xhlai[0] <= 0.0):
         albedo[0] = msalb[0]
     else:
-        albedo[0] = (0.23 - ((0.23 - msalb[0]) * Float32(math.exp(-((0.75 * xhlai[0]))._val))))
+        albedo[0] = (0.23 - ((0.23 - msalb[0]) * math.exp(-((0.75 * xhlai[0])))))
     slang[0] = (srad[0] * 23.923)
     eeq[0] = ((slang[0] * (2.04E-4 - (1.83E-4 * albedo[0]))) * (td[0] + 29.0))
     eo[0] = (eeq[0] * 1.1)
@@ -27,6 +27,6 @@ def petpt(msalb: List[Float32], srad: List[Float32], tmax: List[Float32], tmin: 
         eo[0] = (eeq[0] * (((tmax[0] - 35.0) * 0.05) + 1.1))
     else:
         if (tmax[0] < 5.0):
-            eo[0] = ((eeq[0] * 0.01) * Float32(math.exp((0.18 * (tmax[0] + 20.0))._val)))
+            eo[0] = ((eeq[0] * 0.01) * math.exp((0.18 * (tmax[0] + 20.0))))
     eo[0] = max(eo[0], 0.0001)
     
