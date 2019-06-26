3c1c2d085fce5e48d6265e5807ac512bac9efd04
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-06-25 18:54:08 -0700

    wip

--- tests/wm/test_cpp_extensions.py
+++ tests/wm/test_cpp_extensions.py
@@ -3,3 +3,4 @@ from delphi.cpp.AnalysisGraph import AnalysisGraph
 def test_cpp_extensions():
     G = AnalysisGraph.from_json_file("tests/data/indra_statements_format.json")
     G.construct_beta_pdfs()
+    G.sample_from_prior()
