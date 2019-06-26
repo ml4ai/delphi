d0b7ada974426763a72a67781096b80ae8c37807
Pratik Bhandari <pratik.bhd@gmail.com>
2019-06-26 09:24:47 -0700

    Further Additions to SAVE statement handling (#292)
    * Decorator function in separate module for SAVE statements.
    
    * Target python version
    
    * Updated to most recent version and added support for SAVE statements with arguments
    
    * Relocation of SAVE elements
    
    * save test file
    
    * python version of save-02
    
    * variable should be of list type
    
    * final changes for handling save statment
    
    * saved-entity is a child of save-stmt
    
    * final changes for handling save statement
    
    * simpler way to load dictionary
    
    * added support for floored division
    
    * fixing dtype issue
    
    * Fixed issue with true division
    
    * Removed incorrect data/ folder addition
    
    * Managing test cases for SAVE statement
    
    * Managing test cases for SAVE statement
    
    * Test files for save statement in arrays
    
    * Removing stray files
    
    * changing test scope
    
    * Add the save node at the end of the ast after all variables have been declared
    
    * Converting double line to single line
    
    * Handle special case ignores for print calls
    
    * Changed test files to pass pytest
    
    * Fixed issue with wrong renaming of variable
    
    * Changing test files to match tests
    
    * Added test for save statements
    
    * Fix for SAVE feature bypass in lambdas
    
    * adding import statement for save
    
    * adding parenthesis to clarify logical operation.
    
    * Parenthesized to make operator grouping easy to understand.
    
    * Changed comment type from docstring to normal comments.
    
    * Changed bypass_regex to a global variable
    
    * Removed deprecated code not relevant to save statements.
    
    * Specifying xenial as the dist
    
    * Added parenthesis to the conditions for clearer grouping
    
    * Adding cython installation
    
    * Bumping pytest version
    
    * Added more grouping parenthsis around the conditions
    
    * Test case for save statement with arrays.
    
    * Test case for save statement with derived types
    
    * Changes to handle arrays in save statement
    
    * Changes to handle arrays in save statement
    
    * Changes to handle arrays in save statement
    
    * Handling saves for derived types
    
    * Test cases for derived types with SAVE statements
    
    * Adding print statment
    
    * Making file handles static
    
    * Making file handles static and other fixes
    
    * Added a fix for format appears under different scope (function, subroutine)
    
    * Making tuples work for single variable reads
    
    * test file changes
    
    * bugfix
    
    * Changed test cases to pass

--- tests/data/program_analysis/io-tests/iotest_05.py
+++ tests/data/program_analysis/io-tests/iotest_05.py
@@ -7,6 +7,7 @@ from delphi.translators.for2py.static_save import *
 from dataclasses import dataclass
 
 
+@static_vars([{'name': 'file_10', 'call': None, 'type': 'file_handle'}, {'name': 'file_20', 'call': None, 'type': 'file_handle'}])
 def main():
     format_30: List[str] = [None]
     format_30 = ['/', "'F = '", 'F5.1', "'; I = '", 'I4']
@@ -20,17 +21,17 @@ def main():
     j: List[int] = [None]
     y: List[float] = [None]
     
-    file_10 = open("infile3", "r")
-    file_20 = open("outfile3", "w")
+    main.file_10 = open("infile3", "r")
+    main.file_20 = open("outfile3", "w")
     
-    (i[0], x[0], j[0], y[0]) = format_10_obj.read_line(file_10.readline())
+    (i[0], x[0], j[0], y[0],) = format_10_obj.read_line(main.file_10.readline())
     
     write_list_20 = [x[0], j[0]]
     write_line = format_30_obj.write_line(write_list_20)
-    file_20.write(write_line)
+    main.file_20.write(write_line)
     write_list_20 = [y[0], i[0]]
     write_line = format_30_obj.write_line(write_list_20)
-    file_20.write(write_line)
+    main.file_20.write(write_line)
     
     return
 
