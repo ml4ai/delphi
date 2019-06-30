dd20911cb0fc08b96405f58f354b3b81acbadddf
manujinda <manujinda@yahoo.com>
2019-06-30 13:11:22 -0700

    Find all simple directed paths (#295)
    * Adds cppitertools as a submodule
    * Deletes .readthedocs.yml
    * Updates pytest command-line option from --doctest-module to --doctest-modules
    * Updates AnalysisGraph creation from CauseMos JSON payload
    * Removes the necessity for a BMI config file.
    * Updates Delphi-CauseMos REST API
    * Splits out C++ extension module into kde and AnalysisGraph modules
    * Adds utility template functions list, lmap, hasKey, and get in AnalysisGraph.cpp
    * Moves from COUT macro to print template function
    * Adds functionality to find all simple paths between two nodes - for sampling from the prior.
    * Adds additional utility functions to AnalysisGraph.cpp to emulate the NetworkX interface

--- delphi/cpp/AnalysisGraph.hpp
+++ delphi/cpp/AnalysisGraph.hpp
@@ -1,163 +1,63 @@
-#ifndef ANALYSISGRAPH_H
-#define ANALYSISGRAPH_H
-
-#include <boost/range/algorithm/for_each.hpp>
-#include <boost/range/numeric.hpp>
-#include <boost/range/adaptors.hpp>
-#include <boost/lambda/bind.hpp>
-#include <boost/lambda/lambda.hpp>
-#include <boost/random/mersenne_twister.hpp>
-#include <boost/random/uniform_int_distribution.hpp>
-#include <boost/range/irange.hpp>
+#pragma once
 
 #include <nlohmann/json.hpp>
-/** \def COUT(x)
-    \brief A macro that prints \a x to standard output (stdout).
-*/
-#define COUT(x) cout << x << endl;
-/** \def CERR(x)
-    \brief A macro that prints \a x to standard error (stderr).
-*/
-#define CERR(x) cerr << x << endl;
+#include <kde.hpp>
+#include <optional>
 
-namespace delphi {
-  using std::cout,
-        std::endl,
-        std::unordered_map,
-        std::pair,
-        std::string,
-        std::ifstream,
-        std::stringstream,
-        std::vector,
-        std::map,
-        boost::inner_product,
-        boost::accumulate,
-        boost::adaptors::transform,
-        boost::adaptors::transformed,
-        boost::adjacency_list,
-        boost::edge,
-        boost::add_edge,
-        boost::vecS,
-        boost::directedS,
-        boost::edges,
-        boost::source,
-        boost::target,
-        boost::irange,
-        boost::get,
-        boost::make_label_writer,
-        boost::write_graphviz,
-        boost::range::for_each,
-        boost::lambda::_1,
-        boost::lambda::_2,
-        boost::lambda::make_const
-  ;
+using std::pair 
+    , std::string
+    , std::optional
+    , std::vector
+    , std::unordered_map
+    , boost::adjacency_list
+    , boost::vecS
+    , boost::setS
+    , boost::listS
+    , boost::bidirectionalS
+    , boost::range::for_each
+;
 
-  using json = nlohmann::json;
+
+template <class T>
+void print(T x) {
+  std::cout << x << std::endl;
 }
 
-using namespace delphi;
-
-/**
- * Returns the square of a number.
- */
-double sqr(double x) { return x * x; }
-
-/**
- * Returns the sum of a vector of doubles.
- */
-double sum(vector<double> v) { return accumulate(v, 0.0); }
-
-
-/**
- * Returns the arithmetic mean of a vector of doubles.
- */
-double mean(vector<double> v) { return sum(v) / v.size(); }
-
-/**
- * Returns a randomly selected element of a vector.
- */
-template <class T> T select_random_element(vector<T> v) {
-  boost::random::mt19937 gen;
-  boost::random::uniform_int_distribution<> dist(0, v.size() - 1);
-  return v[dist(gen)];
+template <class T>
+void printVec(vector<T> xs) {
+  for (auto x : xs) {print(x);}
 }
 
-double sample_from_normal(
-    double mu = 0.0, /**< The mean of the distribution.*/
-    double sd = 1.0  /**< The standard deviation of the distribution.*/
-) {
-  std::random_device rd{};
-  std::mt19937 gen{rd()};
-  std::normal_distribution<> d{mu, sd};
-  return d(gen);
-}
+struct CausalFragment {
+  string subj_adjective;
+  string obj_adjective;
+  // Here we assume that unknown polarities are set to 1.
+  int subj_polarity = 1;
+  int obj_polarity = 1;
+};
 
 
-/**
- * The KDE class implements 1-D Gaussian kernel density estimators.
- */
-class KDE {
-public:
-  KDE () {};
-  vector<double> dataset;
-  double bw; // bandwidth
-
-  KDE(vector<double> v) : dataset(v) {
-    // Compute the bandwidth using Silverman's rule
-    auto mu = mean(v);
-    auto X = v | transformed(_1 - mu);
-
-    // Compute standard deviation of the sample.
-    auto N = v.size();
-    auto stdev = sqrt(inner_product(X, X, 0.0) / (N - 1));
-    bw = pow(4 * pow(stdev, 5) / (3 * N), 1 / 5);
-  }
-
-  auto resample(int n_samples) {
-    vector<double> samples;
-    for (int i : irange(0, n_samples)) {
-      auto element = select_random_element(dataset);
-      samples.push_back(sample_from_normal(element, bw));
-    }
-    return samples;
-  }
-
-  auto pdf(double x) {
-    auto p = 0.0;
-    auto N = dataset.size();
-    for (auto elem : dataset) {
-      auto x1 = exp(-sqr(x - elem) / (2 * sqr(bw)));
-      x1 /= N * bw * sqrt(2 * M_PI);
-      p += x1;
-    }
-    return p;
-  }
-
-  auto pdf(vector<double> v) {
-    vector<double> values;
-    for (auto elem : v) {
-      values.push_back(pdf(elem));
-    }
-    return values;
-  }
-
-  auto logpdf(double x) { return log(pdf(x)); }
+struct Edge {
+  string name;
+  optional<KDE> kde;
+  vector<CausalFragment> causalFragments= {};
 };
 
 struct Node {
   string name;
+  bool visited;
+
+  // Stores all the simple directed paths ending at this node
+  // according to the starting vertex of each path
+  // used as the key of the map. 
+  // start --> [ path1, path2, path3 ]
+  // path = [ (start, v2), (v2, v3), (v3, this_node) ]
+  unordered_map< int, vector< vector< pair< int, int >>>> influenced_by;
 };
 
-struct CAGEdge {
+struct GraphData {
   string name;
-  KDE kde;
 };
 
-struct Model {
-  string name;
-};
 
-typedef pair<int, int> Edge;
-typedef adjacency_list<vecS, vecS, directedS, Node, CAGEdge, Model> DiGraph;
-
-#endif
+typedef adjacency_list<setS, vecS, bidirectionalS, Node, Edge, GraphData> DiGraph;
