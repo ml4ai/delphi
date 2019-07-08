199b779d9d6276c3e9bf2832195d6672750d9f12
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-07-08 12:07:06 -0700

    Adding conan to setup.py requirements

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
     install_requires=[
+        "conan",
         "pybind11",
         "indra[eidos_offline]",
         "tqdm",
