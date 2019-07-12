6fb1411c60c809d4a8238b34e05b4dda16855649
Terrence Lim <hlim1@email.arizona.edu>
2019-07-11 21:56:58 -0700

    Lower cased code

--- tests/data/program_analysis/cycle/cycle_02.f
+++ tests/data/program_analysis/cycle/cycle_02.f
@@ -5,15 +5,15 @@ C     Source: http://annefou.github.io/Fortran/basics/control.html
 
       program odd_number
       implicit none
-      integer :: N, k
-      N = 19
+      integer :: n, k
+      n = 19
       k = 0
-      DO WHILE (.TRUE.)
+      do while (.true.)
         k = k + 1
-        if (k > N) EXIT
-        if (mod(k,2) .eq. 0) CYCLE
-        WRITE(*,10) k, N
-      ENDDO
+        if (k > n) exit
+        if (mod(k,2) .eq. 0) cycle
+        write(*,10) k, n
+      enddo
 
- 10   FORMAT('k = ', I3, '; N = ', I8)
+ 10   format('k = ', i3, '; n = ', i8)
       end program odd_number
