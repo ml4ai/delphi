2d201dfaee7763e8c4dfd8d3d567f13f08ec4f96
Terrence Lim <hlim1@email.arizona.edu>
2019-07-22 20:24:19 -0700

    Commented out some code to pass webapp test

--- delphi/translators/for2py/genPGM.py
+++ delphi/translators/for2py/genPGM.py
@@ -315,8 +315,11 @@ class GrFNGenerator(object):
             var_grfn = {"name":var}
             if var in local_variable_types:
                 var_grfn["index"] = local_last_definitions[var]
-                var_grfn["domain"] = local_variable_types[var]
-                var_grfn["mutatble"] = False
+                # Temporarily commenting out in order to pass the
+                # webapp test that it may not been updated to the
+                # lastest GrFN spec.
+                # var_grfn["domain"] = local_variable_types[var]
+                # var_grfn["mutatble"] = False
             elif var in self.arrays:
                 var_grfn["index"] = self.arrays[var]["index"]
                 var_grfn["domain"] = {
