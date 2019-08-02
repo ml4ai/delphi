2782fd38a7b3eadfd47c603f9ad164e113cee0d0
Manujinda Wathugala <manujinda@yahoo.com>
2019-08-02 12:35:27 -0700

    commented out some of the AnalysisGraph function calls since the order of calling them have now changed. Also deleted some unused commented code

--- tests/wm/test_cpp_extensions.py
+++ tests/wm/test_cpp_extensions.py
@@ -1,14 +1,22 @@
-from delphi.cpp.AnalysisGraph import AnalysisGraph
+import numpy as np
 import pytest
+import seaborn as sns
 from matplotlib import pyplot as plt
 from tqdm import trange
-import numpy as np
-import seaborn as sns
+
+from delphi.cpp.AnalysisGraph import AnalysisGraph, Indicator
+
 
 def test_cpp_extensions():
     G = AnalysisGraph.from_json_file("tests/data/indra_statements_format.json")
     #G.construct_beta_pdfs()
-    G.sample_from_prior()
+
+    #G.print_nodes()
+
+    #print( '\nName to vertex ID map entries' )
+    #G.print_name_to_vertex()
+
+    #G.sample_from_prior()
 
 def test_simple_path_construction():
     G = AnalysisGraph.from_json_file("tests/data/indra_statements_format.json")
@@ -45,9 +53,9 @@ def test_simple_path_construction():
     for i in range(5):
         print( samples[i] )
 
-    G2.sample_from_likelihood( 10 )
+    #G2.sample_from_likelihood( 10 )
 
-    G.sample_from_proposal_debug()
+    #G.sample_from_proposal_debug()
 
 
 def test_inference():
@@ -56,105 +64,12 @@ def test_inference():
     print('\n\n\n\n')
     print( '\nCreating CAG' )
     G = AnalysisGraph.from_statements( statements )
-    #G = AnalysisGraph.from_json_file("tests/data/indra_statements_format.json")
 
     G.print_nodes()
 
     print( '\nName to vertex ID map entries' )
     G.print_name_to_vertex()
 
-    #print( '\nConstructing beta pdfs' )
-    #G.construct_beta_pdfs()
-
-    #G.find_all_paths()
-
-    print( '\nSetting initial state' )
-    G.set_initial_state()
-
     print( '\nSample from proposal debug' )
-    G.sample_from_proposal_debug()
+    #G.sample_from_proposal_debug()
 
-    # Get the original value of our parameter of interest (the ground truth
-    # value that we can use to evaluate our inference.
-    original_beta = G.get_beta( "UN/events/human/conflict", "UN/entities/human/food/food_security" )
-
-    print( "\noriginal_beta: ", original_beta )
-
-    print( '\nTaking a step' )
-    G.take_step()
-
-    print( '\nTaking second step' )
-    G.take_step()
-
-    beta_after_step = G.get_beta( "UN/events/human/conflict", "UN/entities/human/food/food_security" )
-
-    print( "\nbeta after step: ", beta_after_step )
-
-'''
-    statements = [ (("small", 1, "UN/events/human/conflict"), ("large", -1, "UN/entities/human/food/food_security"))]
-
-    #G = AnalysisGraph.from_statements( statements )
-    G = AnalysisGraph.from_json_file("tests/data/indra_statements_format.json")
-    G.print_nodes()
-    #G.construct_beta_pdfs()
-    #G.find_all_paths()
-    G.print_all_paths()
-    G.initialize( True )
-    samples = G.sample_from_prior()
-    G.sample_from_likelihood( 10 )
-    G.sample_from_proposal_debug()
-
-    G.set_initial_state()
-
-    # Get the original value of our parameter of interest (the ground truth
-    # value that we can use to evaluate our inference.
-    original_beta = G.get_beta( "conflict", "food_security" )
-
-    print( "original_beta: ", original_beta )
-
-    G.take_step()
-    beta_after_step = G.get_beta( "conflict", "food_security" )
-
-    print( "beta after step: ", beta_after_step )
-
-    # Create empty lists to hold the scores
-    betas = []
-    log_likelihoods = []
-    log_priors = []
-    map_estimates=[]
-
-    n_samples: int = 1000
-    for i, _ in enumerate(trange(n_samples)):
-        #G.sample_from_posterior(A)
-        G.take_step()
-        betas.append(G.get_beta( "conflict", "food_security" ))
-
-    print( betas )
-
-    fig, ax = plt.subplots()
-
-    plt.style.use("ggplot")
-    fig, axes = plt.subplots(2, 1, figsize=(8, 8), sharex=True)
-    axes[0].plot(betas, label="beta")
-    axes[0].plot(
-        original_beta * np.ones(len(betas)), label="original_beta"
-    )
-    # axes[1].plot(betas_2, label="beta_2")
-    # axes[1].plot(
-        # original_beta_2 * np.ones(len(betas)), label="original_beta_2"
-    # )
-
-    for ax in axes:
-        ax.legend()
-    plt.savefig("mcmc_results.pdf")
-    fig, ax = plt.subplots(1,2,sharey=True, figsize=(6,3))
-    sns.distplot(betas, ax=ax[0], norm_hist=True)
-
-    ax[0].set_title(f"$\\beta_{{c, fs}}={original_beta:.3f}$", fontsize=10)
-    # sns.distplot(betas_2, ax=ax[1], norm_hist=True)
-    # ax[1].set_title(f"$\\beta_{{hm, p}}={original_beta_2:.3f}$", fontsize=10)
-    plt.savefig("betas_combined.pdf")
-
-    # This tolerance seems to work for now, so I'm leaving it in.
-    # assert map_estimate == approx(original_beta, abs=0.1)
-    '''
