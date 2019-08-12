bd30d9c3b2d2d03093eb8fb2e50898d714012e8b
Paul Hein <pauldhein@email.arizona.edu>
2019-08-12 12:00:41 -0400

    Translate GrFN to WiringDiagram

---
+++ delphi/translators/GrFN2WiringDiagram/translate.py
@@ -0,0 +1,37 @@
+def translate_GrFN(out_node, G, homs):
+    func_node = list(G.predecessors(out_node))[0]
+    inputs = list(G.predecessors(func_node))
+    if len(inputs) == 0:
+        return []
+
+    input_str = f"otimes({', '.join(inputs)})"
+    statements = list()
+    compositions = list()
+    for var_node in inputs:
+        compositions.append(f"compose({var_node}, {out_node})")
+        statements.extend(translate_GrFN(var_node, G))
+    statements.extend()(compositions)
+    statements.append(f"{out_node} = Hom({func_node}, {input_str}, {out_node})")
+    homs.append(out_node)
+    return statements
+
+
+def to_wiring_diagram(G, filename):
+    header = [
+        "using Catlab",
+        "using Catlab.WiringDiagrams",
+        "using Catlab.Doctrines",
+        "import Catlab.Doctrines.⊗",
+        "import Base: ∘",
+        "⊗(a::WiringDiagram, b::WiringDiagram) = otimes(a, b)",
+        "∘(a::WiringDiagram, b::WiringDiagram) = compose(b, a)",
+        "⊚(a,b) = b ∘ a"
+    ]
+    cur_node = G.output_node
+    homs = []
+    statements = translate_GrFN(cur_node, G, homs)
+    statements.append(f"WiringDiagram({', '.join(homs)})")
+
+    with open(filename, "w+") as outfile:
+        outfile.write("\n".join(header))
+        outfile.write("\n".join(statements))
