a40e10fd9822d6a9efc866d5288bba228daa9f4d
Paul Hein <pauldhein@email.arizona.edu>
2019-08-16 17:19:46 -0400

    Finished correcting SIR-Gillespie-SD and updating networks.py

--- delphi/GrFN/networks.py
+++ delphi/GrFN/networks.py
@@ -152,8 +152,6 @@ class ComputationalGraph(nx.DiGraph):
                                 self.nodes[pred_var]["basename"],
                                 attrs["basename"],
                             )
-                if attrs["is_loop_index"]:
-                    G.add_edge(attrs["basename"], attrs["basename"])
 
         return G
 
@@ -248,117 +246,161 @@ class GroundedFunctionNetwork(ComputationalGraph):
             A GroundedFunctionNetwork object.
 
         """
-        functions = {d["name"]: d for d in data["functions"]}
+        functions = {d["name"]: d for d in data["containers"]}
+        occurrences = {}
         G = nx.DiGraph()
         scope_tree = nx.DiGraph()
 
-        def add_variable_node(
-            basename: str, parent: str, index: int, is_loop_index: bool = False
-        ):
+        def identity(x):
+            return x
+
+        def make_variable_name(parent: str, basename: str, index: str):
+            return f"{parent}::{basename}::{index}"
+
+        def add_variable_node(parent: str, basename: str, index: str):
+            full_var_name = make_variable_name(parent, basename, index)
             G.add_node(
-                f"{parent}::{basename}_{index}",
+                full_var_name,
                 type="variable",
                 color="maroon",
                 parent=parent,
-                label=f"{basename}_{index}",
+                label=f"{basename}::{index}",
                 basename=basename,
-                is_loop_index=is_loop_index,
                 padding=15,
                 value=None,
             )
+            return full_var_name
 
-        def find_correct_scope(cur_scope, var_name):
-            if cur_scope.parent is None:
-                return cur_scope.name, -1
+        def process_wiring_statement(stmt, scope, inputs, cname):
+            lambda_name = stmt["function"]["name"]
+            lambda_node_name = f"{scope.name}::" + lambda_name
 
-            new_scope = cur_scope.parent
+            stmt_type = lambda_name.split("__")[-3]
+            if stmt_type == "assign" and len(stmt["input"]) == 0:
+                stmt_type = "literal"
 
-            if var_name in new_scope.variables:
-                return new_scope.name, new_scope.variables[var_name]
-            else:
-                return find_correct_scope(new_scope, var_name)
+            for output in stmt["output"]:
+                (_, var_name, idx) = output.split("::")
+                node_name = add_variable_node(scope.name, var_name, idx)
+                G.add_edge(lambda_node_name, node_name)
 
-        def process_container(
-            scope, loop_index_variable: Optional[str] = None
-        ):
+            ordered_inputs = list()
+            for inp in stmt["input"]:
+                if inp.endswith("-1"):
+                    (parent, var_name, idx) = inputs[inp]
+                else:
+                    parent = scope.name
+                    (_, var_name, idx) = inp.split("::")
+
+                node_name = add_variable_node(parent, var_name, idx)
+                ordered_inputs.append(node_name)
+                G.add_edge(node_name, lambda_node_name)
+
+            G.add_node(
+                lambda_node_name,
+                type="function",
+                lambda_fn=getattr(lambdas, lambda_name),
+                func_inputs=ordered_inputs,
+                shape="rectangle",
+                parent=scope.name,
+                label=stmt_type[0].upper(),
+                padding=10,
+            )
+
+        def process_call_statement(stmt, scope, inputs, cname):
+            container_name = stmt["function"]["name"]
+            if container_name not in occurrences:
+                occurrences[container_name] = 0
+
+            new_container = functions[container_name]
+            container_color = "blue" if new_container["repeat"] else "green"
+            new_scope = ScopeNode(new_container, occurrences[container_name], parent=scope)
+            scope_tree.add_node(new_scope.name, color=container_color)
+            scope_tree.add_edge(scope.name, new_scope.name)
+
+            input_values = list()
+            for inp in stmt["input"]:
+                if inp.endswith("-1"):
+                    (parent, var_name, idx) = inputs[inp]
+                else:
+                    parent = scope.name
+                    (_, var_name, idx) = inp.split("::")
+                input_values.append((parent, var_name, idx))
+
+            callee_ret, callee_up = process_container(new_scope, input_values, container_name)
+
+            caller_ret, caller_up = list(), list()
+            for var in stmt["output"]:
+                parent = scope.name
+                (_, var_name, idx) = var.split("::")
+                node_name = add_variable_node(parent, var_name, idx)
+                caller_ret.append(node_name)
+
+            for var in stmt["updated"]:
+                parent = scope.name
+                (_, var_name, idx) = var.split("::")
+                node_name = add_variable_node(parent, var_name, idx)
+                caller_up.append(node_name)
+
+            for callee_var, caller_var in zip(callee_ret, caller_ret):
+                lambda_node_name = f"{callee_var}-->{caller_var}"
+                G.add_node(
+                    lambda_node_name,
+                    type="function",
+                    lambda_fn=identity,
+                    func_inputs=[callee_var],
+                    shape="rectangle",
+                    parent=scope.name,
+                    label="A",
+                    padding=10,
+                )
+                G.add_edge(callee_var, lambda_node_name)
+                G.add_edge(lambda_node_name, caller_var)
+
+            for callee_var, caller_var in zip(callee_up, caller_up):
+                lambda_node_name = f"{callee_var}-->{caller_var}"
+                G.add_node(
+                    lambda_node_name,
+                    type="function",
+                    lambda_fn=identity,
+                    func_inputs=[callee_var],
+                    shape="rectangle",
+                    parent=scope.name,
+                    label="A",
+                    padding=10,
+                )
+                G.add_edge(callee_var, lambda_node_name)
+                G.add_edge(lambda_node_name, caller_var)
+            occurrences[container_name] += 1
+
+        def process_container(scope, input_vals, cname):
+            input_vars = {a: v for a, v in zip(scope.arguments, input_vals)}
+
             for stmt in scope.body:
-                if "name" in stmt:
-                    stmt_type = functions[stmt["name"]]["type"]
-                    if stmt_type in ("assign", "condition", "decision"):
-                        if stmt_type == "assign":
-                            if "body" in functions[stmt["name"]]:
-                                stmt_type = "literal"
+                func_def = stmt["function"]
+                func_type = func_def["type"]
+                if func_type == "lambda_source":
+                    process_wiring_statement(stmt, scope, input_vars, cname)
+                elif func_type == "function_name":
+                    process_call_statement(stmt, scope, input_vars, cname)
+                else:
+                    raise ValueError(f"Undefined function type: {func_type}")
 
-                        output = stmt["output"]
-                        scope.variables[output["variable"]] = output["index"]
-                        add_variable_node(
-                            output["variable"], scope.name, output["index"]
-                        )
-                        G.add_edge(
-                            stmt["name"],
-                            f"{scope.name}::{output['variable']}_{output['index']}",
-                        )
+            return_list, updated_list = list(), list()
+            for var_name in scope.returns:
+                (_, basename, idx) = var_name.split("::")
+                return_list.append(make_variable_name(scope.name, basename, idx))
 
-                        ordered_inputs = list()
-                        for input in stmt.get("input", []):
-                            parent = scope.name
-                            index = input["index"]
-                            base_name = input["variable"]
-                            if (
-                                index == -1
-                                and base_name != loop_index_variable
-                            ):
-                                parent, index = find_correct_scope(
-                                    scope, base_name
-                                )
-
-                            add_variable_node(
-                                base_name,
-                                parent,
-                                index,
-                                base_name == loop_index_variable,
-                            )
-                            node_name = f"{parent}::{base_name}_{index}"
-                            ordered_inputs.append(node_name)
-                            G.add_edge(node_name, stmt["name"])
-
-                        G.add_node(
-                            stmt["name"],
-                            type="function",
-                            lambda_fn=getattr(lambdas, stmt["name"]),
-                            func_inputs=ordered_inputs,
-                            shape="rectangle",
-                            parent=scope.name,
-                            label=stmt_type[0].upper(),
-                            padding=10,
-                        )
-
-                    elif stmt_type == "loop_plate":
-                        index_variable = functions[stmt["name"]][
-                            "index_variable"
-                        ]
-                        scope_tree.add_node(stmt["name"], color="blue")
-                        scope_tree.add_edge(scope.name, stmt["name"])
-                        new_scope = ScopeNode(
-                            functions[stmt["name"]], parent=scope
-                        )
-                        process_container(
-                            new_scope, loop_index_variable=index_variable
-                        )
-                    else:
-                        pass
-                elif "function" in stmt and stmt["function"] != "print":
-                    scope_tree.add_node(stmt["function"], color="green")
-                    scope_tree.add_edge(scope.name, stmt["function"])
-                    new_scope = ScopeNode(
-                        functions[stmt["function"]], parent=scope
-                    )
-                    process_container(new_scope)
+            for var_name in scope.updated:
+                (_, basename, idx) = var_name.split("::")
+                updated_list.append(make_variable_name(scope.name, basename, idx))
+            return return_list, updated_list
 
         root = data["start"]
-        scope_tree.add_node(root, color="green")
-        cur_scope = ScopeNode(functions[root])
-        process_container(cur_scope)
+        occurrences[root] = 0
+        cur_scope = ScopeNode(functions[root], occurrences[root])
+        scope_tree.add_node(cur_scope.name, color="green")
+        process_container(cur_scope, [], root)
         return cls(G, scope_tree)
 
     @classmethod
@@ -391,8 +433,6 @@ class GroundedFunctionNetwork(ComputationalGraph):
         )
         lambdas = importlib.__import__(stem + "_lambdas")
         return cls.from_dict(pgm_dict, lambdas)
-
-
 
     @classmethod
     def from_fortran_file(cls, fortran_file: str, tmpdir: str = "."):
@@ -597,15 +637,15 @@ class GroundedFunctionNetwork(ComputationalGraph):
         """ Export to a PyGraphviz AGraph object. """
         A = nx.nx_agraph.to_agraph(self)
         A.graph_attr.update(
-            {"dpi": 227, "fontsize": 20, "fontname": "Menlo", "rankdir": "TB"}
+            {"dpi": 227, "fontsize": 20, "fontname": "Menlo", "rankdir": "LR"}
         )
         A.node_attr.update({"fontname": "Menlo"})
 
         def build_tree(cluster_name, root_graph):
             subgraph_nodes = [
-                n[0]
-                for n in self.nodes(data=True)
-                if n[1]["parent"] == cluster_name
+                node_name
+                for node_name, node_data in self.nodes(data=True)
+                if node_data["parent"] == cluster_name
             ]
             root_graph.add_nodes_from(subgraph_nodes)
             subgraph = root_graph.add_subgraph(
@@ -613,6 +653,7 @@ class GroundedFunctionNetwork(ComputationalGraph):
                 name=f"cluster_{cluster_name}",
                 label=cluster_name,
                 style="bold, rounded",
+                rankdir="LR"
             )
             for n in self.scope_tree.successors(cluster_name):
                 build_tree(n, subgraph)
--- scripts/SIR_demo/demo_generate_grfn.py
+++ scripts/SIR_demo/demo_generate_grfn.py
@@ -1,4 +1,5 @@
 import sys
+import importlib
 
 from delphi.GrFN.networks import GroundedFunctionNetwork
 
@@ -9,11 +10,15 @@ from delphi.GrFN.networks import GroundedFunctionNetwork
 print('Running demo_generate_grfn.py')
 data_dir = "scripts/SIR_Demo/"
 sys.path.insert(0, "scripts/SIR_Demo/")
+model_file = "SIR-Gillespie-SD"
+json_file = f"{model_file}_GrFN.json"
+lambdas = importlib.__import__(f"{model_file}_lambdas")
 
-grfn = GroundedFunctionNetwork.from_fortran_file("scripts/SIR_Demo/SIR-simple.f")
+# grfn = GroundedFunctionNetwork.from_fortran_file("scripts/SIR_Demo/SIR-simple.f")
+grfn = GroundedFunctionNetwork.from_json_and_lambdas(json_file, lambdas)
 agraph = grfn.to_agraph()
 agraph.draw('SIR-gillespie.pdf', prog='dot')
-CAG = grfn.to_CAG_agraph()
-CAG.draw('SIR-gillespie-CAG.pdf', prog='dot')
+# CAG = grfn.to_CAG_agraph()
+# CAG.draw('SIR-gillespie-CAG.pdf', prog='dot')
 
 # -----------------------------------------------------------------------------
