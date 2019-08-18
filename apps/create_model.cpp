2fbba53f7df8afaf735b9f3c6b349161c9af7df6
Loren Champlin <champlin@email.arizona.edu>
2019-08-18 13:44:24 -0700

    Changed to fit the settings I am attempting in jupyter

--- apps/create_model.cpp
+++ apps/create_model.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-  auto G = AnalysisGraph::from_json_file(argv[1]);
-  G.get_subgraph_for_concept("UN/entities/human/food/food_security");
+  auto G = AnalysisGraph::from_json_file(argv[1],0.8,0.5);
+  G.get_subgraph_for_concept("UN/events/human/human_migration",2,true);
+  G.prune(2);
   G.to_png();
   return EXIT_SUCCESS;
 }
