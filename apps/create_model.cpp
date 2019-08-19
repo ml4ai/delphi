280ddb777887c8bf686ce44e17ef02a343f4d20b
Loren Champlin <champlin@email.arizona.edu>
2019-08-19 09:59:02 -0700

    Setup to recreate segfault 11 error

--- apps/create_model.cpp
+++ apps/create_model.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  auto G = AnalysisGraph::from_json_file(argv[1], 0.8, 0.5);
+  auto G = AnalysisGraph::from_json_file(argv[1], 0.9, 0.0);
   G = G.get_subgraph_for_concept("UN/events/human/human_migration", 2, true);
-  G.to_png();
   G.map_concepts_to_indicators();
   G.replace_indicator("UN/events/human/human_migration",
                       "Net migration",
                       "New asylum seeking applicants",
                       "UNHCR");
   G.construct_beta_pdfs();
+  G.to_png();
   G.train_model(2015, 1, 2015, 12, 100, 900);
   return EXIT_SUCCESS;
 }
