02a4c7ae15e1d1580586e325c95ba6d8fcfb3f2d
Paul Hein <pauldhein@email.arizona.edu>
2019-09-16 10:17:25 -0400

    Adding make extensions to docker test script

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-
 docker pull pauldhein/project-containers
-docker run -d -p 127.0.0.1:80:4567 pauldhein/project-containers /bin/sh -c "git clone https://github.com/ml4ai/delphi.git /"
-docker ps -a
-docker run pauldhein/project-containers /bin/sh -c "cd /repos/delphi; git checkout -qf travis-docker-ci"
+docker run pauldhein/project-containers /bin/sh -c "git fetch; git checkout master; git pull"
+docker run pauldhein/project-containers /bin/sh -c "pip install -e .[test,docs]"
+docker run pauldhein/project-containers /bin/sh -c "make extensions"
+
 docker run pauldhein/project-containers /bin/sh -c "make test"
-docker run pauldhein/project-containers /bin/sh -c "cd /delphi/delphi/docs; make apidocs; make html"
+# docker run -w="/repo/delphi/docs" pauldhein/project-containers /bin/sh -c "make apidocs; make html"
