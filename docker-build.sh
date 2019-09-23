12dc29c21de9b8d1dd083aeef0820fca23018007
Paul Hein <pauldhein@email.arizona.edu>
2019-09-23 14:16:16 -0400

    Preparing to run working travis file

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker pull pauldhein/project-containers:delphi
 docker run -itd --rm --name build-con pauldhein/project-containers:delphi
-docker exec build-con bash -c 'git fetch; git checkout 5f8eec2bfbe66b98e91b98ebda5db4de7e45e580'
+docker exec build-con bash -c 'git fetch; git checkout 04fedc0cb701f90112648440d704858a8d3acc72'
 docker exec build-con bash -c 'rm -rf /repo/delphi/build'
 docker exec build-con pip install -e .[test,docs]
 docker exec build-con make test
