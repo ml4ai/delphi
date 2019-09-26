0f01e5460d6f223b8966b307234ee8460fb3b81b
Paul Hein <pauldhein@email.arizona.edu>
2019-09-26 11:53:46 -0400

    Adding coverage and docs output to travis testing

--- docker-build.sh
+++ docker-build.sh
@@ -6,4 +6,6 @@ docker exec build-con git checkout 04fedc0cb701f90112648440d704858a8d3acc72
 docker exec build-con rm -rf /repo/delphi/build
 docker exec build-con pip install -e .[test,docs]
 docker exec build-con make test
-# docker exec -w /repo/delphi/docs build-con bash -c "make apidocs; make html"
+docker exec -w /repo/delphi/docs build-con make apidocs
+docker exec -w /repo/delphi/docs build-con make html
+docker cp build-con:/repo/delphi/docs ~/Desktop
