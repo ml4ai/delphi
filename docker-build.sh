611979715d1834fd5f99ce195439639d58a54ccb
Paul Hein <pauldhein@email.arizona.edu>
2019-09-26 10:39:38 -0400

    Better travis test

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 docker pull pauldhein/project-containers:delphi
 docker run -itd --rm --name build-con pauldhein/project-containers:delphi
-docker exec build-con bash -c 'git fetch; git checkout 04fedc0cb701f90112648440d704858a8d3acc72'
-docker exec build-con bash -c 'rm -rf /repo/delphi/build'
+docker exec build-con git fetch
+docker exec build-con git checkout 04fedc0cb701f90112648440d704858a8d3acc72
+docker exec build-con rm -rf /repo/delphi/build
 docker exec build-con pip install -e .[test,docs]
 docker exec build-con make test
 # docker exec -w /repo/delphi/docs build-con bash -c "make apidocs; make html"
