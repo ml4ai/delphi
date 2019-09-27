14c7e5df8826daa5dd21aeff5ceda71f4e7ac687
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-09-27 07:14:49 -0700

    Adding Boost program options to create_model executable

--- apps/create_model.cpp
+++ apps/create_model.cpp
@@ -2,14 +2,49 @@
 
 #include "AnalysisGraph.hpp"
 #include "spdlog/spdlog.h"
-
-using namespace std;
+#include <boost/program_options.hpp>
 
 int main(int argc, char* argv[]) {
+  using namespace std;
+  using namespace boost::program_options;
+
+  options_description desc("Allowed options");
+  positional_options_description pd;
+
+  // Path to JSON-serialized INDRA statements
+  string stmts;
+
+  desc.add_options()
+    ("help,h", "Executable for creating Delphi models")
+    ("stmts", value<string>(&stmts), "Path to JSON-serialized INDRA statements")
+    ("belief_score_cutoff", value<double>()->default_value(0.9),
+     "INDRA belief score cutoff for statements to be included in the model.")
+    ("grounding_score_cutoff", value<double>()->default_value(0.7),
+     "Grounding score cutoff for statements to be included in the model")
+  ;
+
+  // Setting positional arguments
+  pd.add("stmts", 1);
+  pd.add("belief_score_cutoff", 2);
+  pd.add("grounding_score_cutoff", 3);
+
+  variables_map vm;
+  store(parse_command_line(argc, argv, desc), vm);
+  notify(vm);
+
+  if (vm.count("help") || argc == 1) {
+    cout << desc << endl;
+    return 1;
+  }
+
   RNG *R = RNG::rng();
   R->set_seed(87);
   spdlog::set_level(spdlog::level::debug);
-  auto G = AnalysisGraph::from_json_file(argv[1], 0.9, 0.0);
+  auto G = AnalysisGraph::from_json_file(
+      stmts,
+      vm["belief_score_cutoff"].as<double>(), 
+      vm["grounding_score_cutoff"].as<double>());
+
   G.map_concepts_to_indicators();
   G.construct_beta_pdfs();
   G.to_png();
