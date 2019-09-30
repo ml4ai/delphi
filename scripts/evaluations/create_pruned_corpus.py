9ed58649618469724426bac0a20ebc8e48eef16d
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-09-29 18:03:57 -0700

    Updating script to prune corpus for evaluation - changing from UN to WM ontology

--- scripts/evaluations/create_pruned_corpus.py
+++ scripts/evaluations/create_pruned_corpus.py
@@ -2,17 +2,22 @@
 
 import sys
 import json
+from tqdm import tqdm
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         sts = json.load(f)
     filtered_sts = []
-    for s in sts:
+    for s in tqdm(sts):
         if s["type"] == "Influence":
             for c in (s["subj"], s["obj"]):
-                for key in [k for k in c["db_refs"] if k != "UN"]:
-                    del c["db_refs"][key]
+                for k in list(c["concept"]["db_refs"].keys()):
+                    if k!="WM":
+                        del c["concept"]["db_refs"][k]
 
+                if c["concept"]["db_refs"].get("WM") is not None:
+                    c["concept"]["db_refs"]["WM"] = c["concept"]["db_refs"]["WM"][0:1]
             filtered_sts.append(s)
+
     with open(sys.argv[2], "w") as f:
         f.write(json.dumps(filtered_sts, indent=2))
