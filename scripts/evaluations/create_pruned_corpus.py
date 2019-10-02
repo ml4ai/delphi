b4144229a56cf217a17da85226a9bc5a851d9fc6
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-10-01 21:59:40 -0700

    Refactored pruned corpus creation script for readability, filtering out more improperly grounded statements, such as ones containing concepts grounded to causal_factor

--- scripts/evaluations/create_pruned_corpus.py
+++ scripts/evaluations/create_pruned_corpus.py
@@ -1,23 +1,41 @@
 """ Prune the preassembled corpus json file """
 
-import sys
 import json
+import sys
+
 from tqdm import tqdm
+
+
+def isInfluenceStatement(s):
+    return s["type"] == "Influence"
+
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         sts = json.load(f)
     filtered_sts = []
-    for s in tqdm(sts):
-        if s["type"] == "Influence":
-            for c in (s["subj"], s["obj"]):
-                for k in list(c["concept"]["db_refs"].keys()):
-                    if k!="WM":
-                        del c["concept"]["db_refs"][k]
+    hasWMKey = lambda x: x.get("WM") is not None
+    hasNonZeroWMGroundingList = lambda x: len(x["WM"]) != 0
 
-                if c["concept"]["db_refs"].get("WM") is not None:
-                    c["concept"]["db_refs"]["WM"] = c["concept"]["db_refs"]["WM"][0:1]
-            filtered_sts.append(s)
+    for s in filter(
+        lambda s: isInfluenceStatement(s)
+        and all(
+            map(
+                lambda x: hasWMKey(x) and hasNonZeroWMGroundingList(x),
+                map(lambda x: s[x]["concept"]["db_refs"], ("subj", "obj")),
+            )
+        ),
+        sts,
+    ):
+
+        for c in (s["subj"], s["obj"]):
+            for k in list(c["concept"]["db_refs"].keys()):
+                if k != "WM":
+                    del c["concept"]["db_refs"][k]
+                c["concept"]["db_refs"]["WM"] = c["concept"]["db_refs"]["WM"][
+                    0:1
+                ]
+        filtered_sts.append(s)
 
     with open(sys.argv[2], "w") as f:
         f.write(json.dumps(filtered_sts, indent=2))
