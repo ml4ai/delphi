57b0c252e5690d7dc8264e892d3ad9952a8f5ac7
Adarsh Pyarelal <adarsh@email.arizona.edu>
2019-10-06 21:06:51 -0700

    Refactoring dssat data processing scripts

--- scripts/data_processing/dssat-maize-historical-ethiopia.py
+++ scripts/data_processing/dssat-maize-historical-ethiopia.py
@@ -1,38 +1,63 @@
+import sys
 import pandas as pd
 from glob import glob
 import numpy as np
 
 dfs = list()
 
-directoryPath = 'data/raw/data_for_november_2019_evaluation/ethiopia_east_africa_data/'
-filenames = glob(directoryPath + '*.csv')
+filenames = glob("data/raw/data_for_november_2019_evaluation/*/dssat/*.csv")
 
-
-colnames = ['HWAMA', 'TAVGA', 'PRCPA']
 for filename in filenames:
-    df = pd.read_csv(filename, encoding='latin-1',usecols=lambda x: x in colnames)
-    df['Year'] = np.arange(2018-len(df), 2018)
-    big_frame = pd.DataFrame({'Value':df.iloc[:,0].values, 'Variable': df.columns[0], 'Year':df['Year'].values})
+    df = pd.read_csv(
+        filename, encoding="latin-1", usecols=["HWAMA", "TAVGA", "PRCPA"]
+    )
+    df["Year"] = np.arange(2018 - len(df), 2018)
+    big_frame = pd.DataFrame(
+        {
+            "Value": df.iloc[:, 0].values,
+            "Variable": df.columns[0],
+            "Year": df["Year"].values,
+        }
+    )
 
     for col in df.columns[1:-1]:
         val = df[col].values
         indicator = col
-        df_new = pd.DataFrame({'Value':val, 'Variable':indicator})
-        df_new = pd.concat([df_new, df['Year']], axis=1, join='inner')
-        big_frame = pd.concat([big_frame, df_new], sort=False, ignore_index=True)
-        if filename == directoryPath + 'ETH_gambela_maize_meher_historical_all.csv':
-            big_frame['State'] = 'Gambela'
+        df_new = pd.DataFrame({"Value": val, "Variable": indicator})
+        df_new = pd.concat([df_new, df["Year"]], axis=1, join="inner")
+        big_frame = pd.concat(
+            [big_frame, df_new], sort=False, ignore_index=True
+        )
+        if "gambela" in filename:
+            big_frame["State"] = "Gambela"
+        elif "jonglei" in filename:
+            big_frame["State"] = "Jonglei"
         else:
-            big_frame['State'] = None
+            big_frame["State"] = None
+
+        if "SSD" in filename:
+            big_frame["Country"] = "South Sudan"
+        elif "ETH" in filename:
+            big_frame["Country"] = "Ethiopia"
+        else:
+            raise Exception("Neither SSD or ETH in filename - are you sure you
+                    "are passing in the correct files?")
+
     dfs.append(big_frame)
-    
+
 big_frame = pd.concat(dfs, ignore_index=True, sort=True)
 
-big_frame['Country'], big_frame['Source'] = 'South Sudan', 'DSSAT'
-big_frame['Month'], big_frame['Unit'], big_frame['County'] = None, None, None
+big_frame["Month"], big_frame["County"] = None, None, None
+big_frame[big_frame["Variable"] == "HWAMA"]["Unit"] = "kg/ha"
+big_frame[big_frame["Variable"] == "PRCPA"]["Unit"] = "mm"
+big_frame[big_frame["Variable"] == "TAVGA"]["Unit"] = "Celsius"
 
-dict_var = {'HWAMA': 'Average Harvested Weight at Maturity (Maize)', 'TAVGA' : 'Average Temperature for Maize Production', 'PRCPA': 'Average Precipitation for Maize Production'}
+dict_var = {
+    "HWAMA": "Average Harvested Weight at Maturity (Maize)",
+    "TAVGA": "Average Temperature",
+    "PRCPA": "Average Precipitation",
+}
 
-big_frame['Variable'].replace(dict_var, inplace=True)
+big_frame["Variable"].replace(dict_var, inplace=True)
 
-big_frame.to_csv('data/dssat-maize-data2.csv', index=False)
+big_frame.to_csv(sys.argv[1], index=False, sep="\t")
