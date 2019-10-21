38e7d53db349107ab8f1fdd8030fe8697c3feb5d
Pratik Bhandari <pratik.bhd@gmail.com>
2019-10-21 00:42:04 -0700

    new goto test file

--- tests/data/program_analysis/goto/goto_02.py
+++ tests/data/program_analysis/goto/goto_02.py
@@ -33,14 +33,18 @@ def factorial():
                 write_list_stream = [i[0], fact[0]]
                 write_line = format_10_obj.write_line(write_list_stream)
                 sys.stdout.write(write_line)
-                if (i[0] == n[0]):
-                    return
-                label_flag_2[0] = True
-        n[0] = 10
-        fact[0] = 1
-        i[0] = 0
-        goto_flag_1[0] = False
-        label_flag_3[0] = True
+                if (i[0] != n[0]):
+                    label_flag_2[0] = True
+                else:
+                    break
+        if ((not(i[0]) and not(n[0])) or (i[0] != n[0])):
+            n[0] = 10
+            fact[0] = 1
+            i[0] = 0
+            goto_flag_1[0] = False
+            label_flag_3[0] = True
+        else:
+            break
     
 
 factorial()
