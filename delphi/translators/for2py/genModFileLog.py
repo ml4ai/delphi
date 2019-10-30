12402b691884a5049f20a54cf664568336d51c4f
Terrence Lim <hlim1@email.arizona.edu>
2019-10-30 13:35:39 -0700

    Able to generate a complete log JSON file.

--- delphi/translators/for2py/genModFileLog.py
+++ delphi/translators/for2py/genModFileLog.py
@@ -79,14 +79,44 @@ def get_file_list_in_directory(dir_path):
 
     return files
 
-def modules_from_file(file_path, file_to_mod_mapper):
-    """This function checks and extracts module names, if exist,
-    from the file, and map it to the file name.
+def modules_from_file(file_path, file_to_mod_mapper, mod_to_file_mapper):
+    """This function checks either the module and file path already exist
+    int the log file. If it does, then it compares the last_modified_time
+    in the log file with the last modified time of file in disk. Then, it
+    will call 'populate_mapper' function if file was not already looked
+    before or the file was modified since looked last time.
     Args:
         file_path (str): File path that is guaranteed to exist in
         the directory.
         file_to_mod_mapper (dict): Dictionary of lists that will
         hold file-to-module_name mappings.
+        mod_to_file_mapper (dict): Dictionary that holds a module
+        to its residing file path.
+    Returns:
+        None.
+    """
+    last_modified_time = get_file_last_modified_time(file_path)
+
+    if file_path in file_to_mod_mapper:
+        last_modified_time_in_log = file_to_mod_mapper[file_path][-1]
+        if last_modified_time  == last_modified_time_in_log:
+            return
+        else:
+            assert (
+                    last_modified_time > last_modified_time_in_log
+            ), "Last modified time in the log file cannot be later than on disk file's time."
+            populate_mapper(file_path, file_to_mod_mapper, mod_to_file_mappeR)
+    else:
+        populate_mapper(file_path, file_to_mod_mapper, mod_to_file_mappeR)
+
+def populate_mappers(file_path, file_to_mod_mapper, mod_to_file_mapper):
+    """This function populates two mappers by checking and extracting 
+    module names, if exist, from the file, and map it to the file name.
+    Args:
+        file_to_mod_mapper (dict): Dictionary of lists that will
+        hold file-to-module_name mappings.
+        mod_to_file_mapper (dict): Dictionary that holds a module
+        to its residing file path.
     Returns:
         None.
     """
@@ -107,30 +137,73 @@ def modules_from_file(file_path, file_to_mod_mapper):
         module_names.extend(re.findall(r'(?i)(?<=endmodule )[^-. \n]*', file_content))
 
     file_to_mod_mapper[file_path] = module_names
+    file_to_mod_mapper[file_path].append(last_modified_time)
+    for mod in module_names:
+        mod_to_file_mapper[mod] = [file_path]
 
-def get_file_last_modified_time(file_name, file_to_mod_mapper):
-    file_stat = os.stat(file_name)
+def get_file_last_modified_time(file_path):
+    """This function retrieves the file status and assigns the last modified
+    time of a file at the end of the file_to_mod_mapper[file_path] list.
 
-def mod_file_log_generator():
+    Params:
+        file_path (str): File path that is guaranteed to exist in
+        the directory.
+    Returns:
+        int: Last modified time represented in integer.
+    """
+    file_stat = os.stat(file_path)
+    return file_stat[8]
+
+def mod_file_log_generator(temp_dir=None):
+    """This function is like a main function to invoke other functions
+    to perform all checks and population of mappers. Though, loading of
+    and writing to JSON file will happen in this function.
+
+    Args: 
+        temp_dir (str): Temporary directory that log JSON file resides.
+
+    Returns:
+        None.
+    """
+    module_log_file = "modFileLog.json"
+    if temp_dir == None:
+        temp_dir = "./tmp"
+    module_log_file_path = temp_dir + "/" + module_log_file
+
+    # If module log file already exists, load the data.
+    if isfile(module_log_file_path):
+        with open(module_log_file_path) as json_f:
+            module_logs = json.load(json_f)
+        # This will hold the file-to-module and file last modified date info.
+        # One thing to notice is that the last index will be a place for
+        # last modified time for file.
+        # Structure (One-to-Many):
+        #   {
+        #       "__file_name__" : ["__module_name__",...,"last_modified_time"],
+        #       ...
+        #   }
+        file_to_mod_mapper = module_logs["file_to_mod"]
+        # This will hold the module-to-file mapping, so any program that accesses
+        # module log JSON file can directly access the file path with the module
+        # name specified with "USE" without looping through the file_to_mod mapper.
+        # Structure (One-to-One).
+        mod_to_file_mapper = module_logs["mod_to_file"]
+    else:
+        file_to_mod_mapper = {}
+        mod_to_file_mapper = {}
+
     file_path = parse_args()
     dir_path = get_directory_path(file_path)
-
     files = get_file_list_in_directory(dir_path)
-
-    # This will hold the file-to-module and file last modified date info.
-    # One thing to notice is that the last index will be the place for
-    # last modified time for file.
-    # Structure:
-    #   {
-    #       "__file_name__" : ["__module_name__",...,"last_modified_time"],
-    #       ...
-    #   }
-    file_to_mod_mapper = {}
+    
     for f in files:
         file_path = dir_path + f
-        modules_from_file(file_path, file_to_mod_mapper)
+        modules_from_file(file_path, file_to_mod_mapper, mod_to_file_mapper)
 
-    print (file_to_mod_mapper)
+    module_log = {"file_to_mod": file_to_mod_mapper, "mod_to_file": mod_to_file_mapper}
+
+    with open(module_log_file_path, 'w+') as json_f:
+        json_f.write(json.dumps(module_log, indent=2))
 
 if __name__ == "__main__":
     mod_file_log_generator()
