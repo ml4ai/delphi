6389a8d134e18beab62c6b5409d2da05e995ec21
Terrence Lim <hlim1@email.arizona.edu>
2019-11-03 18:54:52 -0700

    More test cases, such as module in different directory.

---
+++ tests/data/program_analysis/multiple_files/test_multiple_files_03.f
@@ -0,0 +1,7 @@
+      program testing
+        use mathModule_4
+        implicit none
+
+        print *, "Module in another file"
+
+      end program testing
---
+++ tests/data/program_analysis/multiple_files/test_multiple_files_04.f
@@ -0,0 +1,8 @@
+      program testing
+        use module_def
+        use module_mno
+        implicit none
+
+        print *, "test_multiple_files_03.f"
+
+      end program testing
