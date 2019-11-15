6ece90057b98556468281fccc098e89ddc55ca5c
skhan <souratosh.khan86@gmail.com>
2019-11-15 13:45:12 -0700

    adding changes to sensitivity.py

--- delphi/GrFN/sensitivity.py
+++ delphi/GrFN/sensitivity.py
@@ -7,6 +7,17 @@ from delphi.GrFN.utils import timeit
 
 
 @timeit
+def sobol_analysis(network, num_samples, prob_def):
+    print("Sampling via Saltelli...")
+    samples = saltelli.sample(prob_def, num_samples, calc_second_order=True)
+    samples = np.split(samples, samples.shape[1], axis=1)
+    values = {n: torch.tensor(s) for n, s in zip(prob_def["names"], samples)}
+    print("Running GrFN...")
+    Y = network.run(values).squeeze().numpy()
+    print("Analyzing via Sobol...")
+    return sobol.analyze(prob_def, Y, print_to_console=True)
+
+@timeit
 def FAST_analysis(network, num_samples, prob_def):
     print("Sampling via FAST sampler...")
     samples = fast_sampler.sample(prob_def, num_samples)
