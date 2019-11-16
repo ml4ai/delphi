b0dc3a2a77f1a93ee5755facc7ad97f6b93ad6f7
Pratik Bhandari <pratik.bhd@gmail.com>
2019-11-16 01:16:02 -0700

    bugfix

--- delphi/translators/for2py/genPGM.py
+++ delphi/translators/for2py/genPGM.py
@@ -2320,7 +2320,7 @@ class GrFNGenerator(object):
         # Get the GrFN element of the RHS side of the assignment which are
         # the variables involved in the assignment operations.
         sources = self.gen_grfn(node.value, state, "assign")
-        print(sources)
+
         # Detect assigns which are string initializations of the
         # following form: String(10). String initialization of the form
         # String(10, "abcdef") are valid assignments where the index of the
@@ -2496,7 +2496,6 @@ class GrFNGenerator(object):
             # string assignment.
             if not is_string_annotation:
                 grfn["variables"].append(variable_spec)
-        print("---")
         return [grfn]
 
     def process_tuple(self, node, state, *_):
@@ -2732,7 +2731,6 @@ class GrFNGenerator(object):
         target_string = f"@variable::{target_name}::{target['var']['index']}"
 
         for src in sources:
-            print(src)
             # Check for a write to a file
             if re.match(r"\d+", target_name) and "list" in src:
                 return fn
@@ -2789,8 +2787,6 @@ class GrFNGenerator(object):
                     string_var = src["call"]["function"].split(".")[0]
                     index = state.last_definitions[string_var]
                     source.append(f"@variable::{string_var}::{index}")
-                else:
-                    source = []
 
             elif "var" in src:
                 source_string = f"@variable::{src['var']['variable']}::" \
@@ -2807,7 +2803,6 @@ class GrFNGenerator(object):
             # else:
             #     assert False, f"Unidentified source: {src}"
 
-        print(source)
         # Removing duplicates
         unique_source = []
         [unique_source.append(obj) for obj in source if obj not in
