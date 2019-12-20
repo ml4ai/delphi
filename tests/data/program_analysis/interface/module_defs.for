f35a4e493ac0cbae197ca9adaee3b14556b73477
Terrence Lim <hlim1@email.arizona.edu>
2019-12-20 13:45:38 -0700

    Updating interface test files

--- tests/data/program_analysis/interface/module_defs.for
+++
@@ -1,6 +0,0 @@
-!     Data construct for control variables
-      TYPE Control_Type
-        INTEGER   DAS, DYNAMIC, FROP, ErrCode, LUNIO, MULTI, N_ELEMS
-        INTEGER   NYRS, REPNO, ROTNUM, RUN, TRTNUM
-        INTEGER   YRDIF, YRDOY, YRSIM
-      END TYPE Control_Type
