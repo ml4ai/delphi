aa080797d9c9152f6433d7e6ba9459a60c6ebb10
Paul Hein <pauldhein@email.arizona.edu>
2020-01-31 12:20:13 -0500

    NumPy-thonic run, sobol_analysis, and S2_surface

--- delphi/GrFN/networks.py
+++ delphi/GrFN/networks.py
@@ -533,55 +533,44 @@ class GroundedFunctionNetwork(ComputationalGraph):
     def sobol_analysis(
         self, num_samples, prob_def, use_torch=False, var_types=None
     ):
-        def create_input_tensor(name, samples):
-            type_info = var_types[name]
-            if type_info[0] == str:
-                (val1, val2) = type_info[1]
-                return np.where(samples >= 0.5, val1, val2)
-            else:
-                return torch.tensor(samples)
+        def create_input_vector(name, vector):
+            if var_types is None:
+                return vector
 
-        def get_input(name, sample):
             type_info = var_types[name]
-            if type_info[0] == str:
-                (val1, val2) = type_info[1]
-                return val1 if sample >= 0.5 else val2
-            else:
-                return sample
+            if type_info[0] != str:
+                return vector
 
+            if type_info[0] == str:
+                (str1, str2) = type_info[1]
+                return np.where(vector >= 0.5, str1, str2)
+            else:
+                raise ValueError(f"Unrecognized value type: {type_info[0]}")
+
+        # Create an array of samples from the bounds supplied in prob_def
         samples = saltelli.sample(
             prob_def, num_samples, calc_second_order=True
         )
-        if use_torch:
-            samples = np.split(samples, samples.shape[1], axis=1)
-            samples = [s.squeeze() for s in samples]
-            if var_types is None:
-                values = {
-                    n: torch.tensor(s)
-                    for n, s in zip(prob_def["names"], samples)
-                }
-            else:
-                values = {
-                    n: create_input_tensor(n, s)
-                    for n, s in zip(prob_def["names"], samples)
-                }
-            Y = self.run(values, torch_size=len(samples[0])).numpy()
-        else:
-            Y = np.zeros(samples.shape[0])
-            for i, sample in enumerate(samples):
-                if var_types is None:
-                    values = {n: v for n, v in zip(prob_def["names"], sample)}
-                else:
-                    values = {n: get_input(n, val)
-                              for n, val in zip(prob_def["names"], sample)}
 
-                res = self.run(values)
-                Y[i] = res[0]
+        # Create vectors of sample inputs to run through the model
+        vectorized_sample_list = np.split(samples, samples.shape[1], axis=1)
+        vectorized_input_samples = {
+            name: create_input_vector(name, vector)
+            for name, vector in zip(prob_def["names"], vectorized_sample_list)
+        }
 
-        return sobol.analyze(prob_def, Y)
+        # Produce model output and reshape for analysis
+        outputs = self.run(vectorized_input_samples)
+        Y = outputs[0]
+        Y = Y.reshape((Y.shape[0],))
 
-    def S2_surface(self, sizes, bounds, presets, use_torch=False,
-            num_samples=10):
+        # Recover the sensitivity indices from the sampled outputs
+        S = sobol.analyze(prob_def, Y)
+        return S
+
+    def S2_surface(
+        self, sizes, bounds, presets, use_torch=False, num_samples=10
+    ):
         """Calculates the sensitivity surface of a GrFN for the two variables with
         the highest S2 index.
 
@@ -610,33 +599,24 @@ class GroundedFunctionNetwork(ComputationalGraph):
         S2 = Si["S2"]
         (s2_max, v1, v2) = get_max_s2_sensitivity(S2)
 
-        x_var = args[v1]
-        y_var = args[v2]
-        search_space = [(x_var, bounds[x_var]), (y_var, bounds[y_var])]
-        preset_vals = {
-            arg: presets[arg]
+        x_var, y_var = args[v1], args[v2]
+        x_bounds, y_bounds = bounds[x_var], bounds[y_var]
+        (x_sz, y_sz) = sizes
+        X = np.linspace(*x_bounds, x_sz)
+        Y = np.linspace(*y_bounds, y_sz)
+        Xv, Yv = np.meshgrid(X, Y)
+
+        input_vectors = {
+            arg: np.full_like(Xv, presets[arg])
             for i, arg in enumerate(args)
             if i != v1 and i != v2
         }
+        input_vectors.update({x_var: Xv, y_var: Yv})
 
-        X = np.linspace(*search_space[0][1], sizes[0])
-        Y = np.linspace(*search_space[1][1], sizes[1])
-
-        if use_torch:
-            Xm, Ym = torch.meshgrid(torch.tensor(X), torch.tensor(Y))
-            inputs = {n: torch.full_like(Xm, v) for n, v in presets.items()}
-            inputs.update({search_space[0][0]: Xm, search_space[1][0]: Ym})
-            Z = self.run(inputs).numpy()
-        else:
-            Xm, Ym = np.meshgrid(X, Y)
-            Z = np.zeros((len(X), len(Y)))
-            for x, y in itertools.product(range(len(X)), range(len(Y))):
-                inputs = {n: v for n, v in presets.items()}
-                inputs.update({search_space[0][0]: x, search_space[1][0]: y})
-                Z[x][y] = self.run(inputs)[0]
+        outputs = self.run(input_vectors)
+        Z = outputs[0]
 
         return X, Y, Z, x_var, y_var
-
 
     def to_FIB(self, other):
         """ Creates a ForwardInfluenceBlanket object representing the
