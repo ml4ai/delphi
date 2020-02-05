ecb68d75b7c25507b805bcaba883f774efc84e58
Paul Hein <pauldhein@email.arizona.edu>
2020-02-05 13:34:25 -0700

    Attempting a scala project setup

--- scripts/py4j/caller.py
+++ scripts/py4j/caller.py
@@ -1,4 +1,5 @@
 from py4j.java_gateway import JavaGateway
+import json
 
 gateway = JavaGateway()                   # connect to the JVM
 random = gateway.jvm.java.util.Random()   # create a java.util.Random instance
@@ -7,5 +8,6 @@ number2 = random.nextInt(10)
 print(number1, number2)
 
 addition_app = gateway.entry_point               # get the AdditionApplication instance
-value = addition_app.addition(number1, number2)
+# value = addition_app.addition(number1, number2)
+value = addition_app.translateJson(json.dumps({"letter": "b", "number": 3.14, "bool": True}))
 print(value)
--- scripts/py4j/py4jserver/src/main/scala/Main.scala
+++ scripts/py4j/py4jserver/src/main/scala/Main.scala
@@ -13,12 +13,10 @@ class AdditionApplication {
 }
 
 
-object Py4JServer {
-  def main(args: Array[String]): Unit = {
-    val app = new AdditionApplication();
-    // app is now the gateway.entry_point
-    val server = new GatewayServer(app);
-    println("Starting the server");
-    server.start();
-  }
+object Py4JServer extends App {
+  val app = new AdditionApplication();
+  // app is now the gateway.entry_point
+  val server = new GatewayServer(app);
+  println("Starting the server");
+  server.start();
 }
