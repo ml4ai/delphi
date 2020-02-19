91fad7b7d051d03cade398d7a4f853c0cfc890a1
skhan <souratosh.khan86@gmail.com>
2020-02-19 11:58:25 -0700

    fast, rbd_fast methods added to SensitivityAnalyzer; methods to save dict object in json,csv, pickle files and reading from such files implemented

--- delphi/GrFN/sensitivity.py
+++ delphi/GrFN/sensitivity.py
@@ -1,11 +1,11 @@
 import time
-
-from SALib.sample import saltelli, fast_sampler, latin
 import SALib as SAL
-from SALib.analyze import sobol, fast, rbd_fast
 import numpy as np
 import torch
-
+import pandas as pd
+import json
+import csv
+import pickle
 from delphi.GrFN.networks import ComputationalGraph
 from delphi.GrFN.utils import timeit
 
@@ -17,6 +17,14 @@ class InputError(Exception):
 # TODO khan: add documentation to this class (this should help you ensure you
 # understand the code)
 class SensitivityIndices(object):
+    """ This class creates an object with first and second order sensitivity
+    indices as well as the total sensitivty index for a given sample size. It
+    also contains the confidence interval associated with the computation of
+    each index. The indices are in the form of a dictionary and they can be saved
+    to or read from a JSON, pickle and csv file. In addition, the maximum and
+    minimum second order indices between any two input variables can be
+    determined using the max (min) and argmax (argmin) methods.
+    """
     def __init__(self, S: dict):
         """
         Args:
@@ -45,7 +53,11 @@ class SensitivityIndices(object):
     def from_csv(cls, filepath: str):
         # TODO khan: implement this so that Si_dict is a sensitivity index
         # dictionary loaded from the CSV file provided by filepath
-        Si_dict = None
+        
+        with open(filepath, 'r') as fin:
+            reader = csv.reader(fin)
+            Si_dict = {row[0]:row[1] for row in reader}
+
         return cls(Si_dict)
 
     @classmethod
@@ -57,14 +69,23 @@ class SensitivityIndices(object):
     def from_json(cls, filepath: str):
         # TODO khan: implement this so that Si_dict is a sensitivity index
         # dictionary loaded from the JSON file provided by filepath
-        Si_dict = None
+
+        with open(filename, encoding='utf-8').read() as data:
+            js = json.loads(data)
+
+        Si_dict = {'S1':float(js['S1']), 'S2':float(js['S2']), 'ST':float(js['ST']), 'S1_conf':float(js['S1_conf']), 'S2_conf':float(js['S2_conf']), 'ST_conf':float(js['ST_conf'])}
+
+
         return cls(Si_dict)
 
     @classmethod
     def from_pickle(cls, filepath: str):
         # TODO khan: implement this so that Si_dict is a sensitivity index
         # dictionary loaded from the PKL file provided by filepath
-        Si_dict = None
+        
+        with open(filename, 'rb') as fin:
+            Si_dict = pickle.load(fin)
+
         return cls(Si_dict)
 
     def get_min_S2(self):
@@ -91,15 +112,40 @@ class SensitivityIndices(object):
 
     def to_csv(self, filepath: str):
         # TODO khan: Save the data in this class to a CSV file
-        return NotImplemented
+
+        column_names = ['S1', 'S2', 'ST', 'S1_conf', 'S2_conf', 'ST_conf']
+
+        try:
+            with open(filepath, 'w') as fout:
+                writer = csv.DictWriter(fout, fieldnames= column_names)
+                writer.writeheader()
+                for data in self.S:
+                    writer.writerow(data)
+        except IOError:
+            print("Input File Missing!")
+
 
     def to_json(self, filepath: str):
         # TODO khan: Save the data in this class to a JSON file
-        return NotImplemented
-
+        
+        try:
+            with open(filepath, 'w') as fout:
+                json.dump(self.S, fout())
+        except IOError:
+            print("Input File Missing!")
+        
+        
     def to_pickle(self, filepath: str):
         # TODO khan: Save the data in this class to a PKL file
-        return NotImplemented
+
+        try:
+            fout = open(filepath, 'wb')
+            pickle.dump(self.S, fout)
+            fout.close()
+        except IOError:
+            print("Input File Missing!")
+
+        
 
 
 class SensitivityAnalyzer(object):
@@ -197,15 +243,21 @@ class SensitivityAnalyzer(object):
         save_time: bool=False, verbose: bool=False
     ) -> dict:
         # TODO Khan: adapt this method to be the same style as Si_from_Sobol
-        print("Sampling via FAST sampler...")
-        samples = fast_sampler.sample(prob_def, num_samples, M=M)
-        samples = np.split(samples, samples.shape[1], axis=1)
-        samples = [s.squeeze() for s in samples]
-        values = {n: torch.tensor(s) for n, s in zip(prob_def["names"], samples)}
-        print("Running GrFN...")
-        Y = network.run(values).numpy()
-        print("Analyzing via FAST...")
-        return fast.analyze(prob_def, Y, M=M)
+       
+        prob_def = cls.setup_problem_def(G.inputs, B)
+
+        (samples, sample_time) = cls.__run_sampling(SAL.sample.fast_sampler.sample,
+                prob_def, N, M=M, seed=seed)
+
+        (Y, exec_time) = cls.__excecute_CG(G, samples, prob_def, C, V)
+
+        (S, analyze_time) = cls.__run_analysis(SAL.analyze.fast.analyze, prob_def, Y, M=M,
+                print_to_console=Flase, seed=seed)
+
+       Si = SensitivityIndices(S)
+       return Si if not save_time \
+               else (Si, (sample_time, exec_time, analyze_time))
+
 
     @classmethod
     def Si_from_RBD_FAST(
@@ -214,127 +266,19 @@ class SensitivityAnalyzer(object):
         save_time: bool=False, verbose: bool=False
     ):
         # TODO Khan: adapt this method to be the same style as Si_from_Sobol
-        print("Sampling via RBD-FAST...")
-        samples = latin.sample(prob_def, num_samples)
-        X = samples
-        samples = np.split(samples, samples.shape[1], axis=1)
-        samples = [s.squeeze() for s in samples]
-        values = {n: torch.tensor(s) for n, s in zip(prob_def["names"], samples)}
-        print("Running GrFN..")
-        Y = network.run(values).numpy()
-        print("Analyzing via RBD ...")
-        return rbd_fast.analyze(prob_def, Y, X, M=M)
+        
+        prob_def = cls.setup_problem_def(G.inputs, B)
+
+        (samples, sample_time) = cls.__run_sampling(SAL.sample.latin.sample,
+                prob_def, N, seed=seed)
+
+        (Y, exec_time) = cls.__execute_CG(G, samples, prob_def, C, V)
+
+        (S, analyze_time) = cls.__run_analysis(SAL.analyze.rbd_fast.analyze,
+                X=samples, Y, M=M, print_to_console=False, seed=seed)
 
 
-# class SobolIndex(object):
-#     """ This class computes sobol indices from the SALib library
-#
-#     Attributes:
-#         model (str): Name of Model (Upper Case)
-#         file_bounds (str): Name of csv file containing Upper and Lower Bounds
-#         of Variables
-#         sample_size (list): List of sample sizes required for sobol index
-#         computation
-#     """
-#
-#     def __init__(self, model, file_bounds, sample_size):
-#
-#         self.model = model
-#         self.file_bounds = file_bounds
-#         self.sample_size = sample_size
-#
-#     def sobol_index_from_GrFN(self, GrFN):
-#
-#         """
-#             Args:
-#                 GrFN : GroundedFunctionNetwork
-#
-#             Returns:
-#                     Dictionary with sample sizes, first and second order sobol
-#                     indices for a particular model
-#         """
-#
-#         sys.path.insert(0, "../../../tests/data/program_analysis")
-#
-#         if min(self.sample_size) < 1:
-#             raise InputError("Sample Size too small --- Incorrect Sobol Indices Generated -- Min. sample size > 10E+4")
-#
-#         if self.model == 'PETPT':
-#             fortran_code = 'PETPT.for'
-#         elif self.model == 'PETASCE':
-#             fortran_code = 'PETASCE_simple.for'
-#         else:
-#             raise InputError("Model Name Invalid!")
-#
-#         tG = GrFN.from_fortran_file("../../../tests/data/program_analysis/" + fortran_code)
-#
-#         args = tG.inputs
-#         var_names = [args[i].split('::')[4] for i in range(len(args))]
-#
-#         var_df = pd.read_csv(self.file_bounds, sep=',', header=0)
-#         var_dict_lb = pd.Series(var_df.Lower.values, index=var_df.Var).to_dict()
-#         var_dict_ub = pd.Series(var_df.Upper.values, index=var_df.Var).to_dict()
-#
-#         var_bounds = dict()
-#         type_dict  = dict()
-#         for var_name in var_names:
-#
-#             if self.model == 'PETPT':
-#                 key = self.model + "::@global::" + self.model.lower() + "::0::" + var_name + "::-1"
-#             else:
-#                 key = self.model + "_simple::@global::" + self.model.lower() + "::0::" + var_name + "::-1"
-#
-#             val = [var_dict_lb[var_name], var_dict_ub[var_name]]
-#
-#             var_bounds.update({key:val})
-#
-#             if var_name != 'meevp' and var_name != 'doy':
-#                 type_val = (float, [0.0])
-#             elif var_name == 'meevp':
-#                 type_val = (str, ["A", "G"])
-#             elif  var_name == 'doy':
-#                 type_val = (int, list(range(1, 366)))
-#
-#             type_dict.update({key:type_val})
-#
-#
-#
-#         problem = {
-#                 'num_vars': len(args),
-#                 'names': args,
-#                 'bounds': [var_bounds[arg] for arg in args]
-#                 }
-#
-#         Ns = self.sample_size
-#
-#         sobol_indices_lst = []
-#
-#         for i in range(len(Ns)):
-#
-#             Si, sample_time, analysis_time = tG.sobol_analysis(Ns[i], problem, var_types=type_dict)
-#             S1_dict = dict(zip(var_names,Si["S1"].tolist()))
-#
-#             for k in range(Si["S2"].shape[0]):
-#                 for l in range(k,Si["S2"].shape[1]):
-#                     if k != l:
-#                         Si["S2"][l][k] = Si["S2"][k][l]
-#
-#             Si["S2"] = np.nan_to_num(Si["S2"]).tolist()
-#
-#             S2_dataframe = pd.DataFrame(data = Si["S2"], columns = var_names).to_json()
-#
-#             sobol_dict = {"sample size": np.log10(Ns[i]), "First Order": S1_dict, "Second Order (DataFrame)": S2_dataframe, "Sample Time": sample_time, "Analysis Time": analysis_time}
-#
-#             sobol_indices_lst.append(sobol_dict)
-#
-#         return sobol_indices_lst
-#
-#     def generate_json(self):
-#
-#         """
-#             Returns:
-#                     JSON file with sample sizes and sobol indices
-#         """
-#
-#         with open("sobol_indices_" + self.model + ".json", "w") as fout:
-#             json.dump(self.sobol_index_from_GrFN(), fout)
+       Si = SensitivityIndices(S)
+       return Si if not save_time \
+               else (Si, (sample_time, exec_time, analyze_time))
+
