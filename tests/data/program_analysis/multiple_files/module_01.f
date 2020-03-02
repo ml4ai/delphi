2ecae987b17ebc2eb58cc9e2f9f58712bac6720b
Saumya K. Debray <saumya.debray@gmail.com>
2020-03-02 14:30:01 -0700

    removed two old directories containing no-longer-used tests

--- tests/data/program_analysis/multiple_files/module_01.f
+++
@@ -1,4 +0,0 @@
-        module module_abc
-            implicit none
-            real :: symbol_1 = 123.456, symbol_2 = 654.321
-        end module module_abc
--- tests/data/program_analysis/multiple_files/module_03.f
+++
@@ -1,5 +0,0 @@
-        module module_ghi
-            implicit none
-            real :: symbol_5 = 1230.124, symbol_6 = 8932.44
-            integer :: symbol_7 = 381293, symbol_8 = 213123 
-        end module module_ghi
