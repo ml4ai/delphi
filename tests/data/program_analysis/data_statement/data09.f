2f08ffa7b36918e82d54cda73eed691a2f4a86cb
Pratik Bhandari <pratik.bhd@gmail.com>
2020-03-03 16:37:55 -0700

    new and modified test files

--- tests/data/program_analysis/data_statement/data09.f
+++ tests/data/program_analysis/data_statement/data09.f
@@ -17,23 +17,27 @@
 *                   DEC
 **********************************************************************
 
-      PROGRAM MAIN
-
-      IMPLICIT NONE
-
-!      CHARACTER*3 MonthTxt(12)
-      CHARACTER (LEN=*) MonthTxt(12)
-!      CHARACTER*3, DIMENSION(12) :: MonthTxt
-      DATA MonthTxt /'JAN','FEB','MAR','APR','MAY','JUN',
-     &               'JUL','AUG','SEP','OCT','NOV','DEC'/
+	  REAL FUNCTION PRINTMONTHS(MONTHARR, S)
 
       INTEGER I
-
-      WRITE (*,12) 'MonthTxt: '
+      REAL S
+      CHARACTER (LEN=*) MONTHARR(12)
+      WRITE (*,12) 'MONTHARR: '
       DO I = 1, 12
-          WRITE (*,12) MonthTxt(I)
+          WRITE (*,12) MONTHARR(I)
       END DO
-
+      PRINTMONTHS = 5.0
  12   FORMAT(A)
+	  RETURN
+	  END FUNCTION PRINTMONTHS
 
+
+      PROGRAM MAIN
+      IMPLICIT NONE
+      CHARACTER*3 MonthTxt(12)
+      DATA MonthTxt /'JAN','FEB','MAR','APR','MAY','JUN',
+     &               'JUL','AUG','SEP','OCT','NOV','DEC'/
+      REAL R, X, PRINTMONTHS
+      R = 5.0
+      X = PRINTMONTHS(MonthTxt, R)
       END PROGRAM MAIN
