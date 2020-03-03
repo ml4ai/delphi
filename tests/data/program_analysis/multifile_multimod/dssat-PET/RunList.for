1160be46e31783adce2b14568d549ea6e4711c22
Saumya Debray <saumya.debray@gmail.com>
2020-03-03 10:17:28 -0700

    Misc bug fixes (#454)
    * Fortran tweaks to ModuleDefs.for to get around OFP problem
    
    * removed two old directories containing no-longer-used tests
    
    * workaround for OFP problem with DATA statement
    
    * workaround for OFP problem with DATA statement

--- tests/data/program_analysis/multifile_multimod/dssat-PET/RunList.for
+++ tests/data/program_analysis/multifile_multimod/dssat-PET/RunList.for
@@ -19,7 +19,7 @@
       CHARACTER*1  RNMODE
       CHARACTER*12 FILEX, FILEX_LAST
 
-      INTEGER DYNAMIC, EXPNO, LUN, NYRS, RLUN, RUN, TRTNUM
+      INTEGER DYNAMIC, EXPNO, LUN, NYRS, RLUN, RUN, TRTNUM, init_first
 
       TYPE (ControlType) CONTROL
       TYPE (SwitchType)  ISWITCH	
@@ -35,7 +35,12 @@
       TRTNUM  = CONTROL % TRTNUM
 
 C     DATA FIRST /.TRUE./
-      FIRST = .TRUE.
+C Some gymnastics involving the DATA statement to work around OFP bug      
+      DATA init_first /0/
+      if (init_first .eq. 0) then
+         init_first = 1
+         FIRST = .TRUE.
+      endif
 !-----------------------------------------------------------------------
       IF (FIRST) THEN
         CALL GETLUN('RUNLST',RLUN)
