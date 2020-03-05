69e55ee83a599e455b26e3ef641c8d19c86d440d
Pratik Bhandari <pratik.bhd@gmail.com>
2020-03-05 12:39:47 -0700

    fileio test files

--- tests/data/program_analysis/io-tests/iotest_08.for
+++ tests/data/program_analysis/io-tests/iotest_08.for
@@ -1,31 +1,33 @@
 C Read a single integer from a file, then write it out to another file.
 C The expected output written to file 'outfile1' is the line:
 C 12345
-      SUBROUTINE ERROR (ERRKEY)
 
-      INTEGER ERRKEY
-      WRITE(*,20) 'ERRKEY: ', ERRKEY
-
- 20   FORMAT(A, I1)
-      END SUBROUTINE ERROR
+C      SUBROUTINE ERROR (ERRKEY)
+C
+C      INTEGER ERRKEY
+C      WRITE(*,20) 'ERRKEY: ', ERRKEY
+C
+C 20   FORMAT(A, I1)
+C      END SUBROUTINE ERROR
 
       PROGRAM MAIN
 
       INTEGER LUNIO, ERR
-      CHARACTER*7 FILEIO
+      CHARACTER*8 FILEIO
 
       LUNIO = 10
-      FILEIO = "infile1c"
+      FILEIO = "testfile"
 
       OPEN (LUNIO, FILE=FILEIO, STATUS = 'OLD',IOSTAT=ERR)
-      IF (ERR .NE. 0) CALL ERROR(5)
+C      OPEN (LUNIO, FILE=FILEIO, STATUS = "OLD")
+C      IF (ERR .NE. 0) CALL ERROR(5)
 
-      OPEN (20, FILE="outfile1", STATUS="REPLACE")
+      OPEN (20, FILE="testout", STATUS="REPLACE")
 
       READ (LUNIO,10) I
       WRITE (20,10) I
 
  10   FORMAT (I5)
-      CLOSE (LUNIO)
+C      CLOSE (LUNIO)
       STOP
       END PROGRAM MAIN
