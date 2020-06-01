7887ece655c25ce3ba29700bd9287844ec1738aa
Manujinda Wathugala <dulan.wathugala@gmail.com>
2020-06-01 16:15:33 -0700

    Fixed the scrolling problem. Have to clean and organize the code

--- scripts/data_processing/web_scraping/scrape_Data_Planet_indicators.py
+++ scripts/data_processing/web_scraping/scrape_Data_Planet_indicators.py
@@ -7,6 +7,22 @@ import pandas as pd
 import pickle
 
 
+def scroll_to_element(browser, parent_id, element_id, topPos):
+    if topPos == -1:
+        java_script_call =\
+            f"var myElement = document.getElementById('{element_id}');\
+            var topPos = myElement.offsetTop;\
+            document.getElementById('{parent_id}').scrollTop = topPos - 55;\
+            console.log(topPos);\
+            console.log(myElement.innerText);"
+    else:
+        java_script_call =\
+            f"document.getElementById('{parent_id}').scrollTop = {topPos} - 55;\
+            console.log({topPos});"
+    #print(java_script_call)
+    browser.execute_script(java_script_call)
+
+
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
@@ -44,42 +60,64 @@ with open('DP_branches.data', 'rb') as filehandle:
 '''
 branches = []
 
-num_lines = 200
 box_idx = 0
-last_success_idx = 0
 continuous_unsuccess = 0
 
 while True:
-#for box_idx in range(1, 20000):
-#for box_idx in branches:
     box_idx += 1
+    if box_idx > 20000:
+        exit()
+        break
+
+    table = driver.find_element_by_id('isc_1Ctable')
+    rows = table.find_elements_by_css_selector('#isc_1Ctable > tbody > tr')
+    num_lines = len(rows)
+
     try:
         tree_open_boxes = driver.find_element_by_id(f'isc_19open_icon_{box_idx}')
         tree_open_boxes.click()
         print(box_idx, 'GOOD')
-        last_success_idx = box_idx
         continuous_unsuccess = 0
         branches.append(box_idx)
         time.sleep(1)
-        #tree_pane.send_keys(Keys.END)
-        tree_pane.send_keys(Keys.PAGE_DOWN)
-        #time.sleep(5)
+
+        table = driver.find_element_by_id('isc_1Ctable')
+        rows = table.find_elements_by_css_selector('#isc_1Ctable > tbody > tr')
+
+        for row in rows:
+            line_struct = row.find_elements_by_css_selector('td > div > table > tbody > tr > td')
+            last_ID = line_struct[2].get_attribute('id')
+            expanded_box_idx = int(last_ID.split('Cell')[1])
+            if expanded_box_idx == box_idx:
+                driver.execute_script(f"document.getElementById('isc_TreeViewImpl_1_0_body_cell{expanded_box_idx}_0').style.position = 'relative';")
+                indicator = line_struct[2].get_attribute('innerText')
+                offset_top = int(driver.find_element_by_id(f'isc_TreeViewImpl_1_0_body_cell{box_idx}_0').get_attribute('offsetTop'))
+                print('Expanded Row: ', box_idx, last_ID, indicator, offset_top)
+                break
+
+        scroll_to_element(driver, 'isc_1C', f'isc_TreeViewImpl_1_0_body_cell{box_idx}_0', offset_top)
+        time.sleep(1)
     except common.exceptions.NoSuchElementException:
-        #print('common.exceptions.NoSuchElementException')
         continuous_unsuccess += 1
         if continuous_unsuccess > num_lines:
-            #box_idx = last_success_idx + num_lines - 1
-            box_idx = last_success_idx + 1
+            last_visible_line_struct = rows[-3].find_elements_by_css_selector('td > div > table > tbody > tr > td')
+            last_ID = last_visible_line_struct[2].get_attribute('id')
+            box_idx = int(last_ID.split('Cell')[1])
+            driver.execute_script(f"document.getElementById('isc_TreeViewImpl_1_0_body_cell{box_idx}_0').style.position = 'relative';")
+            indicator = last_visible_line_struct[2].get_attribute('innerText')
+            offset_top = int(rows[-3].find_element_by_id(f'isc_TreeViewImpl_1_0_body_cell{box_idx}_0').get_attribute('offsetTop'))
+            print(box_idx, last_ID, indicator, offset_top)
             continuous_unsuccess = 0
-            tree_pane.send_keys(Keys.PAGE_DOWN)
-        #pass
+            scroll_to_element(driver, 'isc_1C', f'isc_TreeViewImpl_1_0_body_cell{box_idx}_0', offset_top)
+            time.sleep(1)
+        pass
     except common.exceptions.ElementNotInteractableException:
-        #print('common.exceptions.ElementNotInteractableException')
         pass
     except common.exceptions.StaleElementReferenceException:
         print('Done')
         break
 
+exit()
 
 with open('DP_branches.data', 'wb') as filehandle:
     # store the data as binary data stream
