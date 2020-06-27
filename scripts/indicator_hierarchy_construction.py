b0c712794473760fc2a4968837477ba8e9ce1111
Aishwarya Radhakrishnan <aishwarya.rk347@gmail.com>
2020-06-27 00:12:13 -0700

    FAOSTAT script change

--- scripts/indicator_hierarchy_construction.py
+++ scripts/indicator_hierarchy_construction.py
@@ -25,18 +25,18 @@ def clean_FAOSTAT_data(outputFile):
     open(outputFile, 'w').close()
 
     faostat_dir = "FAOSTAT"
-    domain_name = ['ASTI', 'CommodityBalances', 'ConsumerPriceIndices', 'Deflators', 'Development', 'Emissions_Agriculture', 'Emissions_Land_Use', 'Employment_Indicators', 'Environment', 'Exchange_rate', 'Food_Aid_Shipments', 'FoodBalance', 'Food_Security', 'FoodSupply', 'Forestry', 'Household_Surveys', 'Inputs', 'Investment', 'Macro-Statistics', 'Population', 'Price', 'Production', 'Trade']
-    domain_name_yaml = ['ASTI R&D Indicators', 'Food Balance', 'Prices', 'Prices',  'Investment', 'Emissions - Agriculture', 'Emissions - Land Use', 'Inputs', 'Agri-Environmental Indicators', 'Prices', 'Emergency Response', 'Food Balance', 'Food Security', 'Food Security', 'Forestry', 'Food Security', 'Inputs', 'Investment', 'Macro-Statistics', 'Population', 'Prices', 'Production', 'Trade']
+    domain_name = ['gender, area, socioeconomics', 'CapitalStock', 'ASTI', 'CommodityBalances', 'ConsumerPriceIndices', 'Deflators', 'Development', 'Emissions_Agriculture', 'Emissions_Land_Use', 'Employment_Indicators', 'Environment', 'Exchange_rate', 'Food_Aid_Shipments', 'FoodBalance', 'Food_Security', 'FoodSupply', 'Forestry', 'Household_Surveys', 'Inputs', 'Investment', 'Macro-Statistics', 'Population', 'Price', 'Production', 'Trade']
+    domain_name_yaml = ['Food Security', 'Macro-Statistics', 'ASTI R&D Indicators', 'Food Balance', 'Prices', 'Prices',  'Investment', 'Emissions - Agriculture', 'Emissions - Land Use', 'Inputs', 'Agri-Environmental Indicators', 'Prices', 'Emergency Response', 'Food Balance', 'Food Security', 'Food Balance', 'Forestry', 'Food Security', 'Inputs', 'Investment', 'Macro-Statistics', 'Population', 'Prices', 'Production', 'Trade']
     
-    sub_domain = ['ASTI_Expenditures_E_All_Data_',  'ASTI_Researchers_E_All_Data_',  'CommodityBalances_Crops_E_All_Data_', 'CommodityBalances_LivestockFish_E_All_Data_',  'ConsumerPriceIndices_E_All_Data_', 'Deflators_E_All_Data_', 'Development_Assistance_to_Agriculture_E_All_Data_', 'Emissions_Agriculture_Agriculture_total_E_All_Data_', 'Emissions_Agriculture_Burning_crop_residues_E_All_Data_', 'Emissions_Agriculture_Burning_Savanna_E_All_Data_', 'Emissions_Agriculture_Crop_Residues_E_All_Data_', 'Emissions_Agriculture_Cultivated_Organic_Soils_E_All_Data_', 'Emissions_Agriculture_Energy_E_All_Data_', 
-    'Emissions_Agriculture_Enteric_Fermentation_E_All_Data_', 'Emissions_Agriculture_Manure_applied_to_soils_E_All_Data_', 'Emissions_Agriculture_Manure_left_on_pasture_E_All_Data_', 'Emissions_Agriculture_Manure_Management_E_All_Data_', 'Emissions_Agriculture_Rice_Cultivation_E_All_Data_', 'Emissions_Agriculture_Synthetic_Fertilizers_E_All_Data_', 'Emissions_Land_Use_Burning_Biomass_E_All_Data_', 'Emissions_Land_Use_Cropland_E_All_Data_', 'Emissions_Land_Use_Forest_Land_E_All_Data_', 'Emissions_Land_Use_Grassland_E_All_Data_', 'Emissions_Land_Use_Land_Use_Total_E_All_Data_', 'Employment_Indicators_E_All_Data_', 'Environment_Emissions_by_Sector_E_All_Data_', 'Environment_Emissions_intensities_E_All_Data_', 'Environment_Fertilizers_E_All_Data_',  'Environment_LandCover_E_All_Data_', 'Environment_LandUse_E_All_Data_', 
-    'Environment_LivestockManure_E_All_Data_', 'Environment_LivestockPatterns_E_All_Data_',  'Environment_Pesticides_E_All_Data_', 'Environment_Temperature_change_E_All_Data_', 'Exchange_rate_E_All_Data_', 'Food_Aid_Shipments_WFP_E_All_Data_',  'FoodBalanceSheets_E_All_Data_', 'FoodBalanceSheetsHistoric_E_All_Data_', 'Food_Security_Data_E_All_Data_', 'FoodSupply_Crops_E_All_Data_',  'FoodSupply_LivestockFish_E_All_Data_', 'Forestry_E_All_Data_', 'Forestry_Trade_Flows_E_All_Data_', 'Indicators_from_Household_Surveys_E_All_Data_', 'Inputs_FertilizersArchive_E_All_Data_', 'Inputs_FertilizersNutrient_E_All_Data_', 'Inputs_FertilizersProduct_E_All_Data_', 'Inputs_LandUse_E_All_Data_', 'Inputs_Pesticides_Trade_E_All_Data_', 'Inputs_Pesticides_Use_E_All_Data_', 'Investment_CapitalStock_E_All_Data_', 'Investment_CountryInvestmentStatisticsProfile__E_All_Data_', 
-    'Investment_CreditAgriculture_E_All_Data_', 'Investment_ForeignDirectInvestment_E_All_Data_', 'Investment_GovernmentExpenditure_E_All_Data_', 'Investment_MachineryArchive_E_All_Data_', 'Investment_Machinery_E_All_Data_', 'Macro-Statistics_Key_Indicators_E_All_Data_', 'Population_E_All_Data_', 'Price_Indices_E_All_Data_', 'PricesArchive_E_All_Data', 'Prices_E_All_Data_', 'Prices_Monthly_E_All_Data_', 'Production_Crops_E_All_Data_', 'Production_CropsProcessed_E_All_Data_', 'Production_Indices_E_All_Data_', 'Production_Livestock_E_All_Data_', 'Production_LivestockPrimary_E_All_Data_', 'Production_LivestockProcessed_E_All_Data_', 'Trade_Crops_Livestock_E_All_Data_', 'Trade_DetailedTradeMatrix_E_All_Data_', 'Trade_Indices_E_All_Data_', 'Trade_LiveAnimals_E_All_Data_', 'Value_of_Production_E_All_Data_' ]
+    sub_domain = ['ASTI_Expenditures',  'ASTI_Researchers',  'CommodityBalances_Crops', 'CommodityBalances_LivestockFish',  'ConsumerPriceIndices', 'Deflators', 'Development_Assistance_to_Agriculture', 'Emissions_Agriculture_Agriculture_total', 'Emissions_Agriculture_Burning_crop_residues', 'Emissions_Agriculture_Burning_Savanna', 'Emissions_Agriculture_Crop_Residues', 'Emissions_Agriculture_Cultivated_Organic_Soils', 'Emissions_Agriculture_Energy', 
+    'Emissions_Agriculture_Enteric_Fermentation', 'Emissions_Agriculture_Manure_applied_to_soils', 'Emissions_Agriculture_Manure_left_on_pasture', 'Emissions_Agriculture_Manure_Management', 'Emissions_Agriculture_Rice_Cultivation', 'Emissions_Agriculture_Synthetic_Fertilizers', 'Emissions_Land_Use_Burning_Biomass', 'Emissions_Land_Use_Cropland', 'Emissions_Land_Use_Forest_Land', 'Emissions_Land_Use_Grassland', 'Emissions_Land_Use_Land_Use_Total', 'Employment_Indicators', 'Environment_Emissions_by_Sector', 'Environment_Emissions_intensities', 'Environment_Fertilizers',  'Environment_LandCover', 'Environment_LandUse', 
+    'Environment_LivestockManure', 'Environment_LivestockPatterns',  'Environment_Pesticides', 'Environment_Temperature_change', 'Exchange_rate', 'Food_Aid_Shipments_WFP',  'FoodBalanceSheets', 'FoodBalanceSheetsHistoric', 'Food_Security_Data', 'FoodSupply_Crops',  'FoodSupply_LivestockFish', 'Forestry_E', 'Forestry_Trade_Flows', 'Indicators_from_Household_Surveys', 'Inputs_FertilizersArchive', 'Inputs_FertilizersNutrient', 'Inputs_FertilizersProduct', 'Inputs_LandUse', 'Inputs_Pesticides_Trade', 'Inputs_Pesticides_Use', 'Investment_CapitalStock', 'Investment_CountryInvestmentStatisticsProfile', 
+    'Investment_CreditAgriculture', 'Investment_ForeignDirectInvestment', 'Investment_GovernmentExpenditure', 'Investment_MachineryArchive', 'Investment_Machinery', 'Macro-Statistics_Key_Indicators', 'Population_E', 'Price_Indices', 'PricesArchive', 'Prices_E_All_Data', 'Prices_Monthly', 'Production_Crops', 'Production_CropsProcessed', 'Production_Indices', 'Production_Livestock', 'Production_LivestockPrimary', 'Production_LivestockProcessed', 'Trade_Crops_Livestock', 'Trade_DetailedTradeMatrix', 'Trade_Indices', 'Trade_LiveAnimals', 'Value_of_Production' ]
     
     sub_domain_yaml = ['ASTI-Expenditures', 'ASTI-Researchers', 'Commodity Balances - Crops Primary Equivalent',  'Commodity Balances - Livestock and Fish Primary Equivalent', 'Consumer Price Indices', 'Deflators', 'Development Flows to Agriculture', 'Agriculture Total', 'Burning - Crop Residues', 'Burning - Savanna', 'Crop Residues', 'Cultivation of Organic Soils', 'Energy Use', 
     'Enteric Fermentation', 'Manure applied to Soils', 'Manure left on Pasture', 'Manure Management', 'Rice Cultivation', 'Synthetic Fertilizers', 'Burning - Biomass', 'Cropland', 'Forest Land', 'Grassland', 'Land Use Total', 'Employment Indicators', 'Emissions shares', 'Emissions intensities', 'Fertilizers indicators', 'Land Cover', 'Land use indicators', 
     'Livestock Manure', 'Livestock Patterns', 'Pesticides indicators', 'Temperature change', 'Exchange rates - Annual', 'Food Aid Shipments (WFP)', 'New Food Balances', 'Food Balances (old methodology and population)', 'Suite of Food Security Indicators', 'Food Supply - Crops Primary Equivalent', 'Food Supply - Livestock and Fish Primary Equivalent', 'Forestry Production and Trade', 'Forestry Trade Flows', 'Indicators from Household Surveys (gender, area, socioeconomics)', 'Fertilizers archive', 'Fertilizers by Nutrient', 'Fertilizers by Product', 'Land Use', 'Pesticides Trade', 'Pesticides Use', 'Capital Stock', 'Country Investment Statistics Profile', 
-    'Credit to Agriculture', 'Foreign Direct Investment (FDI)', 'Government Expenditure', 'Machinery Archive', 'Machinery', 'Macro Indicators', 'Annual population', 'Producer Price Indices - Annual', 'Producer Prices - Archive', 'Producer Prices - Annual', 'Producer Prices - Monthly', 'Crops', 'Crops processed', 'Production Indices', 'Live Animals', 'Livestock Primary', 'Livestock Processed', 'Crops and livestock products', 'Detailed trade matrix', 'Trade Indices', 'Live animals', 'Value of Agricultural Production']
+    'Credit to Agriculture', 'Foreign Direct Investment (FDI)', 'Government Expenditure', 'Machinery Archive', 'Machinery', 'Macro Indicators', 'Annual population', 'Producer Price Indices - Annual', 'Producer Prices (old series)', 'Producer Prices', 'Producer Prices - Monthly', 'Crops', 'Crops processed', 'Production Indices', 'Live Animals', 'Livestock Primary', 'Livestock Processed', 'Crops and livestock products', 'Detailed trade matrix', 'Trade Indices', 'Live animals', 'Value of Agricultural Production']
 
     required_cols = ("Element", "Purpose", "FAO Source", "Breakdown Variable", "Indicator", "Item", "Measure", "Note")
 
@@ -47,7 +47,7 @@ def clean_FAOSTAT_data(outputFile):
     ):
         print(filename)
 
-        if 'Flags' in filename: continue
+        if 'E_Flags' in filename: continue
 
         df = pd.read_csv(
             filename,
@@ -58,7 +58,8 @@ def clean_FAOSTAT_data(outputFile):
         pointer2ndlayer = dict_file
         for dn in range(len(domain_name)):
             if domain_name[dn] in filename:
-                pointer2ndlayer[domain_name_yaml[dn]] = {}  
+                if domain_name_yaml[dn] not in pointer2ndlayer:
+                    pointer2ndlayer[domain_name_yaml[dn]] = {}  
                 pointer2ndlayer = pointer2ndlayer[domain_name_yaml[dn]]
                 break
 
@@ -67,8 +68,9 @@ def clean_FAOSTAT_data(outputFile):
         filename_first = filename_first[0].split('/')
         filename_first = filename_first[1]
         for sd in range(len(sub_domain)):
-            if filename_first == sub_domain[sd]:
-                pointer2ndlayer[sub_domain_yaml[sd]] = {}
+            if sub_domain[sd] in filename_first:
+                if sub_domain_yaml[sd] not in pointer2ndlayer:
+                    pointer2ndlayer[sub_domain_yaml[sd]] = {}
                 pointer2ndlayer = pointer2ndlayer[sub_domain_yaml[sd]]
                 break
 
