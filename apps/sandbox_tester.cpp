d23ea5f8b71b032994eb5c3a509a80f7f1290e86
Manujinda Wathugala <manujinda@email.arizona.edu>
2020-09-29 17:31:01 -0700

    Added some test code to guide Aishwarya in testing and debugging the deserialization method she is developing.

--- apps/sandbox_tester.cpp
+++ apps/sandbox_tester.cpp
@@ -9,15 +9,66 @@
 int main(int argc, char* argv[]) {
     using namespace std;
     using namespace boost::program_options;
+    using fmt::print;
 
-    ofstream json_orig;
     AnalysisGraph G = AnalysisGraph::from_causemos_json_file("../tests/data/delphi_create_model_payload.json");
+
+    // Serialize the model
+    string json_compact = G.serialize_to_json_string(false);
+    string json_verbose = G.serialize_to_json_string(true);
+
+    // Deserialize the serialized model
+    AnalysisGraph G_from_compact = AnalysisGraph::deserialize_from_json_string(json_compact, false); 
+
+    // Serialize these models again.
+    string json_compact_2 = G_from_compact.serialize_to_json_string(false);
+
+    // Let's check whether they agree.
+    // NOTE: It seems they do not agree. This needs to be fixed
+    if (json_compact.compare(json_compact_2) == 0) {
+        print("Serialization: compact success\n");
+    } else {
+        print("Serialization: compact failure\n");
+    }
+
+    ofstream fs;
+    fs.open ("json_original_compact.txt");
+    fs << json_compact;
+    fs.close();
+    fs.open ("json_deserialized_compact.txt");
+    fs << json_compact_2;
+    fs.close();
+
+    /*
+    // NOTE: This line generates an error.
+    // If this cannot be debugged quickly we can keep it to later.
+    AnalysisGraph G_from_verbose = AnalysisGraph::deserialize_from_json_string(json_verbose, true); 
+
+    // Serialize these models again.
+    string json_verbose_2 = G_from_verbose.serialize_to_json_string(true);
+
+    if (json_verbose.compare(json_verbose_2) == 0) {
+        print("Serialization: verbose success\n");
+    } else {
+        print("Serialization: verbose failure\n");
+    }
+
+    ofstream fs;
+    fs.open ("json_original_verbose.txt");
+    fs << json_verbose;
+    fs.close();
+    fs.open ("json_deserialized_verbose.txt");
+    fs << json_verbose_2;
+    fs.close();
+    */
+
+    /*
+    ofstream json_orig;
     json_orig.open ("json_original_verbose.txt");
-    string json_verbose = G.serialize_to_json_string();
     json_orig.close();
     json_orig.open ("json_original_compact.txt");
-    string json_compact = G.serialize_to_json_string(false);
     json_orig.close();
+
 
     json_orig.open ("json_deserialized_verbose.txt");
     AnalysisGraph GD1 = AnalysisGraph::deserialize_from_json_file("json_original_verbose.txt"); 
@@ -29,6 +80,7 @@ int main(int argc, char* argv[]) {
     string json_compact1 = GD2.serialize_to_json_string(false);
     json_orig << json_compact1;
     json_orig.close();
+    */
     
 
     return(0);
