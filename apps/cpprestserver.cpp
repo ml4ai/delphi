d35152d24bc466ed63d25c61eec0bcd5dcbef1a5
Aishwarya Radhakrishnan <aishwarya.rk347@gmail.com>
2021-01-22 13:29:13 -0700

    Basic POST call working

--- apps/cpprestserver.cpp
+++ apps/cpprestserver.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <served/served.hpp>
+#include <nlohmann/json.hpp>
+//#include "dbg"
+
 
 int main(int argc, const char *argv[])
 {
+
     served::multiplexer mux;
-    mux.handle("/delphi/create_model")
+    mux.handle("/delphi/create-model")
         .post([](served::response & res, const served::request & req) {
             //res << req.data << endl;
 
-            std::cout << "POST req: " << req.body() << std::endl;
+            auto json_data = nlohmann::json::parse(req.body());
+            //std::cout << "POST req: " << json_data << std::endl;
+            res << json_data.dump();
         });
 
 
@@ -18,7 +24,7 @@ int main(int argc, const char *argv[])
 
 
     std::cout << "Try this example with:" << std::endl;
-    std::cout << "  curl \"http://localhost:8123/delphi/create-model\"" << std::endl;
+    std::cout << "curl -X POST \"http://localhost:8123/delphi/create-model\" -d @test.json --header \"Content-Type: application/json\" " << std::endl;
 
     served::net::server server("127.0.0.1", "8123", mux);
     server.run(10);
