67e878fc7632a23ece9f41e3935b33be3aad2f60
Aishwarya Radhakrishnan <aishwarya.rk347@gmail.com>
2021-05-03 10:20:11 -0700

    Test rest api

--- apps/test_cpp_rest_api.py
+++ apps/test_cpp_rest_api.py
@@ -2,17 +2,9 @@ import os
 import time
 import json
 
-
-#SQLITE3_DB_PATH="${DELPHI_DB}"
-#SELECT * FROM delphimodel where id='XYZ';
-
 SQLITE3_DB_PATH="/Users/aishwaya/Desktop/delphi/delphi_test.db"
 
-
-
-
 def main():
-	#os.system("sqlite3 "+SQLITE3_DB_PATH+" '.mode json'  \"SELECT * FROM delphimodel where id='XYZ'\"")
 
     os.system("curl -X POST \"http://localhost:8123/delphi/create-model\" -d @../tests/data/delphi/causemos_create-model.json --header \"Content-Type: application/json\" ;")
 
@@ -26,7 +18,6 @@ def main():
     status = "in progress"
     while status == "in progress":
     	time.sleep(1)
-    	#result_get = os.system("curl \"http://localhost:8123/delphi/models/XYZ/experiments/d93b18a7-e2a3-4023-9f2f-06652b4bba66\" ")
     	rv = os.popen("curl \"http://localhost:8123/delphi/models/XYZ/experiments/"+experiment_id1+"\" ").read()
     	status = json.loads(rv)["status"]
     	print(status)
