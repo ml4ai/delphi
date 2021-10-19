81820b6e02cb70b0195dc504ba5e75bbedd7ebd3
Adarsh Pyarelal <adarsh@arizona.edu>
2021-10-19 13:40:10 -0700

    adding #pragma once to DatabaseHelper.cpp, autoformatting

--- lib/DatabaseHelper.hpp
+++ lib/DatabaseHelper.hpp
@@ -1,47 +1,52 @@
-#ifndef DATABASEHELPER_H
-#define DATABASEHELPER_H
+#pragma once
 
-
+#include <nlohmann/json.hpp>
+#include <range/v3/all.hpp>
 #include <sqlite3.h>
 #include <vector>
-#include <range/v3/all.hpp>
-#include <nlohmann/json.hpp>
-
 
 class Database {
   private:
-  
-    sqlite3  *db;
-      
+  sqlite3* db;
 
   public:
+  Database();
 
-    Database();
+  ~Database();
 
-    ~Database();
-    
-    void create_table();
+  void create_table();
 
-    std::vector<std::string> read_column_text(std::string query);
+  std::vector<std::string> read_column_text(std::string query);
 
-    std::vector<std::string> read_column_text_query(std::string table_name, std::string column_name);
+  std::vector<std::string> read_column_text_query(std::string table_name,
+                                                  std::string column_name);
 
-    std::vector<std::string> read_column_text_query_where(std::string table_name, std::string column_name, std::string where_column_name, std::string where_value);
+  std::vector<std::string>
+  read_column_text_query_where(std::string table_name,
+                               std::string column_name,
+                               std::string where_column_name,
+                               std::string where_value);
 
-    nlohmann::json select_delphimodel_row(std::string modelId);
+  nlohmann::json select_delphimodel_row(std::string modelId);
 
-    nlohmann::json select_causemosasyncexperimentresult_row(std::string modelId);
+  nlohmann::json select_causemosasyncexperimentresult_row(std::string modelId);
 
-    void insert(std::string insert_query);
+  void insert(std::string insert_query);
 
-    void insert_into_delphimodel(std::string id, std::string model);
+  void insert_into_delphimodel(std::string id, std::string model);
 
-    void insert_into_causemosasyncexperimentresult(std::string id, std::string status, std::string experimentType, std::string results);
+  void insert_into_causemosasyncexperimentresult(std::string id,
+                                                 std::string status,
+                                                 std::string experimentType,
+                                                 std::string results);
 
-    void update_row(std::string table_name, std::string column_name, std::string value, std::string where_column_name, std::string where_value);
+  void update_row(std::string table_name,
+                  std::string column_name,
+                  std::string value,
+                  std::string where_column_name,
+                  std::string where_value);
 
-    void delete_rows(std::string table_name, std::string where_column_name, std::string where_value);
-
+  void delete_rows(std::string table_name,
+                   std::string where_column_name,
+                   std::string where_value);
 };
-
-#endif /* DATABASEHELPER_H */
