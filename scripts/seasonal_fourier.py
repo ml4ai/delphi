230cee1ecd17bde1c4e8e00601b5c0bcc5afc048
Manujinda Wathugala <manujinda@email.arizona.edu>
2021-10-23 17:18:17 -0700

    Started testing with real rainfall data.
    More dedicated plotting:
    1. Moved plotting out of Fourier curve generating functions into a
    separate function.
    2. Created a new function to plot the predictions against binned data
    distributions. For this function to work, predictions should be at bin
    locations.

--- scripts/seasonal_fourier.py
+++ scripts/seasonal_fourier.py
@@ -1,14 +1,80 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 from matplotlib.cm import get_cmap
 import scipy.linalg as la
 
 plt.rcParams['figure.figsize'] = [8, 8]
-plt.rcParams.update({'font.size': 18})
+plt.rcParams.update({'font.size': 12})
 sns.set_style("whitegrid")
 
 np.set_printoptions(precision=3, linewidth=100)  #, formatter={'float': '{: 0.3f}'.format})
+
+
+def plot_predictions_with_data_distributions(LDS_pred, num_pred, df_binned, prediction_step_length, type='violin', title='', file_name=''):
+    name = 'Accent'
+    cmap = get_cmap('tab10')
+    colors = cmap.colors
+
+    fig, ax = plt.subplots(dpi=250, figsize=(12, 6.75))
+    ax.set_prop_cycle(color=colors)
+
+    x_pred_LDS = [f'{x}' for x in np.arange(1, num_pred + 1)]
+
+    if prediction_step_length == 1:
+
+        if type == 'violin':
+            sns.violinplot(data=df_binned, x='Observation Point str', y='Observation')  # , color='skyblue')
+        elif type == 'box':
+            sns.boxplot(data=df_binned, x='Observation Point str', y='Observation')
+
+        sns.swarmplot(data=df_binned, x='Observation Point str', y='Observation', color="k", alpha=0.8, s=3)
+    else:
+        print('***** WARNING: Cannot produce distribution plots for prediction step sizes other than 1 *****')
+        title = '***** WARNING: prediction step sizes $\\neq$ 1 *****\nDistributions Cannot be aligned with Predictions'
+
+    sns.lineplot(x=x_pred_LDS, y=LDS_pred[-2, :], label='LDS value', marker='o', color='r', linewidth=2)
+
+    plt.title(title)
+    plt.tight_layout()
+    plt.legend()
+
+    if file_name:
+        plt.savefig(file_name)
+    else:
+        plt.show()
+
+
+def plot_all(x_2pi, gtf, trig_pred, LDS_pred, num_pred, df_binned, L, num_datapoints, step_length, title='', plot_derivatives=False, file_name=''):
+    name = 'Accent'
+    cmap = get_cmap('tab10')
+    colors = cmap.colors
+
+    fig, ax = plt.subplots(dpi=250, figsize=(12, 6.75))
+    ax.set_prop_cycle(color=colors)
+
+    x_pred_LDS = np.arange(0, num_pred * step_length, step_length) * 2 * L / (num_datapoints) - L
+    # x_2pi += L
+
+    sns.lineplot(x=x_2pi, y=gtf, color='y', linewidth=4, label='Original function')
+    sns.lineplot(x=x_2pi, y=trig_pred, label='Trig function', linewidth=3, color='k')
+
+    sns.lineplot(x=x_pred_LDS, y=LDS_pred[-2, :], label='LDS value', marker='o', color='r', linewidth=2)
+
+    if plot_derivatives:
+        sns.lineplot(x=x_pred_LDS, y=LDS_pred[-1, :], label='LDS derivative', marker='o', color='b', linewidth=0.5)
+        sns.lineplot(x=x_pred_LDS[: -1], y=np.diff(LDS_pred[-2, :]), label='diff( LDS value )', marker='o', color='g', linewidth=0.5)
+
+    plt.title(title)
+    plt.tight_layout()
+    plt.legend()
+
+    if file_name:
+        plt.savefig(file_name)
+    else:
+        plt.show()
+
 
 def discretize_line(y1, y2, spl):
     dx = 1 / (spl)
@@ -129,14 +195,15 @@ def fourier_curve_from_full_blown_LDS(A, s0, num_pred, L, num_datapoints, step_l
     for t in range(1, num_pred):
         curves[:, t] = np.matmul(A, curves[:, t - 1])
 
-    x = np.arange(0, num_pred * step_length, step_length) * 2 * L / (num_datapoints) - L
+    # x = np.arange(0, num_pred * step_length, step_length) * 2 * L / (num_datapoints) - L
+    # sns.lineplot(x=x, y=curves[-2, :], label='LDS value', marker='o', color='r', linewidth=2)
+    # sns.lineplot(x=x, y=curves[-1, :], label='LDS derivative', marker='o', color='b', linewidth=0.5)
+    # sns.lineplot(x=x[: -1], y=np.diff(curves[-2, :]), label='diff', marker='o', color='g', linewidth=0.5)
+    #
+    # plt.legend()
+    # plt.show()
 
-    sns.lineplot(x=x, y=curves[-2, :], label='LDS value', marker='o', color='r', linewidth=2)
-    sns.lineplot(x=x, y=curves[-1, :], label='LDS derivative', marker='o', color='b', linewidth=0.5)
-    sns.lineplot(x=x[: -1], y=np.diff(curves[-2, :]), label='diff', marker='o', color='g', linewidth=0.5)
-
-    plt.legend()
-    plt.show()
+    return curves[np.r_[-2:0], :]
 
 
 # Have to update this similar to generate_sinusoidal_curves() to be used in the current pipeline
@@ -307,7 +374,8 @@ def fourier_curve_from_trig_functions(C0, C, D, x, L):
         # ax.plot(x, sin_nx, '-')
         # ax.plot(x, cos_nx, '-')
 
-    sns.lineplot(x=x, y=fFS, label='Trig function', linewidth=3, color='k')
+    # sns.lineplot(x=x, y=fFS, label='Trig function', linewidth=3, color='k')
+    return fFS
 
 
 def fourier_curve_from_LDS(A, s0, num_pred, L, num_datapoints, step_length):
@@ -316,34 +384,44 @@ def fourier_curve_from_LDS(A, s0, num_pred, L, num_datapoints, step_length):
     for t in range(1, num_pred):
         curves[:, t] = np.matmul(A, curves[:, t - 1])
 
-    x = np.arange(0, num_pred * step_length, step_length) * 2 * L / (num_datapoints) - L
-    sns.lineplot(x=x, y=curves[-2, :], label='LDS value', marker='o', color='r', linewidth=2)
-    sns.lineplot(x=x, y=curves[-1, :], label='LDS derivative', marker='o', color='b', linewidth=0.5)
-    sns.lineplot(x=x[: -1], y=np.diff(curves[-2, :]), label='diff', marker='o', color='g', linewidth=0.5)
+    # x = np.arange(0, num_pred * step_length, step_length) * 2 * L / (num_datapoints) - L
+    # sns.lineplot(x=x, y=curves[-2, :], label='LDS value', marker='o', color='r', linewidth=2)
+    # sns.lineplot(x=x, y=curves[-1, :], label='LDS derivative', marker='o', color='b', linewidth=0.5)
+    # sns.lineplot(x=x[: -1], y=np.diff(curves[-2, :]), label='diff', marker='o', color='g', linewidth=0.5)
+    #
+    # plt.legend()
+    # plt.show()
 
-    plt.legend()
-    plt.show()
-
+    return curves[np.r_[-2:0], :]
 
 
 # vvvvvvvvvvvvvvvvvvvv Estimating Fourier coefficients using least squares vvvvvvvvvvvvvvvvvvvvv
 
 # Partitioning data into bins within a period
-def partition_data_according_to_period(data, timesteps, period=12):
+def partition_data_according_to_period(data, timesteps, period=12, L=np.pi):
     partitions = {}
     means = []
+    observation_point_with_in_a_period_str = []
+    observation_point_with_in_a_period_num = []
+    observation_point_with_in_2pi = generate_x(L, period + 1) + L
 
     for partition in range(period):
         partitions[partition] = []
         means.append(0)
 
     for idx, val in enumerate(data):
-        partitions[timesteps[idx] % period].append(val)
+        observation_point = timesteps[idx] % period
+        partitions[observation_point].append(val)
+        observation_point_with_in_a_period_num.append(observation_point_with_in_2pi[observation_point])
+        observation_point_with_in_a_period_str.append(f'{observation_point + 1}')
 
     for partition, vals in partitions.items():
         means[partition] = sum(vals) / len(vals)
 
-    return means, partitions
+    df_binned = pd.DataFrame({'Observation Point str': observation_point_with_in_a_period_str,
+                              'Observation Point num': observation_point_with_in_a_period_num,
+                              'Observation': data})
+    return means, partitions, df_binned
 
 
 def compute_fourier_coefficients_from_least_square_optimization(binned_data, num_data, components, L):
@@ -422,43 +500,58 @@ exit()
 spl = 1000
 
 # Periodic data sequence
-# data = [0, 0, 1, 0, 0]
-data = [100, 100, 200, 150, 140]
-timesteps = [0, 1, 2, 3, 4]
-num_data = len(data)
-period = 5
+df_rain = pd.read_csv('../data/mini_use_case/TerraClimateOromiaMontlhyPrecip.csv')
+rain = np.array(df_rain['(mm) Precipitation (TerraClimate) at State, 1958-01-01 to 2019-12-31'].tolist())
+months = list(range(len(rain)))
 
-bin_means, binned_data = partition_data_according_to_period(data, timesteps, period)
+# data = [0, 0, 1, 0, 0]
+# data = [100, 100, 200, 150, 140]
+# timesteps = [0, 1, 2, 3, 4]
+# period = 5
+
+data = rain
+timesteps = months
+period = 12
+
+num_data = len(data)
+
+L = np.pi
+
+bin_means, binned_data, df_binned = partition_data_according_to_period(data, timesteps, period, L)
 
 # Number of data points
 m = len(bin_means)  # => number of line segments = m-1 = 4
 
 f = linear_interpolate_data_sequence(bin_means, spl)
-
+'''
+# Experiment: Use spl based interpolated data in the least square estimation of Fourier coefficients
+# Preparing data
+bin_means, binned_data = partition_data_according_to_period(f, list(range(len(f))), len(f))
+num_data = len(f)
+period = len(f)
+'''
 # Total number of samples
 tns = len(f)  # spl * (m - 1)
 
 dx = 2 / (tns - 1)
-L = np.pi
 x = generate_x(L, len(f))  # L * np.arange(-1, 1, dx)
 dxs = np.diff(x)
 
-fig, ax = plt.subplots()
-sns.lineplot(x=x, y=f, color='y', linewidth=4, label='Original function')
-name = 'Accent'
-cmap = get_cmap('tab10')
-colors = cmap.colors
-ax.set_prop_cycle(color=colors)
-
+# fig, ax = plt.subplots()
+# sns.lineplot(x=x, y=f, color='y', linewidth=4, label='Original function')
+# name = 'Accent'
+# cmap = get_cmap('tab10')
+# colors = cmap.colors
+# ax.set_prop_cycle(color=colors)
 
 # Compute Fourier series
 C0 = np.sum(f * np.ones_like(x)) * dx
 fFS = C0 / 2
 
-components = 5
+components = 4
 
-least_sq = False
-full_blown = True
+least_sq = True
+full_blown = False
 
 if full_blown:
     A_sinusoidal, s0_sinusoidal = generate_sinusoidal_generating_full_blown_LDS(components)
@@ -469,6 +562,35 @@ else:
     A_sinusoidal, s0_sinusoidal = generate_sinusoidal_generating_LDS(components, x[0])
 
 A_fun, sinusoidals = generate_sinusoidal_curves(A_sinusoidal, s0_sinusoidal, dx, len(f), L)
+
+
+def sinusoidals_to_df(x, sinusoidals, components):
+    sinus = []
+    for comp in range(components):
+        freq = comp + 1
+        for i, s in enumerate(sinusoidals[2 * comp, :]):
+            sinus.append({'Type': 'sin', 'Frequency': freq, 'Angle': x[i], 'Value': s})
+        for i, c in enumerate(sinusoidals[2 * comp + 1, :] / freq):
+            sinus.append({'Type': 'cos', 'Frequency': freq, 'Angle': x[i], 'Value': c})
+
+    sinus_df = pd.DataFrame(sinus)
+    return sinus_df
+
+
+def plot_sinusoidals(sinus_df, period):
+    x_highlight = generate_x(L, period + 1)
+    g = sns.FacetGrid(sinus_df, col='Type', row='Frequency', margin_titles=True)
+    g.map(sns.lineplot, 'Angle', 'Value')
+    axes = g.axes
+    for ax_row in axes:
+        for ax_col in ax_row:
+            for start in np.arange(0, period, 2):
+                ax_col.axvspan(x_highlight[start], x_highlight[start + 1], color="green", alpha=0.3)
+    g.set_titles(col_template='{col_name}', row_template='$\omega = {row_name}$')
+    plt.show()
+# sinus_df = sinusoidals_to_df(x, sinusoidals, components)
+# plot_sinusoidals(sinus_df, period)
+# exit()
 '''
 trig_sinus = generate_sinusoidal_curves_from_trig_functions(x, components, len(f), L)
 for k in range(components):
@@ -519,19 +641,38 @@ print(magnitudes)
 # exit()
 
 # How many spl's to advance the LDS
-num_full_spls_to_predict = 15
-prediction_step_length = 0.5
-points_to_predict = int(np.ceil(num_full_spls_to_predict / prediction_step_length))
+num_full_spls_to_predict = 12
+prediction_step_length = 1
+num_points_to_predict = int(np.ceil(num_full_spls_to_predict / prediction_step_length))
 
 if full_blown:
     A, s0 = generate_full_full_blown_LDS_for_mat_exp(A_sinusoidal, s0_sinusoidal, C0, C, D, dx, L, spl * prediction_step_length)
     # A, s0 = generate_full_full_blown_LDS_for_mat_exp(A_sinusoidal, s0_sinusoidal, C0_trig, C_trig, D_trig, dx, L, spl * prediction_step_length)
+    title = 'Full Blown LDS Predictions'
 else:
     # A, s0 = generate_full_LDS(A_sinusoidal, s0_sinusoidal, C0, C, D, dx, L, spl * prediction_step_length)
     A, s0 = generate_full_LDS_for_mat_exp(A_sinusoidal, s0_sinusoidal, C0, C, D, dx, L, spl * prediction_step_length)
+    title = 'Compact LDS Predictions'
 
-fourier_curve_from_trig_functions(C0_trig, C_trig, D_trig, x, L)
+plot_derivatives = False
+
+if plot_derivatives:
+    title += ' and Derivatives'
+
+if prediction_step_length < 1 and least_sq:
+    title += '\nIntermediate Points & Least SQ'
+elif prediction_step_length < 1:
+    title += '\nIntermediate Points'
+elif least_sq:
+    title += '\nLeast SQ'
+
 if full_blown:
-    fourier_curve_from_full_blown_LDS(A, s0, points_to_predict, L, m, prediction_step_length)
+    LDS_pred = fourier_curve_from_full_blown_LDS(A, s0, num_points_to_predict, L, m, prediction_step_length)
 else:
-    fourier_curve_from_LDS(A, s0, points_to_predict, L, m, prediction_step_length)
+    LDS_pred = fourier_curve_from_LDS(A, s0, num_points_to_predict, L, m, prediction_step_length)
+
+trig_pred = fourier_curve_from_trig_functions(C0_trig, C_trig, D_trig, x, L)
+
+
+plot_all(x, f, trig_pred, LDS_pred, num_points_to_predict, df_binned, L, m, prediction_step_length, title, plot_derivatives, '')
+# plot_predictions_with_data_distributions(LDS_pred, num_points_to_predict, df_binned, prediction_step_length, type='violin', title='', file_name='')
