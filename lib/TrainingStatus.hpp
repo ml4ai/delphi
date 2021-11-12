d34de967ee7fe08a09b61799532333a54a0c42dc
Joseph Astier <jastier@gmail.com>
2021-11-12 14:08:58 -0700

    The TrainingStatus class now uses the DatabaseHelper methods

--- lib/TrainingStatus.hpp
+++ lib/TrainingStatus.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <thread>
-#include <nlohmann/json.hpp>
+#include "DatabaseHelper.hpp"
 
 class AnalysisGraph;
 
@@ -11,23 +11,16 @@ class TrainingStatus {
   private:
   AnalysisGraph* ag =nullptr;
   std::thread *pThread = nullptr;
+  Database* database = nullptr;
   void scheduler();
 
   public:
   const string table = "training_status";
-  const int nCols = 7;
-  const string col0 = "id";
-  const string col1 = "progress";
-  const string col2 = "trained";
-  const string col3 = "stopped";
-  const string col4 = "log_likelihood";
-  const string col5 = "log_likelihood_previous";
-  const string col6 = "log_likelihood_map";
   TrainingStatus();
   ~TrainingStatus();
-  bool write_to_db();
+  void write_to_db();
   void init_db();
   void start_monitoring(AnalysisGraph* ag);
   void stop_monitoring();
-  nlohmann::json read_from_db(string modelId);
+  string read_from_db(string modelId);
 };
