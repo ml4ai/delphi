d900f1b59aab763c8fac2c8e47b962d9f1d4d486
Joseph Astier <jastier@gmail.com>
2022-02-10 16:07:53 -0700

    Model status now created within a mutex so the ID is used at most once

--- apps/delphi_rest_api.cpp
+++ apps/delphi_rest_api.cpp
@@ -211,7 +211,7 @@ class Model {
     Adarsh
     */
     static int get_sampling_resolution() {
-        return 100;
+        return 100; 
     }
 
     static int get_burn() {
@@ -334,8 +334,8 @@ int main(int argc, const char* argv[]) {
     // prepare the model and experiment databases for use
     ModelStatus ms("startup", sqlite3DB);
     ExperimentStatus es("startup", "startup", sqlite3DB);
-    ms.startup();
-    es.startup();
+    ms.clean_db();
+    es.clean_db();
 
     /* Allow users to check if the REST API is running */
     mux.handle("/status").get(
@@ -352,7 +352,7 @@ int main(int argc, const char* argv[]) {
                            const served::request& req) {
             nlohmann::json json_data = nlohmann::json::parse(req.body());
 
-	    string model_id_field = ModelStatus::get_model_id_field_name();
+	    string model_id_field = "id";
 
 	    // input must have a model ID field
 	    if(!json_data.contains(model_id_field)) {
@@ -369,10 +369,9 @@ int main(int argc, const char* argv[]) {
 	    string modelId = json_data[model_id_field];
 	    ModelStatus ms(modelId, sqlite3DB);
 
-	    json status = ms.get_status();
-
             // do not overwrite model if it is training
-            if (ms.is_busy(status)) {
+            if (!ms.start_training()) {
+		json status = ms.get_status();
                 json ret;
                 ret[ms.MODEL_ID] = modelId;
                 ret[ms.PROGRESS] = status[ms.PROGRESS];
@@ -381,9 +380,6 @@ int main(int argc, const char* argv[]) {
                 response << dumpStr; 
                 return dumpStr; 
             }
-
-	    // otherwise, create and train model.
-            ms.set_initial_status();
 
 	    size_t kde_kernels = Model::get_kde_kernels();
 	    int burn = Model::get_burn();
@@ -483,26 +479,23 @@ int main(int argc, const char* argv[]) {
 
 	    ModelStatus ms(modelId, sqlite3DB);
 
-	    string experiment_id_field_name = 
-	      ExperimentStatus::get_experiment_id_field_name();
-	    string model_id_field_name = 
-	      ExperimentStatus::get_model_id_field_name();
-
             json ret;
-            ret[model_id_field_name] = modelId;
+            ret[ms.MODEL_ID] = modelId;
 
 	    json modelStatus = ms.get_status();
 
 	    // Model not found
 	    if(modelStatus.empty()) {
-              ret["status"] = "Invalid model ID";
+              ret[ms.STATUS] = "Invalid model ID";
               res << ret.dump();
               return ret;
 	    }
 
             // Model not trained
-	    if(ms.is_busy(modelStatus)) {
-	      ret["status"] = 
+	    float progress = modelStatus[ms.PROGRESS];
+	    if(progress < 1.0) {
+	      ret[ms.PROGRESS] = progress;
+	      ret[ms.STATUS] = 
 	        "Model training must finish before experimenting";
               res << ret.dump();
               return ret;
@@ -609,23 +602,24 @@ int main(int argc, const char* argv[]) {
                 return ret;
             }
 
-	    // check existence of model
-	    json model_status = ms.get_status();
-	    if(model_status.empty()) {
-	      string report = "Model does not exist. "
-	        "Please create model before editing.";
-	      ret[ms.STATUS] = report;
+	    json modelStatus = ms.get_status();
+	    
+            // Model not found
+            if(modelStatus.empty()) {
+              ret[ms.STATUS] = "Model does not exist. "
+                "Please create model before editing.";
               res << ret.dump();
               return ret;
-            }
-	    
-	    // The model must be fully trained before it can be edited.
-	    if(ms.is_busy(model_status)) {
-	      ret[ms.PROGRESS] = model_status[ms.PROGRESS];
+            }   
+
+            // Model not trained
+            float progress = modelStatus[ms.PROGRESS];
+            if(progress < 1.0) {
+              ret[ms.PROGRESS] = progress;
 	      ret[ms.STATUS] = "Training must finish before editing.";
               res << ret.dump();
               return ret;
-            }
+            }  
 
 	    // Get the model row from the database
             json query_result = sqlite3DB->select_delphimodel_row(modelId);
