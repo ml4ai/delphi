43cdca22e1cf1f2753be8b212c52eb5e0cb70c59
Joseph Astier <jastier@gmail.com>
2022-02-15 08:00:40 -0700

    Fixed a state issue

--- apps/delphi_rest_api.cpp
+++ apps/delphi_rest_api.cpp
@@ -186,10 +186,10 @@ class Model {
             model_id,
             G.serialize_to_json_string(false)
         );
+        ms.finalize("Ready");
 	if(!ms.unlock()) {
 	  cout << "Model " << model_id << " unlock failed" << endl;
 	}
-        ms.finalize("Ready!");
     }
 
     static size_t get_kde_kernels() {
