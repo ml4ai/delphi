b6ad861eefe355ed3bc7068a1a6cb99fd69fa2c2
Joseph Astier <jastier@gmail.com>
2022-03-10 16:41:19 -0700

    Added config file reader

--- lib/Config.cpp
+++ lib/Config.cpp
@@ -1,18 +1,47 @@
 #include "Config.hpp"
 #include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+
+// This configuration class reads the config file each time
+// a variable is retrieved.
 
 // Return the max samples for early stopping
 int Config::get_early_stopping_n_samples() {
-  update();
-  return early_stopping_n_samples;
+  json config = read_config();
+  if(config.contains(EARLY_STOPPING_N_SAMPLES)){
+    return (int)config[EARLY_STOPPING_N_SAMPLES];
+  }
+  return DEFAULT_EARLY_STOPPING_N_SAMPLES;
 }
 
 // Return the log liklihood delta for early stopping
 double Config::get_early_stopping_log_liklihood_delta() {
-  update();
-  return early_stopping_log_liklihood_delta;
+  json config = read_config();
+  if(config.contains(EARLY_STOPPING_LOG_LIKELIHOOD_DELTA)){
+    return (double)config[EARLY_STOPPING_LOG_LIKELIHOOD_DELTA];
+  }
+  return DEFAULT_EARLY_STOPPING_LOG_LIKELIHOOD_DELTA;
 }
 
-// read the config file and update our values
-void update() {
+// Return a JSON struct read from the config file
+json Config::read_config() {
+  ifstream file;
+  file.open(filename);
+  string jsonString;
+  string line;
+  json config;
+  if ( file.is_open() ) {
+    while ( file ) { 
+      getline (file, line);
+      jsonString += line;
+    }
+    file.close();
+    config = json::parse(jsonString);
+  }
+  else {
+    cerr << "Couldn't open config file at '" << filename << "'" << endl;
+  }
+  return config;
 }
+
--- lib/Config.hpp
+++ lib/Config.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Config.hpp"
 #include <nlohmann/json.hpp>
 
 using namespace std;
@@ -9,16 +8,25 @@ using json = nlohmann::json;
 // user-set parameters that can be adjusted in realtime
 class Config {
 
+  // this path presumes delphi running from the build directory.
+
   private:
-    json config;
+    string filename = "../config.json";
+    json read_config();
     void update();
-    double early_stopping_n_samples = 100;
-    double early_stopping_log_liklihood_delta = 0.001;
 
   public:
-    Config(){}
+    Config(){};
+    Config(string filename):filename(filename){};
     ~Config(){}
 
     int get_early_stopping_n_samples();
     double get_early_stopping_log_liklihood_delta ();
+
+    const string DEFAULT_FILENAME = "../config.json";
+    const int DEFAULT_EARLY_STOPPING_N_SAMPLES = 100;
+    const double DEFAULT_EARLY_STOPPING_LOG_LIKELIHOOD_DELTA = 0.0001;
+
+    const string EARLY_STOPPING_LOG_LIKELIHOOD_DELTA = "log_liklihood_delta";
+    const string EARLY_STOPPING_N_SAMPLES = "n_samples";
 };
